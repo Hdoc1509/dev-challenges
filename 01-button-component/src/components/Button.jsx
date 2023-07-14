7eded9f23454a588e364445220dbb3921fa5a875
hdoc <blueguitarist1520@gmail.com>
2023-07-14 17:04:04 -0500

    refactor(01-button-component): change class prefix `button` to `btn`

--- 01-button-component/src/components/Button.jsx
+++ 01-button-component/src/components/Button.jsx
@@ -1,5 +1,5 @@
 import './Button.scss'
 
 export const Button = ({ variant }) => {
-  return <button className={`button--${variant ?? 'default'}`}>Default</button>;
+  return <button className={`btn--${variant ?? 'default'}`}>Default</button>;
 };
