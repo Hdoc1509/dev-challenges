78a7cbdccf69247b5a9a41879a84d2a376c5cf53
hdoc <blueguitarist1520@gmail.com>
2023-07-15 23:43:53 -0500

    feat(01-button-component): add types for components

--- 01-button-component/src/components/Button.tsx
+++ 01-button-component/src/components/Button.tsx
@@ -1,7 +1,12 @@
 import classNames from "classnames";
 import "./Button.scss";
 
-export const Button = ({ variant, disableShadow }) => {
+type Props = {
+  variant?: "outline" | "text";
+  disableShadow?: boolean;
+};
+
+export const Button = ({ variant, disableShadow = false }: Props) => {
   const btnClass = classNames("btn", {
     [`btn--${variant}`]: variant !== undefined,
     ["btn--no-shadow"]: disableShadow,
--- 01-button-component/src/components/ButtonVariant.tsx
+++ 01-button-component/src/components/ButtonVariant.tsx
@@ -1,6 +1,11 @@
+import { PropsWithChildren } from 'react';
 import './ButtonVariant.scss'
 
-export const ButtonVariant = ({ name, children }) => {
+type Props = {
+	name: string
+}
+
+export const ButtonVariant = ({ name, children }: PropsWithChildren<Props>) => {
   return (
     <span className="button-variant">
       <p className="button-variant__name">{name}</p>
