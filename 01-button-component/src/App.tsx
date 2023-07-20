7c4dcb6b55ba262e53784e1086235fbd5af5e39e
hdoc <blueguitarist1520@gmail.com>
2023-07-19 21:11:52 -0500

    refactor(01-button-component): use other icon for show icon variants/styles

--- 01-button-component/src/App.tsx
+++ 01-button-component/src/App.tsx
@@ -3,6 +3,8 @@ import { Button } from "./components/Button";
 import { ButtonVariant } from "./components/ButtonVariant";
 
 function App() {
+  const buttonIcon = "shopping_cart";
+
   return (
     <>
       <header>
@@ -35,16 +37,16 @@ function App() {
             <Button endIcon="add_shopping_cart" />
           </ButtonVariant>
           <ButtonVariant name="End icon - outlined">
-            <Button endIcon="add_shopping_cart" iconStyle="outlined" />
+            <Button endIcon={buttonIcon} iconStyle="outlined" />
           </ButtonVariant>
           <ButtonVariant name="End icon - round">
-            <Button endIcon="add_shopping_cart" iconStyle="round" />
+            <Button endIcon={buttonIcon} iconStyle="round" />
           </ButtonVariant>
           <ButtonVariant name="End icon - sharp">
-            <Button endIcon="add_shopping_cart" iconStyle="sharp" />
+            <Button endIcon={buttonIcon} iconStyle="sharp" />
           </ButtonVariant>
           <ButtonVariant name="End icon - tow-tone">
-            <Button endIcon="add_shopping_cart" iconStyle="two-tone" />
+            <Button endIcon={buttonIcon} iconStyle="two-tone" />
           </ButtonVariant>
           <ButtonVariant name="Small (sm)">
             <Button />
