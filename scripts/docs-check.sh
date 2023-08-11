c862bbb6f937ab39ca67fcbdff4befa7e485a5df
hdoc <hector.ochoa.dev@gmail.com>
2023-08-10 19:07:38 -0500

    ci: only build if changed file in challenge folder isn't `README.md`

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -1,6 +1,8 @@
 echo "prev commit: $CACHED_COMMIT_REF"
 echo "current commit: $COMMIT_REF"
-git diff --name-only $CACHED_COMMIT_REF $COMMIT_REF | tr -d '\n' | grep -q "^$1/README.md\$"
+git diff --quiet . ||
+  git diff --name-only $CACHED_COMMIT_REF $COMMIT_REF |
+  tr -d '\n' | grep -q "^$1/README.md\$"
 status=$?
 echo "diff exit code: ${status}"
 exit $status
