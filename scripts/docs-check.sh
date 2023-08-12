e7cdbeb3c65546a6cd631025ba0b1865145511e1
hdoc <hector.ochoa.dev@gmail.com>
2023-08-11 21:57:48 -0500

    ci: use commits id's for every `git diff` command

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -1,6 +1,6 @@
 echo "prev commit: $CACHED_COMMIT_REF"
 echo "current commit: $COMMIT_REF"
-git diff --quiet . ||
+git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ||
   git diff --name-only $CACHED_COMMIT_REF $COMMIT_REF |
   tr -d '\n' | grep -q "^$1/README.md\$"
 status=$?
