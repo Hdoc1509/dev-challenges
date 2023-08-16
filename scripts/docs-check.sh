91a4eeb8eb2fdf4f92828f42db49dd1d50b0ea41
hdoc <hector.ochoa.dev@gmail.com>
2023-08-16 18:12:54 -0500

    ci: adapt script to run from repo root

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -1,8 +1,9 @@
 echo "prev commit: $CACHED_COMMIT_REF"
 echo "current commit: $COMMIT_REF"
-git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF ||
+__package_dir="$1"
+git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF "${__package_dir}" ||
   git diff --name-only $CACHED_COMMIT_REF $COMMIT_REF |
-  tr -d '\n' | grep -q "^$1/README.md\$"
+  tr -d '\n' | grep -q "^${__package_dir}/README.md\$"
 status=$?
 echo "diff exit code: ${status}"
 exit $status
