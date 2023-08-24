bc29ce9f0d7e6cc178da0b6bfa2e6dcd371eb4ac
hdoc <hector.ochoa.dev@gmail.com>
2023-08-24 18:07:55 -0500

    feat: add internal package `@internal/components`

---
+++ libs/components/src/HeaderWithTheme.tsx
@@ -0,0 +1,20 @@
+import { Icon } from "@hdoc/react-material-icons";
+import { ThemeButton } from "@hdoc/react-toggle-theme";
+import "./HeaderWithTheme.scss";
+
+type HeaderWithThemeProps = {
+  title: string;
+}
+
+export const HeaderWithTheme = ({ title }: HeaderWithThemeProps) => (
+  <header>
+    <h1>{title}</h1>
+    <div className="theme-switcher">
+      <ThemeButton
+        lightElement={<Icon name="light_mode" />}
+        darkElement={<Icon name="dark_mode" />}
+        fullRounded
+      />
+    </div>
+  </header>
+);
---
+++ libs/components/src/main.tsx
@@ -0,0 +1,2 @@
+export { Footer } from './Footer'
+export { HeaderWithTheme } from './HeaderWithTheme'
