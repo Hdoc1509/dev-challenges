9c9993f4f215cdca8f79bbe737bdda89f39e3229
hdoc <hector.ochoa.dev@gmail.com>
2023-09-14 21:26:53 -0500

    feat(input-component): use `Simplify` utility type from `type-fest`

--- apps/input-component/lib/types.d.ts
+++ apps/input-component/lib/types.d.ts
@@ -1,19 +1,22 @@
 import type { ComponentProps } from "react";
+import type { Simplify } from "type-fest";
 import { Icon } from "@hdoc/react-material-icons";
 
 type IconProps = ComponentProps<typeof Icon>;
 type InputMultiline = { multiline: true; rows?: number };
 type InputNotMultiline = { multiline?: never; rows?: never };
 
-export type InputProps = {
-  label?: string;
-  placeholder?: string;
-  error?: boolean;
-  disabled?: boolean;
-  helperText?: string;
-  startIcon?: IconProps["name"];
-  endIcon?: IconProps["name"];
-  value?: string;
-  size?: "sm" | "md";
-  fullWidth?: boolean;
-} & (InputNotMultiline | InputMultiline);
+export type InputProps = Simplify<
+  {
+    label?: string;
+    placeholder?: string;
+    error?: boolean;
+    disabled?: boolean;
+    helperText?: string;
+    startIcon?: IconProps["name"];
+    endIcon?: IconProps["name"];
+    value?: string;
+    size?: "sm" | "md";
+    fullWidth?: boolean;
+  } & (InputNotMultiline | InputMultiline)
+>;
