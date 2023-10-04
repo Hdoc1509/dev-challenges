506fc66d7982a2f354927f1bf8dfdb7139d6adb4
hdoc <hector.ochoa.dev@gmail.com>
2023-10-04 10:42:54 -0500

    build: check name of changed files only in package directory

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -1,8 +1,8 @@
 echo "prev commit: $CACHED_COMMIT_REF"
 echo "current commit: $COMMIT_REF"
 __package_dir="$1"
-git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF "${__package_dir}" ||
-  git diff --name-only $CACHED_COMMIT_REF $COMMIT_REF |
+git diff --quiet "$CACHED_COMMIT_REF" "$COMMIT_REF" "${__package_dir}" ||
+  git diff --name-only "$CACHED_COMMIT_REF" "$COMMIT_REF" "${__package_dir}" |
   tr -d '\n' | grep -q "^${__package_dir}/README.md\$"
 status=$?
 echo "diff exit code: ${status}"
