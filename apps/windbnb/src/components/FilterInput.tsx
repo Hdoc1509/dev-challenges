d3c390ad9695a67b50c521820d3b1ead6cd914a1
hdoc <hector.ochoa.dev@gmail.com>
2023-10-07 22:29:26 -0500

    feat(windbnb): add placeholder for `FilterInput`

--- apps/windbnb/src/components/FilterInput.tsx
+++ apps/windbnb/src/components/FilterInput.tsx
@@ -3,20 +3,27 @@ import "./FilterInput.scss";
 
 type Props = {
   label: string;
-  value: string;
+  value?: string;
   isSelected?: boolean;
   name: string;
+  placeholder?: string;
 };
 
-export const FilterInput = ({ label, value, isSelected, name }: Props) => {
-  const filterInputClass = clsx("filter-input", {
-    "filter-input--selected": isSelected,
-  });
+export const FilterInput = ({
+  label,
+  value,
+  isSelected,
+  name,
+  placeholder,
+}: Props) => {
+  const labelClass = clsx({ 'selected': isSelected });
 
   return (
-    <div className={filterInputClass}>
-      <label>{label}</label>
-      <input name={name} value={value} readOnly />
+    <div className="filter-input">
+      <label className={labelClass}>
+        {label}
+        <input name={name} value={value} placeholder={placeholder} readOnly />
+      </label>
     </div>
   );
 };
