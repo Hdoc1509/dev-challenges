fff4f249b2be38b92249916caa26fee8fcee7ae9
hdoc <hector.ochoa.dev@gmail.com>
2023-10-16 11:10:48 -0500

    build(windbnb): import used variants of icon fonts

--- apps/windbnb/src/main.tsx
+++ apps/windbnb/src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@material-design-icons/font";
+import "@material-design-icons/font/filled.css";
+import "@material-design-icons/font/round.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
