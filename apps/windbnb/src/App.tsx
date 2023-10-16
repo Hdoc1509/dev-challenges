9d8c6d060a2885974501db078bb1fbf5286a5c61
hdoc <hector.ochoa.dev@gmail.com>
2023-10-15 21:44:04 -0500

    refactor(windbnb): move header to its own component

--- apps/windbnb/src/App.tsx
+++ apps/windbnb/src/App.tsx
@@ -1,16 +1,12 @@
 import { useEffect } from "react";
 import { Footer } from "@internal/components";
 import { SearchResults } from "./components/Results";
-import { SearchBar } from "./components/SearchBar";
-import { FilterDrawer } from "./components/FilterDrawer";
-import { useFilterStore } from "./store/filter";
+import { Header } from "./components/Header.tsx";
 import { useStays } from "./hooks/useStays.ts";
-import logoUrl from "../assets/logo.png";
 import "./App.css";
 
 function App() {
   const { stays, getStays, isLoading } = useStays();
-  const filter = useFilterStore((state) => state.filter);
 
   useEffect(() => {
     void getStays();
@@ -18,16 +14,7 @@ function App() {
 
   return (
     <>
-      <header className="main-header">
-        <img src={logoUrl} className="main-header__logo" alt="windbnb logo" />
-        <SearchBar />
-        <FilterDrawer
-          isOpen={filter !== null}
-          onSearch={({ guests, location }) =>
-            void getStays({ guests, location })
-          }
-        />
-      </header>
+      <Header getStays={getStays} />
       <main>
         {isLoading ? <p>Loading...</p> : <SearchResults results={stays} />}
       </main>
---
+++ apps/windbnb/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { SearchBar } from "./SearchBar";
+import { FilterDrawer } from "./FilterDrawer";
+import { useFilterStore } from "../store/filter";
+import type { FnSearchOptions } from "../types";
+import logoUrl from "../../assets/logo.png";
+import "./Header.scss";
+
+export const Header = ({ getStays }: { getStays: FnSearchOptions }) => {
+  const filter = useFilterStore((state) => state.filter);
+
+  return (
+    <header className="main-header">
+      <img src={logoUrl} className="main-header__logo" alt="windbnb logo" />
+      <SearchBar />
+      <FilterDrawer
+        isOpen={filter !== null}
+        onSearch={(search) =>
+          getStays({ guests: search?.guests, location: search?.location })
+        }
+      />
+    </header>
+  );
+};
