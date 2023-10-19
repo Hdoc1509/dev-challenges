59ed1d80a3780346c4a128cfc6a3ca166f235bde
hdoc <hector.ochoa.dev@gmail.com>
2023-10-19 18:55:58 -0500

    feat(todo-app): add styles for todo form

--- apps/todo-app/src/components/TodoList.tsx
+++ apps/todo-app/src/components/TodoList.tsx
@@ -1,13 +1,14 @@
-import { TodoItem } from './TodoItem'
+import { TodoItem } from "./TodoItem";
+import { Button } from "@hdoc/react-button";
 import type { Todo } from "../types";
-import './TodoList.scss';
+import "./TodoList.scss";
 
 export const TodoList = ({ todos = [] }: { todos: Todo[] }) => {
   return (
     <div className="todo-container">
       <form className="todo-form">
-        <input type="text" />
-        <button>Add</button>
+        <input type="text" placeholder="Add details" />
+        <Button text="Add" color="primary" />
       </form>
       <ul className="todo-list">
         {todos.length === 0 ? (
