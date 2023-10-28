c52de5427641b24c4ac5041a3e60f0fc1e4de392
hdoc <hector.ochoa.dev@gmail.com>
2023-10-28 12:15:17 -0500

    build(quote-generator): import only filled variant of icon fonts

--- apps/random-quote-generator/src/main.tsx
+++ apps/random-quote-generator/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@material-design-icons/font";
+import "@material-design-icons/font/filled.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
