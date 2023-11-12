2e9c221c416ba7260119e12506252a65c39c7211
hdoc <hector.ochoa.dev@gmail.com>
2023-11-12 12:44:01 -0500

    feat(country-quiz): use custom store hook for retrieve mocked questions

--- apps/country-quiz/src/App.tsx
+++ apps/country-quiz/src/App.tsx
@@ -1,53 +1,43 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useQuestionStore } from "./store/questions";
 import { Button } from "@hdoc/react-button";
 import { Footer } from "@internal/components";
 import { Quiz } from "./components/Quiz";
-import { QUESTION } from "./utils";
-import type { Question } from "./types";
 import characterUrl from "./assets/character.svg";
 import "./App.css";
 
 function App() {
-  const [question, setQuestion] = useState<Question>({
-    id: 1,
-    category: "countryOfCapital",
-    question:
-      Math.random() > 0.5
-        ? QUESTION.countryOfCapital("Kuala Lumpur")
-        : QUESTION.flagOfCountry(),
-    correctAnswer: "Malaysia",
-    answerOptions: ["Vietnam", "Malaysia", "Sweden", "Austria"],
-    selectedAnswer: null,
-    hasBeenAnsweredCorrectly: null,
-  });
+  const questions = useQuestionStore((s) => s.questions);
+  const currentQuestionIndex = useQuestionStore((s) => s.currentQuestionIndex);
+  const getQuestions = useQuestionStore((s) => s.getQuestions);
+  const selectAnswer = useQuestionStore((s) => s.selectAnswer);
 
-  const onAnswer = (answer: string) => {
-    console.log({ answer });
-    setQuestion({
-      ...question,
-      selectedAnswer: answer,
-      hasBeenAnsweredCorrectly: answer === question.correctAnswer,
-    });
-  };
+  const question = questions[currentQuestionIndex];
+
+  const onAnswer = (answer: string) => selectAnswer(question.id, answer);
+
+  useEffect(() => void getQuestions(), [getQuestions]);
 
   return (
     <>
       <main>
         <h1>Country Quiz</h1>
-        <div className="quiz-container">
+        <div className="quiz-container" data-answer={question?.selectedAnswer}>
           <img
             src={characterUrl}
             className="quiz-container__character"
             alt="character"
           />
-          <Quiz
-            question={question.question}
-            correctAnswer={question.correctAnswer}
-            answerOptions={question.answerOptions}
-            selectedAnswer={question.selectedAnswer}
-            onAnswer={onAnswer}
-          />
-          {question.selectedAnswer && (
+          {question && (
+            <Quiz
+              question={question.question}
+              correctAnswer={question.correctAnswer}
+              answerOptions={question.answerOptions}
+              selectedAnswer={question.selectedAnswer}
+              onAnswer={onAnswer}
+            />
+          )}
+          {question?.selectedAnswer && (
             <Button className="quiz-next" text="Next" color="warning" />
           )}
         </div>
---
+++ apps/country-quiz/src/store/questions.ts
@@ -0,0 +1,60 @@
+import { create } from "zustand";
+import { getMockQuestions } from "../services/questions";
+import type { Question } from "../types";
+
+type State = {
+  questions: Question[];
+  currentQuestionIndex: number;
+  correctAnswers: number;
+  incorrectAnswers: number;
+  unansweredQuestions: number;
+  totalQuestions: number;
+};
+
+type Action = {
+  getQuestions: (limit?: number) => Promise<void>;
+  selectAnswer: (questionId: number, answer: string) => void;
+  goNextQuestion: () => void;
+  goPrevQuestion: () => void;
+  reset: () => void;
+};
+
+const initialState: State = {
+  questions: [],
+  currentQuestionIndex: 0,
+  correctAnswers: 0,
+  incorrectAnswers: 0,
+  unansweredQuestions: 0,
+  totalQuestions: 0,
+};
+
+export const useQuestionStore = create<State & Action>()((set, get) => {
+  return {
+    ...initialState,
+
+    getQuestions: async (limit?: number) => {
+      const questions = await getMockQuestions(limit);
+
+      set({
+        questions: structuredClone(questions).sort(() => Math.random() - 0.5),
+      });
+    },
+    selectAnswer: (questionId: number, answer: string) => {
+      const { questions } = get();
+      const newQuestions = structuredClone(questions);
+      const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
+      const question = newQuestions[questionIndex];
+
+      newQuestions[questionIndex] = {
+        ...question,
+        selectedAnswer: answer,
+        hasBeenAnsweredCorrectly: answer === question.correctAnswer,
+      };
+
+      set({ questions: newQuestions });
+    },
+    goNextQuestion: () => {},
+    goPrevQuestion: () => {},
+    reset: () => set(initialState),
+  };
+});
