1bd0fc5e1a4e761abca1322886553eb9516acb72
hdoc <hector.ochoa.dev@gmail.com>
2023-11-18 17:55:12 -0500

    feat(country-quiz): add mock for countries

---
+++ apps/country-quiz/src/mocks/countries.ts
@@ -0,0 +1,28 @@
+import type { Country } from "../schemas/country";
+
+export const countries: Country[] = [
+  {
+    name: "Guan",
+    capital: ["Hagåtña"],
+    flagUrl: "https://flagcdn.com/gu.svg",
+    region: "Oceania",
+  },
+  {
+    name: "United Arab Emirates",
+    capital: ["Abu Dhabi"],
+    flagUrl: "https://flagcdn.com/ae.svg",
+    region: "Asia",
+  },
+  {
+    name: "Hong Kong",
+    capital: ["City of Victoria"],
+    flagUrl: "https://flagcdn.com/hk.svg",
+    region: "Asia",
+  },
+  {
+    name: "Antigua and Barbuda",
+    capital: ["Saint John's"],
+    flagUrl: "https://flagcdn.com/ag.svg",
+    region: "Americas",
+  },
+];
