95c0461a8b4e95fe351e09bd05419e2ae03ecd2a
hdoc <hector.ochoa.dev@gmail.com>
2023-11-30 18:07:39 -0500

    feat(weather-app): stylize sidebar

--- apps/weather-app/src/components/CityWeather.tsx
+++ apps/weather-app/src/components/CityWeather.tsx
@@ -4,23 +4,21 @@ import "./CityWeather.scss";
 
 export const CityWeather = () => {
   return (
-    <aside className="city-weather">
+    <aside className="weather">
       <header>
-        <Button text="Search for places" />
-        <ButtonIcon icon="gps_fixed" />
+        <Button text="Search for places" className="weather__search" />
+        <ButtonIcon icon="gps_fixed" className="weather__current" />
       </header>
-      <section>
-        <img src="/shower.png" alt="shower" />
-        <p className="weather-degree">15 °C</p>
-        <p className="weather-description">Shower</p>
-        <footer>
-          <p className="weather-date">Today · Fri, 5 Jun</p>
-          <p className="weather-location">
-            <Icon name="location_on" />
-            Helsinki
-          </p>
-        </footer>
-      </section>
+      <img src="/shower.png" alt="shower" className="weather__icon" />
+      <p className="weather__degree">
+        15<span className="weather__degree-unit">℃</span>
+      </p>
+      <p className="weather__description">Shower</p>
+      <p className="weather__date">Today • Fri, 5 Jun</p>
+      <p className="weather__location">
+        <Icon name="location_on" />
+        Helsinki
+      </p>
     </aside>
   );
 };
