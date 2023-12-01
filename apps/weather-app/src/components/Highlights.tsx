15d913883e10c2b9eae9e19d0e137a51b80386e7
hdoc <hector.ochoa.dev@gmail.com>
2023-11-30 19:13:03 -0500

    feat(weather-app): stylize highlights

--- apps/weather-app/src/components/Highlights.tsx
+++ apps/weather-app/src/components/Highlights.tsx
@@ -7,29 +7,40 @@ export const Highlights = () => {
       <h2>{`Today's Highlights`}</h2>
       <section className="wind">
         <h3>Wind Status</h3>
-        <p>7 mph</p>
+        <p className="wind__speed">7</p>
         <p className="wind__direction">
-          <Icon name="near_me" />
+          <div className="wind__direction-icon-wrapper">
+            <Icon name="near_me" />
+          </div>
           WSW
         </p>
       </section>
       <section className="humidity">
         <h3>Humidity</h3>
-        <p>84%</p>
+        <p className="humidity__percentage">84</p>
         <div className="humidity-slider">
-          <div
-            className="humidity-slider__value"
-            style={{ width: "84%" }}
-          ></div>
+          <div className="humidity-slider__legend">
+            <span>0</span>
+            <span>50</span>
+            <span>100</span>
+          </div>
+          <div className="humidity-slider__bar-wrapper">
+            <div className="humidity-slider__bar"></div>
+            <div
+              className="humidity-slider__value"
+              style={{ width: "84%" }}
+            ></div>
+          </div>
+          <span className="humidity-slider__symbol">%</span>
         </div>
       </section>
       <section className="visibility">
         <h3>Visibility</h3>
-        <p>6,4 miles</p>
+        <p className="visibility__distance">6,4</p>
       </section>
       <section className="air-pressure">
         <h3>Air Pressure</h3>
-        <p>998 mb</p>
+        <p className="air-pressure__pressure">998</p>
       </section>
     </article>
   );
