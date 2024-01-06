4ec33c2c6188eeadb7ef826cf85eb60ea22d62aa
hdoc <hector.ochoa.dev@gmail.com>
2024-01-05 21:22:31 -0500

    refactor(weather-app): remove unused properties and actions from weather store

--- apps/weather-app/src/store/weather.ts
+++ apps/weather-app/src/store/weather.ts
@@ -1,5 +1,4 @@
 import { create } from "zustand";
-import type { LocationPosition } from "../schemas/location";
 import type { Weather } from "../schemas/weather";
 
 export type Forecast = {
@@ -12,23 +11,18 @@ export type Forecast = {
 };
 
 type State = {
-  location: LocationPosition | null;
   weather: Weather | null;
   forecast: Forecast[] | null;
 };
 
 type Action = {
-  setLocation: (location: LocationPosition) => void;
   setWeather: (weather: Weather) => void;
   setForecast: (forecast: Forecast[]) => void;
 };
 
 export const useWeatherStore = create<State & Action>()((set) => ({
-  location: null,
   weather: null,
   forecast: null,
-  highlights: null,
-  setLocation: (location: LocationPosition) => set({ location }),
   setWeather: (weather: Weather) => set({ weather }),
   setForecast: (forecast: Forecast[]) => set({ forecast }),
 }));
