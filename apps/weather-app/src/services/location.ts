4a3cbe95807944ecef3da6f4174ec06a5bac5599
hdoc <hector.ochoa.dev@gmail.com>
2024-01-05 22:11:30 -0500

    refactor(weather-app): remove unused service

--- apps/weather-app/src/services/location.ts
+++
@@ -1,8 +0,0 @@
-const searchResults = ["London", "Barcelona", "Long Beach"];
-
-export const getLocations = async (search: string) => {
-  console.log({ search });
-  return Promise.resolve(
-    searchResults.filter((r) => r.toLowerCase().includes(search.toLowerCase())),
-  );
-};
