49ca3a6feb62914cb01eb0c8e1123006d22e755a
hdoc <hector.ochoa.dev@gmail.com>
2024-01-05 21:17:26 -0500

    feat(weather-app): add service for search a city

---
+++ apps/weather-app/src/schemas/search-city.ts
@@ -0,0 +1,14 @@
+import * as z from "zod";
+
+export const SearchCityResponseSchema = z.array(
+  z.object({
+    id: z.number(),
+    name: z.string(),
+    region: z.string(),
+    country: z.string(),
+    lat: z.number(),
+    lon: z.number(),
+    url: z.string(),
+  }),
+);
+export type SearchCityResponse = z.infer<typeof SearchCityResponseSchema>;
