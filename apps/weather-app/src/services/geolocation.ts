830b83bc8d5425b5d08ed07b8ca25138814fa8cf
hdoc <hector.ochoa.dev@gmail.com>
2024-01-08 17:22:33 -0500

    refactor(weather-app): remove unused service

--- apps/weather-app/src/services/geolocation.ts
+++ apps/weather-app/src/services/geolocation.ts
@@ -1,22 +1,4 @@
-import { OPEN_WEATHER_MAP_API } from "../config";
-import { LocationResponseSchema, type LocationPosition } from "../schemas/location";
-
-export const getCityPosition = async (
-  city: string,
-): Promise<LocationPosition> => {
-  const params = new URLSearchParams({
-    q: city,
-    limit: "1",
-    appid: OPEN_WEATHER_MAP_API.KEY,
-  });
-
-  const res = await fetch(
-    `${OPEN_WEATHER_MAP_API.URL}/geo/1.0/direct?${params.toString()}`,
-  );
-  const data = LocationResponseSchema.parse(await res.json());
-
-  return data[0];
-};
+import { type LocationPosition } from "../schemas/location";
 
 export const getCurrentPosition = async (): Promise<LocationPosition> => {
   return new Promise((resolve) => {
