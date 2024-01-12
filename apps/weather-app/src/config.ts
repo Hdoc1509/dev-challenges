d2b359744be8961eb8bcb2410ecf017009ed02bc
hdoc <hector.ochoa.dev@gmail.com>
2024-01-12 18:17:17 -0500

    refactor(weather-app): remove unused api

--- apps/weather-app/src/config.ts
+++ apps/weather-app/src/config.ts
@@ -1,9 +1,3 @@
-// NOTE: See docs at https://openweathermap.org/api/one-call-3
-export const OPEN_WEATHER_MAP_API = {
-  KEY: import.meta.env.VITE_OPEN_WEATHER_MAP_API_KEY,
-  URL: "http://api.openweathermap.org",
-};
-
 // NOTE: See docs at https://www.weatherapi.com/docs/
 export const WEATHER_API = {
   KEY: import.meta.env.VITE_WEATHER_API_KEY,
