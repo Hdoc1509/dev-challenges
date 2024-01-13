f68315482a75fd0f715f7246ab5d4cbd340af106
hdoc <hector.ochoa.dev@gmail.com>
2024-01-13 17:48:23 -0500

    refactor(weather-app): move weather details component to another file

--- apps/weather-app/src/components/CityWeather.tsx
+++ apps/weather-app/src/components/CityWeather.tsx
@@ -1,50 +1,9 @@
 import Skeleton from "react-loading-skeleton";
-import { Button, ButtonIcon } from "@hdoc/react-button";
-import { Icon } from "@hdoc/react-material-icons";
 import { useWeatherStore } from "../store/weather";
-import { getCurrentDate } from "../utils/date";
 import { getWeatherIcon } from "../utils/icons";
-import type { Weather } from "../schemas/weather";
+import { Button, ButtonIcon } from "@hdoc/react-button";
+import { WeatherDetails } from "./WeatherDetails";
 import "./CityWeather.scss";
-
-const WeatherDetails = ({ weather }: { weather: Weather | null }) => {
-  const temperatureUnit = useWeatherStore((s) => s.temperatureUnit);
-  const current = weather?.current;
-  const location = weather?.location;
-
-  return (
-    <>
-      <p className="weather__degree">
-        {current == null ? (
-          <Skeleton />
-        ) : (
-          <>
-            {current.temperature[temperatureUnit]}
-            <span className="weather__degree-unit">
-              °{temperatureUnit[0].toUpperCase()}
-            </span>
-          </>
-        )}
-      </p>
-      <p className="weather__description">
-        {current?.condition.name ?? <Skeleton />}
-      </p>
-      <p className="weather__date">
-        {current == null ? <Skeleton /> : `Today • ${getCurrentDate()}`}
-      </p>
-      <p className="weather__location">
-        {location == null ? (
-          <Skeleton />
-        ) : (
-          <>
-            <Icon name="location_on" />
-            {location.name}, {location.country}
-          </>
-        )}
-      </p>
-    </>
-  );
-};
 
 type Props = {
   getCurrentLocationWeather: () => void;
---
+++ apps/weather-app/src/components/WeatherDetails.tsx
@@ -0,0 +1,45 @@
+import { useWeatherStore } from "../store/weather";
+import { getCurrentDate } from "../utils/date";
+import Skeleton from "react-loading-skeleton";
+import { Icon } from "@hdoc/react-material-icons";
+import type { Weather } from "../schemas/weather";
+import "./WeatherDetails.scss";
+
+export const WeatherDetails = ({ weather }: { weather: Weather | null }) => {
+  const temperatureUnit = useWeatherStore((s) => s.temperatureUnit);
+  const current = weather?.current;
+  const location = weather?.location;
+
+  return (
+    <>
+      <p className="weather__degree">
+        {current == null ? (
+          <Skeleton />
+        ) : (
+          <>
+            {current.temperature[temperatureUnit]}
+            <span className="weather__degree-unit">
+              °{temperatureUnit[0].toUpperCase()}
+            </span>
+          </>
+        )}
+      </p>
+      <p className="weather__description">
+        {current?.condition.name ?? <Skeleton />}
+      </p>
+      <p className="weather__date">
+        {current == null ? <Skeleton /> : `Today • ${getCurrentDate()}`}
+      </p>
+      <p className="weather__location">
+        {location == null ? (
+          <Skeleton />
+        ) : (
+          <>
+            <Icon name="location_on" />
+            {location.name}, {location.country}
+          </>
+        )}
+      </p>
+    </>
+  );
+};
