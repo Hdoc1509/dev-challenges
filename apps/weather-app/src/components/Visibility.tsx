b7657bfe4e9d0ecb2bdc5b149a3369330624c9fc
hdoc <hector.ochoa.dev@gmail.com>
2024-01-13 18:58:46 -0500

    feat(weather-app): add skeleton for visibility info

--- apps/weather-app/src/components/Visibility.tsx
+++ apps/weather-app/src/components/Visibility.tsx
@@ -1,10 +1,20 @@
-import './Visibility.scss';
+import Skeleton from "react-loading-skeleton";
+import "./Visibility.scss";
 
 export const Visibility = ({ visibility }: { visibility?: number }) => {
   return (
     <section className="visibility">
       <h3>Visibility</h3>
-      <p className="visibility__distance">{visibility}</p>
+      <p className="visibility__distance">
+        {visibility == null ? (
+          <Skeleton />
+        ) : (
+          <>
+            {visibility}
+            <span className="visibility__distance-unit"> miles</span>
+          </>
+        )}
+      </p>
     </section>
   );
 };
