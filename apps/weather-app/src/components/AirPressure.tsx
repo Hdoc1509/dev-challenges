fe15e6b758c8ec38f0c6b2ca560da557120300f5
hdoc <hector.ochoa.dev@gmail.com>
2024-01-13 19:02:17 -0500

    feat(weather-app): add skeleton for air pressure info

--- apps/weather-app/src/components/AirPressure.tsx
+++ apps/weather-app/src/components/AirPressure.tsx
@@ -1,10 +1,20 @@
+import Skeleton from "react-loading-skeleton";
 import "./AirPressure.scss";
 
 export const AirPressure = ({ airPressure }: { airPressure?: number }) => {
   return (
     <section className="air-pressure">
       <h3>Air Pressure</h3>
-      <p className="air-pressure__pressure">{airPressure}</p>
+      <p className="air-pressure__pressure">
+        {airPressure == null ? (
+          <Skeleton />
+        ) : (
+          <>
+            {airPressure}
+            <span className="air-pressure__pressure-unit"> mb</span>
+          </>
+        )}
+      </p>
     </section>
   );
 };
