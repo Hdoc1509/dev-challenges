3fca29fb34e876e0e1e6403837304654d15bc823
hdoc <hector.ochoa.dev@gmail.com>
2024-01-16 21:42:37 -0500

    build(weather-app): import only filled variant of icons font

--- apps/weather-app/src/main.tsx
+++ apps/weather-app/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@material-design-icons/font";
-import "./index.css";
+import "@material-design-icons/font/filled.css";
 import "react-loading-skeleton/dist/skeleton.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
