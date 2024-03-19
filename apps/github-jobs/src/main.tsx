6022d6c79be99b610b56894410151062540c9b70
hdoc <hector.ochoa.dev@gmail.com>
2024-03-18 21:34:35 -0500

    feat(github-jobs): improve style of search options

--- apps/github-jobs/src/main.tsx
+++ apps/github-jobs/src/main.tsx
@@ -6,7 +6,9 @@ import "@fontsource/roboto/latin-400.css";
 import "@fontsource/roboto/latin-500.css";
 import "@fontsource/roboto/latin-700.css";
 import "@fontsource-variable/montserrat"; // Supports weights 100-900
-import "@fontsource/poppins";
+import "@fontsource/poppins/latin-400.css";
+import "@fontsource/poppins/latin-500.css";
+import "@fontsource/poppins/latin-700.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
