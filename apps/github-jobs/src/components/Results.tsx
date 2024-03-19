1bc31d256cd2cc8f817358efaa477ad5047a1d49
hdoc <hector.ochoa.dev@gmail.com>
2024-03-18 22:39:32 -0500

    feat(github-jobs): add pagination component

--- apps/github-jobs/src/components/Results.tsx
+++ apps/github-jobs/src/components/Results.tsx
@@ -1,6 +1,8 @@
+import ReactPaginate from "react-paginate";
 import { type Job } from "../services/jobs";
 import { JobCard } from "./JobCard";
 import "./Results.scss";
+import { Icon } from "@hdoc-react/material-icons";
 
 type Props = {
   jobs: Job[];
@@ -12,6 +14,14 @@ export const Results = ({ jobs }: Props) => {
       {jobs.map((job) => (
         <JobCard key={job.id} job={job} />
       ))}
+      <ReactPaginate
+        className="jobs-pagination"
+        breakLabel="..."
+        nextLabel={<Icon name="keyboard_arrow_right" />}
+        previousLabel={<Icon name="keyboard_arrow_left" />}
+        pageCount={10}
+        marginPagesDisplayed={1}
+      />
     </main>
   );
 };
