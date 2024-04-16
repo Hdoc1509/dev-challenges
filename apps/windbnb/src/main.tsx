816019ef7766ada575178d5a123ddff8367ea076
hdoc <hector.ochoa.dev@gmail.com>
2024-04-15 21:17:32 -0500

    feat(windbnb): use fonts from `fontsource`

--- apps/windbnb/src/main.tsx
+++ apps/windbnb/src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@material-design-icons/font/filled.css";
 import "@material-design-icons/font/round.css";
+import "@fontsource-variable/montserrat";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource-variable/mulish";
+import "@fontsource/mulish/400.css";
+import "@fontsource/mulish/700.css";
+import "@fontsource/mulish/800.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
