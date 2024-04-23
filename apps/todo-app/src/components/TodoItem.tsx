6bf7c4980022157d9148301078084a52b50d5515
hdoc <hector.ochoa.dev@gmail.com>
2024-04-22 22:04:52 -0500

    refactor(todo-app): use `<Button />` from `@hrc/button` for delete one task

--- apps/todo-app/src/components/TodoItem.tsx
+++ apps/todo-app/src/components/TodoItem.tsx
@@ -1,4 +1,5 @@
 import { clsx } from "clsx";
+import { Button } from "@hrc/button";
 import { Icon } from "@hrc/material-icons";
 import { Checkbox } from "@hrc/input";
 import type { Todo } from "../types";
@@ -30,9 +31,9 @@ export const TodoItem = ({ todo, onToggle, onRemove }: Props) => {
         color="primary"
       />
       {completed && (
-        <span className="todo-item__delete" onClick={onRemove}>
+        <Button variant="text" onClick={onRemove} fullRounded>
           <Icon name="delete" variant="outlined" />
-        </span>
+        </Button>
       )}
     </li>
   );
