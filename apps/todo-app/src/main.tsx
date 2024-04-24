b21d73eca203997dd3d88e21e406fbcc4121ea3c
hdoc <hector.ochoa.dev@gmail.com>
2024-04-23 21:54:15 -0500

    refactor(todo-app): use fonts from `fontsource`

--- apps/todo-app/src/main.tsx
+++ apps/todo-app/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@material-design-icons/font/filled.css";
 import "@material-design-icons/font/outlined.css";
+import "@fontsource-variable/montserrat";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource-variable/raleway";
+import "@fontsource/raleway/700.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
