e789c6e5e0fb6fb4f78865fd2540395be0b27e92
hdoc <hector.ochoa.dev@gmail.com>
2024-04-26 21:58:46 -0500

    refactor(quote-generator): use fonts from `fontsource`

--- apps/random-quote-generator/src/main.tsx
+++ apps/random-quote-generator/src/main.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@material-design-icons/font/filled.css";
+import "@fontsource-variable/montserrat";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource-variable/raleway";
+import "@fontsource/raleway/500.css";
+import "@fontsource/raleway/700.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
