338513f1e85541ed2185e856eeb3bfaf309d8b17
hdoc <hector.ochoa.dev@gmail.com>
2024-04-27 12:16:59 -0500

    refactor(country-quiz): use fonts from `fontsource`

--- apps/country-quiz/src/main.tsx
+++ apps/country-quiz/src/main.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@material-design-icons/font/outlined.css";
+import "@fontsource-variable/montserrat";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
