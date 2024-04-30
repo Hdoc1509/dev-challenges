799039eac0a14477fffaadab3f76883fa1263caf
hdoc <hector.ochoa.dev@gmail.com>
2024-04-30 18:01:25 -0500

    feat(windbnb): add vite aliases

--- apps/windbnb/vite.config.ts
+++ apps/windbnb/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
+  resolve: {
+    alias: {
+      "@scss": resolve(__dirname, "src/scss")
+    }
+  }
 })
