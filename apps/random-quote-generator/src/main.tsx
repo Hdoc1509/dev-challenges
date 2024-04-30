126590229d4b6ceceb46ccd9a1ab0ac5bddec728
hdoc <hector.ochoa.dev@gmail.com>
2024-04-30 17:17:21 -0500

    refactor(quote-generator): use css variables and sass mixins from `@lib/sass-utils`

--- apps/random-quote-generator/src/main.tsx
+++ apps/random-quote-generator/src/main.tsx
@@ -8,7 +8,7 @@ import "@fontsource/montserrat/700.css";
 import "@fontsource-variable/raleway";
 import "@fontsource/raleway/500.css";
 import "@fontsource/raleway/700.css";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
