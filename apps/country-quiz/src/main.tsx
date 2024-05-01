2610703a9d89d14d8dfed417d7d242912898cdb0
hdoc <hector.ochoa.dev@gmail.com>
2024-04-30 19:00:30 -0500

    refactor(country-quiz): use css variables and sass mixins from `@lib/sass-utils`

--- apps/country-quiz/src/main.tsx
+++ apps/country-quiz/src/main.tsx
@@ -9,7 +9,7 @@ import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
