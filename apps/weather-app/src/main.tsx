20bfeb75fe5cea43338a4b06307ec0e2d1ffe9be
hdoc <hector.ochoa.dev@gmail.com>
2024-04-30 21:43:09 -0500

    refactor(weather-app): use css variables from `@lib/sass-utils`

--- apps/weather-app/src/main.tsx
+++ apps/weather-app/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@material-design-icons/font/filled.css";
 import "react-loading-skeleton/dist/skeleton.css";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
