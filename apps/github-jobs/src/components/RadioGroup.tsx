3ac8d5d1858fe34e736c2eff9199ee1ff50a8741
hdoc <hector.ochoa.dev@gmail.com>
2024-05-02 18:00:12 -0500

    feat(github-jobs): add internal component `<Radio />`

--- apps/github-jobs/src/components/RadioGroup.tsx
+++ apps/github-jobs/src/components/RadioGroup.tsx
@@ -1,6 +1,27 @@
 import { RadioChecked, RadioUnchecked } from "./Icons";
 import "./RadioGroup.scss";
 
+type RadioProps = {
+  label: string;
+  icon?: React.ReactNode;
+  iconChecked?: React.ReactNode;
+} & Omit<React.ComponentProps<"input">, "size" | "color" | "type">;
+
+const Radio = ({ label, icon, iconChecked, ...restProps }: RadioProps) => {
+  return (
+    <label className="label label--radio">
+      <span className="radio">
+        <input {...restProps} className="radio__inner" type="radio" />
+        <span className="radio__icon">
+          {icon ?? <RadioUnchecked />}
+          {iconChecked ?? <RadioChecked />}
+        </span>
+      </span>
+      {label}
+    </label>
+  );
+};
+
 type Props<T> = {
   options: T[];
   name: string;
@@ -17,22 +38,13 @@ export const RadioGroup = <T extends string>({
   return (
     <div className="radio-group">
       {options.map((label) => (
-        <label key={label}>
-          <div className="radio">
-            <input
-              value={label}
-              type="radio"
-              name={name}
-              form={form}
-              defaultChecked={label === defaultValue}
-            />
-            <span className="radio__icon">
-              <RadioUnchecked />
-              <RadioChecked />
-            </span>
-          </div>
-          {label}
-        </label>
+        <Radio
+          key={label}
+          label={label}
+          name={name}
+          form={form}
+          defaultChecked={label === defaultValue}
+        />
       ))}
     </div>
   );
