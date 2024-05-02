03ed5492abf518d382d61369ff74bb0e890705fa
hdoc <hector.ochoa.dev@gmail.com>
2024-05-02 18:24:59 -0500

    refactor(github-jobs): destructure job object in `<JobCard />`

--- apps/github-jobs/src/components/JobCard.tsx
+++ apps/github-jobs/src/components/JobCard.tsx
@@ -3,23 +3,25 @@ import type { Job } from "../services/jobs";
 import "./JobCard.scss";
 
 export const JobCard = ({ job }: { job: Job }) => {
+  const { thumbnail, company, title, isFullTime, location, createdAt } = job;
+
   return (
     <div className="job-card">
       <div className="job-card__image">
-        {job.thumbnail ? (
-          <img src={job.thumbnail} alt={`${job.company} logo`} />
+        {thumbnail ? (
+          <img src={thumbnail} alt={`${company} logo`} />
         ) : (
           <p className="job-card__no-image">not found</p>
         )}
       </div>
-      <p className="job-card__company">{job.company}</p>
-      <p className="job-card__title">{job.title}</p>
-      {job.isFullTime && <p className="job-card__schedule-type">Full time</p>}
+      <p className="job-card__company">{company}</p>
+      <p className="job-card__title">{title}</p>
+      {isFullTime && <p className="job-card__schedule-type">Full time</p>}
       <p className="job-card__location">
-        <Icon name="public" /> {job.location}
+        <Icon name="public" /> {location}
       </p>
       <p className="job-card__created">
-        <Icon name="access_time" /> {job.createdAt ?? "Unkown date"}
+        <Icon name="access_time" /> {createdAt ?? "Unkown date"}
       </p>
     </div>
   );
