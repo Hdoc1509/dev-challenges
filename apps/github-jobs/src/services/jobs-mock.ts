287ab55a47f2820a0d6e3f30694ed0be21ec82ec
hdoc <hector.ochoa.dev@gmail.com>
2024-05-08 11:28:39 -0500

    refactor(github-jobs): improve readability of filter in mocked jobs service

--- apps/github-jobs/src/services/jobs-mock.ts
+++ apps/github-jobs/src/services/jobs-mock.ts
@@ -5,21 +5,19 @@ import type { JobService } from "./jobs";
 export const getJobs: JobService = (query, options = {}) => {
   // NOTE: zip code will be omitted here as it's not available in mock
   const { location } = options;
-
   const jobs = parseJobs(jobsMock);
+
   const filtered = jobs.filter((job) => {
-    let match = false;
+    const queryMatch =
+      query != null
+        ? job.title.toLowerCase().includes(query.toLowerCase())
+        : true;
+    const locationMatch =
+      location != null
+        ? job.location.toLowerCase().includes(location.toLowerCase())
+        : job.location.match(/new york|\sny/i) != null;
 
-    if (location != null) {
-      match = job.location.toLowerCase().includes(location.toLowerCase());
-    } else {
-      match = job.location.match(/new york|\sny/i) != null;
-    }
-
-    if (query != null)
-      match = job.title.toLowerCase().includes(query.toLowerCase());
-
-    return match;
+    return queryMatch && locationMatch;
   });
 
   return Promise.resolve([null, filtered]);
