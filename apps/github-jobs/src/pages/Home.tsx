5ab1a57c70f944a26fd35d42552a25033309d993
hdoc <hector.ochoa.dev@gmail.com>
2024-05-15 11:11:48 -0500

    feat(github-jobs): hide pagination when there are no results

--- apps/github-jobs/src/pages/Home.tsx
+++ apps/github-jobs/src/pages/Home.tsx
@@ -1,16 +1,19 @@
+import { useJobsStore } from "../store/jobs";
 import { SearchForm } from "../components/SearchForm";
 import { SearchOptions } from "../components/SearchOptions";
 import { Results } from "../components/Results";
 import { Pagination } from "../components/Pagination";
 
 export const Home = () => {
+  const jobs = useJobsStore((s) => s.jobs);
+
   return (
     <div className="home">
       <SearchForm />
       <SearchOptions />
       <main>
         <Results />
-        <Pagination />
+        {jobs.length > 0 && <Pagination />}
       </main>
     </div>
   );
