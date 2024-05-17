a29d3c5171167c8687878e630f36adcda098b2ec
hdoc <hector.ochoa.dev@gmail.com>
2024-05-16 21:49:57 -0500

    refactor(button-component): simplify structure of main content

--- apps/button-component/src/App.tsx
+++ apps/button-component/src/App.tsx
@@ -10,65 +10,63 @@ function App() {
     <>
       <Header title="Buttons" />
       <main>
-        <section className="buttons-grid">
-          <ButtonVariant name="Default">
-            <Button />
-          </ButtonVariant>
-          <ButtonVariant name="Outline">
-            <Button variant="outline" />
-          </ButtonVariant>
-          <ButtonVariant name="Text">
-            <Button variant="text" />
-          </ButtonVariant>
-          <ButtonVariant name="Disable shadow">
-            <Button disableShadow />
-          </ButtonVariant>
-          <ButtonVariant name="Disabled">
-            <Button disabled />
-          </ButtonVariant>
-          <ButtonVariant name="Text disabled">
-            <Button variant="text" disabled />
-          </ButtonVariant>
-          <ButtonVariant name="Start icon">
-            <Button startIcon="add_shopping_cart" />
-          </ButtonVariant>
-          <ButtonVariant name="End icon">
-            <Button endIcon="add_shopping_cart" />
-          </ButtonVariant>
-          <ButtonVariant name="End icon - outlined">
-            <Button endIcon={buttonIcon} iconStyle="outlined" />
-          </ButtonVariant>
-          <ButtonVariant name="End icon - round">
-            <Button endIcon={buttonIcon} iconStyle="round" />
-          </ButtonVariant>
-          <ButtonVariant name="End icon - sharp">
-            <Button endIcon={buttonIcon} iconStyle="sharp" />
-          </ButtonVariant>
-          <ButtonVariant name="End icon - tow-tone">
-            <Button endIcon={buttonIcon} iconStyle="two-tone" />
-          </ButtonVariant>
-          <ButtonVariant name="Small (sm)">
-            <Button size="sm" />
-          </ButtonVariant>
-          <ButtonVariant name="Medium (md)">
-            <Button size="md" />
-          </ButtonVariant>
-          <ButtonVariant name="Large (lg)">
-            <Button size="lg" />
-          </ButtonVariant>
-          <ButtonVariant name="Color default">
-            <Button color="default" />
-          </ButtonVariant>
-          <ButtonVariant name="Color primary">
-            <Button color="primary" />
-          </ButtonVariant>
-          <ButtonVariant name="Color secondary">
-            <Button color="secondary" />
-          </ButtonVariant>
-          <ButtonVariant name="Color danger">
-            <Button color="danger" />
-          </ButtonVariant>
-        </section>
+        <ButtonVariant name="Default">
+          <Button />
+        </ButtonVariant>
+        <ButtonVariant name="Outline">
+          <Button variant="outline" />
+        </ButtonVariant>
+        <ButtonVariant name="Text">
+          <Button variant="text" />
+        </ButtonVariant>
+        <ButtonVariant name="Disable shadow">
+          <Button disableShadow />
+        </ButtonVariant>
+        <ButtonVariant name="Disabled">
+          <Button disabled />
+        </ButtonVariant>
+        <ButtonVariant name="Text disabled">
+          <Button variant="text" disabled />
+        </ButtonVariant>
+        <ButtonVariant name="Start icon">
+          <Button startIcon="add_shopping_cart" />
+        </ButtonVariant>
+        <ButtonVariant name="End icon">
+          <Button endIcon="add_shopping_cart" />
+        </ButtonVariant>
+        <ButtonVariant name="End icon - outlined">
+          <Button endIcon={buttonIcon} iconStyle="outlined" />
+        </ButtonVariant>
+        <ButtonVariant name="End icon - round">
+          <Button endIcon={buttonIcon} iconStyle="round" />
+        </ButtonVariant>
+        <ButtonVariant name="End icon - sharp">
+          <Button endIcon={buttonIcon} iconStyle="sharp" />
+        </ButtonVariant>
+        <ButtonVariant name="End icon - tow-tone">
+          <Button endIcon={buttonIcon} iconStyle="two-tone" />
+        </ButtonVariant>
+        <ButtonVariant name="Small (sm)">
+          <Button size="sm" />
+        </ButtonVariant>
+        <ButtonVariant name="Medium (md)">
+          <Button size="md" />
+        </ButtonVariant>
+        <ButtonVariant name="Large (lg)">
+          <Button size="lg" />
+        </ButtonVariant>
+        <ButtonVariant name="Color default">
+          <Button color="default" />
+        </ButtonVariant>
+        <ButtonVariant name="Color primary">
+          <Button color="primary" />
+        </ButtonVariant>
+        <ButtonVariant name="Color secondary">
+          <Button color="secondary" />
+        </ButtonVariant>
+        <ButtonVariant name="Color danger">
+          <Button color="danger" />
+        </ButtonVariant>
       </main>
       <Footer />
     </>
