fc1cc790197b43379e8909584a2533e7e5bad851
hdoc <hector.ochoa.dev@gmail.com>
2024-05-21 21:20:54 -0500

    feat(weather-app): add general types

---
+++ legacy/weather-app/src/types.ts
@@ -0,0 +1,26 @@
+export type Weather = {
+  location: {
+    name: string;
+    country: string;
+  };
+  current: {
+    temperature: {
+      celsius: number;
+      fahrenheit: number;
+    };
+    condition: {
+      name: string;
+      code: number;
+    };
+    wind: {
+      speed: number;
+      direction: string;
+      directionDegree: number;
+    };
+    humidity: number;
+    visibility: number;
+    airPressure: number;
+  };
+};
+
+export type Wind = Weather["current"]["wind"];
