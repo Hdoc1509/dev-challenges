597894062cd98ab79dc7daa4ed733052c2d3dec5
hdoc <hector.ochoa.dev@gmail.com>
2024-05-21 21:21:52 -0500

    feat(weather-app): add util to parse weather response

---
+++ legacy/weather-app/src/utils/weather.ts
@@ -0,0 +1,25 @@
+import type { WeatherResponse } from "@/schemas/weather";
+import type { Weather } from "@/types";
+
+export const parseWeather = (weather: WeatherResponse): Weather => {
+  const { location, current } = weather;
+
+  return {
+    location,
+    current: {
+      condition: current.condition,
+      temperature: {
+        celsius: current.temp_c,
+        fahrenheit: current.temp_f,
+      },
+      wind: {
+        speed: current.wind_mph,
+        direction: current.wind_dir,
+        directionDegree: current.wind_degree,
+      },
+      humidity: current.humidity,
+      visibility: current.vis_miles,
+      airPressure: current.pressure_mb,
+    },
+  };
+};
