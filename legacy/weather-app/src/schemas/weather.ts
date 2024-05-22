a901471e08b32d6394df8018d442ab47814597d8
hdoc <hector.ochoa.dev@gmail.com>
2024-05-21 21:16:53 -0500

    feat(weather-app): add weather schema

---
+++ legacy/weather-app/src/schemas/weather.ts
@@ -0,0 +1,55 @@
+import * as z from "zod";
+
+const LocationSchema = z
+  .object({
+    name: z.string(),
+    region: z.string(),
+    country: z.string(),
+    lat: z.number(),
+    lon: z.number(),
+    tz_id: z.string(),
+    localtime_epoch: z.number(),
+    localtime: z.string(),
+  })
+  .transform(({ name, country }) => ({ name, country }));
+
+const ConditionSchema = z
+  .object({
+    text: z.string(),
+    icon: z.string(),
+    code: z.number(),
+  })
+  .transform(({ text, code }) => ({ name: text, code }));
+
+const CurrentSchema = z.object({
+  last_updated_epoch: z.number(),
+  last_updated: z.string(),
+  temp_c: z.number(),
+  temp_f: z.number(),
+  is_day: z.number(),
+  condition: ConditionSchema,
+  wind_mph: z.number(),
+  wind_kph: z.number(),
+  wind_degree: z.number(),
+  wind_dir: z.string(),
+  pressure_mb: z.number(),
+  pressure_in: z.number(),
+  precip_mm: z.number(),
+  precip_in: z.number(),
+  humidity: z.number(),
+  cloud: z.number(),
+  feelslike_c: z.number(),
+  feelslike_f: z.number(),
+  vis_km: z.number(),
+  vis_miles: z.number(),
+  uv: z.number(),
+  gust_mph: z.number(),
+  gust_kph: z.number(),
+});
+
+export const WeatherResponseSchema = z.object({
+  location: LocationSchema,
+  current: CurrentSchema,
+});
+
+export type WeatherResponse = z.infer<typeof WeatherResponseSchema>;
