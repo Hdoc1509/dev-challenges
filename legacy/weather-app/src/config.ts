f4412fe9e328654b951093588fb9a0ea5bd4df94
hdoc <hector.ochoa.dev@gmail.com>
2024-05-21 21:36:52 -0500

    feat(weather-app): add env vars config file

---
+++ legacy/weather-app/src/config.ts
@@ -0,0 +1,10 @@
+// Documentation
+// https://www.weatherapi.com/docs/
+export const WEATHERAPI = {
+  KEY: import.meta.env.WEATHERAPI_API_KEY,
+  URL: "https://api.weatherapi.com/v1",
+}
+
+// Documentation
+// https://open-meteo.com/en/docs
+export const OPEN_METEO_API_URL = "https://api.open-meteo.com/v1";
