c50d8d286269c60d483827bc60ea9b4c33f5b8fc
hdoc <hector.ochoa.dev@gmail.com>
2024-05-22 21:37:24 -0500

    feat(weather-app): add server api route for weather

---
+++ legacy/weather-app/src/pages/api/weather.ts
@@ -0,0 +1,43 @@
+import { getWeather } from "@/services/weather";
+import { parseWeather } from "@/utils/weather";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+  const params = new URL(request.url).searchParams;
+  const latitude = params.get("lat");
+  const longitude = params.get("lon");
+
+  if (!latitude || !longitude)
+    return new Response(
+      JSON.stringify({
+        error: "Missing latitude or longitude",
+      }),
+      { status: 400 },
+    );
+
+  const parsedLatitude = Number(latitude);
+  const parsedLongitude = Number(longitude);
+
+  if (isNaN(parsedLatitude) || isNaN(parsedLongitude))
+    return new Response(
+      JSON.stringify({ error: "Invalid latitude or longitude" }),
+      { status: 400 },
+    );
+
+  const [weatherError, weather] = await getWeather({
+    latitude: parsedLatitude,
+    longitude: parsedLongitude,
+  });
+
+  if (weatherError)
+    return new Response(JSON.stringify({ error: weatherError.message }), {
+      status: 500,
+    });
+
+  return new Response(JSON.stringify(parseWeather(weather)), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
