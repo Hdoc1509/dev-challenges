4a1a8b43f6cd479694bac8338a0f02e3e50b49a8
hdoc <hector.ochoa.dev@gmail.com>
2024-05-24 10:54:55 -0500

    feat(weather-app): use weather store from vite-app version

---
+++ legacy/weather-app/src/store/weather.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import type { Weather } from "@/types";
+
+export type Forecast = {
+  day: string;
+  temperature: {
+    min: Weather["current"]["temperature"];
+    max: Weather["current"]["temperature"];
+  };
+  condition: {
+    code: number;
+    name: string;
+  };
+};
+
+export type TemperatureUnit = "fahrenheit" | "celsius";
+
+type State = {
+  temperatureUnit: TemperatureUnit;
+  weather: Weather | null;
+  forecast: Forecast[] | null;
+};
+
+type Action = {
+  setTemperatureUnit: (temperatureUnit: State["temperatureUnit"]) => void;
+  setWeather: (weather: Weather) => void;
+  setForecast: (forecast: Forecast[]) => void;
+  clearData: () => void;
+};
+
+export const useWeatherStore = create<State & Action>()((set) => ({
+  temperatureUnit: "celsius",
+  weather: null,
+  forecast: null,
+  setTemperatureUnit: (temperatureUnit: State["temperatureUnit"]) =>
+    set({ temperatureUnit }),
+  setWeather: (weather: Weather) => set({ weather }),
+  setForecast: (forecast: Forecast[]) => set({ forecast }),
+  clearData: () => set({ weather: null, forecast: null }),
+}));
