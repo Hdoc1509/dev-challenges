91dbe289a31bf6cbde9d6ffbc882b3bed0e1fa7e
hdoc <hector.ochoa.dev@gmail.com>
2024-05-25 18:41:27 -0500

    feat(weather-app): add util to parse search city results

--- legacy/weather-app/src/utils/geolocation.ts
+++ legacy/weather-app/src/utils/geolocation.ts
@@ -1,4 +1,5 @@
-import type { LocationCoords, PromiseWithError } from "@/types";
+import type { SearchCityResponse } from "@/schemas/geolocation";
+import type { City, LocationCoords, PromiseWithError } from "@/types";
 
 const ERROR = {
   PERMISSION_DENIED: 1,
@@ -32,4 +33,14 @@ export const getCurrentCoords = async (): PromiseWithError<LocationCoords> => {
       },
     );
   });
+};
+
+export const parseCities = (cities: SearchCityResponse): City[] => {
+  return cities.map((city) => ({
+    id: city.id,
+    name: city.name,
+    country: city.country,
+    latitude: city.lat,
+    longitude: city.lon,
+  }));
 };
