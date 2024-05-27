d6c281f861d4871331620d907ed059ae783ba61b
hdoc <hector.ochoa.dev@gmail.com>
2024-05-27 18:20:28 -0500

    refactor(weather-app): remove old vite-app

--- apps/weather-app/src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_OPEN_WEATHER_MAP_API_KEY: string;
-  readonly VITE_WEATHER_API_KEY: string;
-}
--- apps/weather-app/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
-  base: '/dev-challenges/weather-app/',
-})
