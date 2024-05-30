4c75f04f0a1296447d63c022224e66862f28b405
hdoc <hector.ochoa.dev@gmail.com>
2024-05-29 21:12:17 -0500

    feat(github-jobs): use jobs store from vite-app version

---
+++ legacy/github-jobs/src/store/jobs.ts
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import type { Job, SearchOptions } from "../types";
+
+type Status = "idle" | "error" | "success" | "loading";
+
+type State = {
+  jobs: Job[];
+  status: Status;
+  error?: Error;
+  searchQuery: string;
+  searchOptions: SearchOptions;
+  pages: number;
+};
+
+type Action = {
+  setJobs: (jobs: Job[]) => void;
+  setStatus: (status: Status) => void;
+  setError: (error: Error) => void;
+  setSearchQuery: (query: string) => void;
+  setSearchOptions: (options: SearchOptions) => void;
+  setPages: (pages: number) => void;
+};
+
+const initialState: State = {
+  jobs: [],
+  status: "idle",
+  error: undefined,
+  searchQuery: "",
+  searchOptions: {
+    fullTime: undefined,
+    location: undefined,
+    page: 0,
+  },
+  pages: 10,
+};
+
+export const useJobsStore = create<State & Action>()((set) => ({
+  ...initialState,
+
+  setJobs: (jobs: Job[]) => set({ jobs }),
+  setStatus: (status: Status) => set({ status }),
+  setError: (error: Error) => set({ error }),
+  setSearchQuery: (query: string) => set({ searchQuery: query }),
+  setSearchOptions: (options: SearchOptions) => set({ searchOptions: options }),
+  setPages: (pages: number) => set({ pages }),
+}));
