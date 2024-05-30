0d4bc300a4f14631b96d18a7bdf1912d8f390a00
hdoc <hector.ochoa.dev@gmail.com>
2024-05-29 20:59:47 -0500

    feat(github-jobs): add project config file

---
+++ legacy/github-jobs/src/config.ts
@@ -0,0 +1,18 @@
+const { SERPAPI_API_KEY, WEATHERAPI_API_KEY, DEV } = import.meta.env;
+
+// Documentation
+// - https://serpapi.com/google-jobs-api
+export const SERPAPI = {
+  KEY: SERPAPI_API_KEY,
+  URL: "https://serpapi.com",
+};
+
+// Documentation
+// - https://www.weatherapi.com/docs/
+export const WEATHERAPI = {
+  KEY: WEATHERAPI_API_KEY,
+  URL: "https://api.weatherapi.com/v1",
+};
+
+// https://docs.astro.build/en/guides/environment-variables/#default-environment-variables
+export const isDev = DEV;
--- legacy/github-jobs/src/env.d.ts
+++ legacy/github-jobs/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly WEATHERAPI_API_KEY: string;
+  readonly SERPAPI_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
