b072cda6724191d43976e1ee2ffe11343ea3ed53
hdoc <hector.ochoa.dev@gmail.com>
2024-06-01 12:45:49 -0500

    feat(github-jobs): add client geolocation service

---
+++ legacy/github-jobs/src/services/geolocation/client.ts
@@ -0,0 +1,31 @@
+import type { LocationResponse } from "@/schemas/geolocation";
+import type { LocationService } from "./types";
+
+export const searchLocation: LocationService = async (options) => {
+  const params = new URLSearchParams();
+
+  if ("coords" in options) {
+    const { latitude, longitude } = options.coords;
+    params.append("q", `${latitude},${longitude}`);
+  } else {
+    params.append("q", `${options.zipCode}`);
+  }
+
+  try {
+    const res = await fetch(`/api/geolocation?${params.toString()}`);
+
+    // NOTE: ALL VALIDATIONS are done on the SERVER
+    // NOTE: if server has an error it returns `{ error: string }`
+    const data = (await res.json()) as
+      | { error: string }
+      | LocationResponse[number];
+
+    if ("error" in data) return [new Error(data.error)];
+
+    return [null, data];
+  } catch (error) {
+    if (error instanceof Error) return [error];
+  }
+
+  return [new Error("An unknown error occurred while trying to get location")];
+};
