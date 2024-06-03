12d675adc1ca4f888712f00db18a263d568283fa
hdoc <hector.ochoa.dev@gmail.com>
2024-06-02 20:46:41 -0500

    feat(github-jobs): handle timeout of Geolocation API without internet connection

--- legacy/github-jobs/src/utils/geolocation.ts
+++ legacy/github-jobs/src/utils/geolocation.ts
@@ -6,6 +6,9 @@ const ERROR = {
   POSITION_UNAVAILABLE: 2,
   TIMEOUT: 3,
 };
+const MESSAGES = {
+  TIMEOUT: "The request to get user location timed out, please try again",
+};
 
 const getErrorMessage = (code: number) => {
   if (code === ERROR.PERMISSION_DENIED)
@@ -14,14 +17,16 @@ const getErrorMessage = (code: number) => {
   if (code === ERROR.POSITION_UNAVAILABLE)
     return "Location services are not available";
 
-  if (code === ERROR.TIMEOUT)
-    return "The request to get user location timed out, please try again";
+  if (code === ERROR.TIMEOUT) return MESSAGES.TIMEOUT;
 
   return "An unknown error occurred while trying to get your location";
 };
 
 const getCurrentCoords = (): PromiseWithError<LocationCoords> => {
   return new Promise((resolve) => {
+    // NOTE: it only happens in dev/preview mode without internet connection
+    setTimeout(() => resolve([new Error(MESSAGES.TIMEOUT)]), 5000);
+
     navigator.geolocation.getCurrentPosition(
       ({ coords }) => {
         const { latitude, longitude } = coords;
