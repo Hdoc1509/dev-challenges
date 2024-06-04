23838d0edac08fa7bab6b5ddd2d76d324a281b1c
hdoc <hector.ochoa.dev@gmail.com>
2024-06-04 11:21:43 -0500

    feat(country-quiz): setup path aliases

--- legacy/country-quiz/vite.config.ts
+++ legacy/country-quiz/vite.config.ts
@@ -1,8 +1,14 @@
+import path from 'path'
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
-  base: '/dev-challenges/legacy/country-quiz'
+  base: '/dev-challenges/legacy/country-quiz',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
