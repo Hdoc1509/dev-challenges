2e8c74058ee8de813a6869bcd22755e9091a9054
hdoc <hector.ochoa.dev@gmail.com>
2024-06-05 16:49:11 -0500

    refactor(country-quiz): remove unnecessary params from npoint countries service

--- legacy/country-quiz/src/services/countries/npoint.ts
+++ legacy/country-quiz/src/services/countries/npoint.ts
@@ -4,9 +4,7 @@ const API_URL = "https://api.npoint.io";
 const BIN_ID = "071b01607bee8b976c4e";
 
 export const getCountriesFromBin = async (): Promise<Country[]> => {
-  const params = new URLSearchParams({ fields: "name,flags,capital,region" });
-
-  const res = await fetch(`${API_URL}/${BIN_ID}?${params.toString()}`);
+  const res = await fetch(`${API_URL}/${BIN_ID}`);
 
   if (!res.ok) throw new Error("Failed to get countries data");
 
