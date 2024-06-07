65c8fa8614007cd08f34f2a671a4a5379f3ac43b
hdoc <hector.ochoa.dev@gmail.com>
2024-06-07 18:35:59 -0500

    fix: prevent eslint from linting vite.config.ts files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
