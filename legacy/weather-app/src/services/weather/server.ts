d23711357cd2fa609e491ac15ddec766ebebed76
hdoc <hector.ochoa.dev@gmail.com>
2024-06-08 18:35:01 -0500

    feat(weather-app): use `fetcher` in server weather service

--- legacy/weather-app/src/services/weather/server.ts
+++ legacy/weather-app/src/services/weather/server.ts
@@ -1,8 +1,9 @@
+import { ServiceError, fetcher } from "@lib/fetcher";
 import { WEATHERAPI } from "@/config";
 import { WeatherResponseSchema, type WeatherResponse } from "@/schemas/weather";
 import type { LocationCoords, PromiseWithError } from "@/types";
 
-const errorPrefix = "Weather service error";
+const WeatherError = new ServiceError("Weather");
 
 export const getWeather = async (
   coords: LocationCoords,
@@ -13,23 +14,15 @@ export const getWeather = async (
     key: WEATHERAPI.KEY,
   });
 
-  try {
-    const res = await fetch(
-      `${WEATHERAPI.URL}/current.json?${params.toString()}`,
-    );
+  const [error, data] = await fetcher(
+    `${WEATHERAPI.URL}/current.json?${params.toString()}`,
+    {
+      schema: WeatherResponseSchema,
+      serviceError: WeatherError,
+    },
+  );
 
-    if (!res.ok) return [new Error(`${errorPrefix}. Response error.`)];
+  if (error) return [error];
 
-    const parsedData = WeatherResponseSchema.safeParse(await res.json());
-
-    if (!parsedData.success) return [new Error(`${errorPrefix}. Invalid data`)];
-
-    return [null, parsedData.data];
-  } catch (error) {
-    if (error instanceof Error) return [error];
-  }
-
-  return [
-    new Error(`${errorPrefix}. Something went wrong. Please try again later.`),
-  ];
+  return [null, data];
 };
