c71ec3c897f42ec09e0ab81c07109d203b841e7e
hdoc <hector.ochoa.dev@gmail.com>
2024-06-09 17:22:33 -0500

    feat(weather-app): handle weatherapi error in server geolocation service

--- legacy/weather-app/src/services/geolocation/server.ts
+++ legacy/weather-app/src/services/geolocation/server.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { ServiceError, fetcher } from "@lib/fetcher";
 import {
   SearchCityResponseSchema,
@@ -6,6 +7,14 @@ import {
 import { WEATHERAPI } from "@/config";
 import type { PromiseWithError } from "@/types";
 
+// based on http://www.weatherapi.com/docs/#intro-error-codes
+const ErrorSchema = z.object({
+  error: z.object({
+    code: z.number(),
+    message: z.string(),
+  }),
+});
+const ResponseSchema = SearchCityResponseSchema.or(ErrorSchema);
 const SearchCityError = new ServiceError("Search city");
 const CITIES_LIMIT = "5";
 
@@ -21,12 +30,19 @@ export const searchCity = async (
   const [error, data] = await fetcher(
     `${WEATHERAPI.URL}/search.json?${params.toString()}`,
     {
-      schema: SearchCityResponseSchema,
+      schema: ResponseSchema,
       serviceError: SearchCityError,
+      checkStatus: false, // allows to read weatherapi endpoint errors in response
     },
   );
 
   if (error) return [error];
+
+  if ("error" in data) return [new Error(data.error.message)]; // api endpoint error
+
+  // if no results, response can succeed returning empty array
+  if (data.length === 0)
+    return [new Error(`No results found for: "${search}"`)];
 
   return [null, data];
 };
