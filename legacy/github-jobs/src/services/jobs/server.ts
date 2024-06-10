bf73c2520ceae5c3db0332b2e80648b4b83c138b
hdoc <hector.ochoa.dev@gmail.com>
2024-06-10 18:10:04 -0500

    feat(github-jobs): use `fetcher` in server jobs service

--- legacy/github-jobs/src/services/jobs/server.ts
+++ legacy/github-jobs/src/services/jobs/server.ts
@@ -1,8 +1,10 @@
+import { ServiceError, fetcher } from "@lib/fetcher";
 import { JobsResponseSchema, type JobsResponse } from "@/schemas/jobs";
-import { JobsResponseError } from "@/errors";
 import { SERPAPI } from "@/config";
 import locationsMock from "@/mocks/locations.json";
 import type { JobService } from "./types";
+
+const JobsError = new ServiceError("Jobs");
 
 export const getJobs: JobService<JobsResponse> = async (search) => {
   const { query, location, fullTime, page } = search;
@@ -11,7 +13,6 @@ export const getJobs: JobService<JobsResponse> = async (search) => {
     q: query,
     api_key: SERPAPI.KEY,
   });
-  const controller = new AbortController();
 
   // INFO: https://serpapi.com/google-jobs-api#api-parameters-pagination
   params.append("start", (page ? (page - 1) * 10 : 0).toString());
@@ -23,39 +24,19 @@ export const getJobs: JobService<JobsResponse> = async (search) => {
     params.append("chips", "employment_type:FULLTIME");
   }
 
-  setTimeout(() => controller.abort(), 5000);
+  const [error, data] = await fetcher(
+    `${SERPAPI.URL}/search.json?${params.toString()}`,
+    {
+      schema: JobsResponseSchema,
+      serviceError: JobsError,
+    },
+  );
 
-  try {
-    const res = await fetch(`${SERPAPI.URL}/search.json?${params.toString()}`, {
-      signal: controller.signal,
-    });
+  if (error) return [error];
 
-    if (!res.ok)
-      return [new JobsResponseError("Jobs service response error", res)];
+  // based on https://serpapi.com/searches/245e315c7524f950/6644d67d7690dc208bd21e49.json
+  if (data.search_information?.jobs_results_state === "Fully empty")
+    return [new Error(`No jobs found for: ${query}`)];
 
-    const parsedData = JobsResponseSchema.safeParse(await res.json());
-
-    if (!parsedData.success)
-      return [new Error("Jobs service data error. Invalid data")];
-
-    const { data } = parsedData;
-
-    // NOTE:
-    // based on https://serpapi.com/searches/245e315c7524f950/6644d67d7690dc208bd21e49.json
-    if (data.search_information?.jobs_results_state === "Fully empty")
-      return [new Error(`No jobs found for: ${query}`)];
-
-    return [null, data];
-  } catch (error) {
-    if (error instanceof Error) {
-      const { name } = error;
-
-      if (name === "AbortError")
-        return [new Error("Jobs service response timed out")];
-
-      return [error];
-    }
-  }
-
-  return [new Error("An unknown error occurred while trying to get jobs")];
+  return [null, data];
 };
