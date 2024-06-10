3ef0565229d2b79014bce20c96e1ff0e3f5a1e44
hdoc <hector.ochoa.dev@gmail.com>
2024-06-09 22:08:10 -0500

    feat(github-jobs): add location response schema

--- legacy/github-jobs/src/schemas/geolocation.ts
+++ legacy/github-jobs/src/schemas/geolocation.ts
@@ -1,16 +1,17 @@
 import * as z from "zod";
 
-export const SearchLocationResponseSchema = z.array(
-  z.object({
-    id: z.number(),
-    name: z.string(),
-    region: z.string(),
-    country: z.string(),
-    lat: z.number(),
-    lon: z.number(),
-    url: z.string(),
-  }),
-);
+export const LocationResponseSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  region: z.string(),
+  country: z.string(),
+  lat: z.number(),
+  lon: z.number(),
+  url: z.string(),
+});
+export type LocationResponse = z.infer<typeof LocationResponseSchema>;
+
+export const SearchLocationResponseSchema = z.array(LocationResponseSchema);
 export type SearchLocationResponse = z.infer<
   typeof SearchLocationResponseSchema
 >;
