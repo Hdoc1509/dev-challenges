25de47e9a9cbe5c7688d288b8daee1acdb2c607d
hdoc <hector.ochoa.dev@gmail.com>
2024-06-11 11:15:54 -0500

    refactor(github-jobs): simplify eslint config
    it inherits from root eslint config

--- legacy/github-jobs/.eslintrc.cjs
+++ legacy/github-jobs/.eslintrc.cjs
@@ -1,25 +1,17 @@
 module.exports = {
-  extends: ["plugin:astro/recommended"],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
     project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
   },
   overrides: [
     {
       files: ["*.astro"],
+      extends: ["plugin:astro/recommended"],
       parser: "astro-eslint-parser",
       parserOptions: {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
-    },
-    {
-      files: ["*.ts", "*.tsx"],
-      extends: ["plugin:@typescript-eslint/recommended-type-checked"],
     },
   ],
 };
