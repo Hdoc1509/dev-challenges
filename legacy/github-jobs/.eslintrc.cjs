45ef82edd8d864c4c8a2b9e0372eb87d8dfc68e9
hdoc <hector.ochoa.dev@gmail.com>
2024-06-10 22:42:33 -0500

    feat(github-jobs): set eslint config correctlty

--- legacy/github-jobs/.eslintrc.cjs
+++ legacy/github-jobs/.eslintrc.cjs
@@ -1,9 +1,15 @@
+// TODO: try https://github.com/kaje94/astro-react-ts-eslint-starter/tree/main
 module.exports = {
   root: true,
-  extends: [
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:astro/recommended",
-  ],
+  extends: ["plugin:astro/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
   overrides: [
     {
       files: ["*.astro"],
@@ -12,6 +18,10 @@ module.exports = {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
+    },
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: ["plugin:@typescript-eslint/recommended-type-checked"],
     },
   ],
 };
