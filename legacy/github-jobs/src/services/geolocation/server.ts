7b1ef18a3f431e220b57fef0b517ddbdb38c389b
hdoc <hector.ochoa.dev@gmail.com>
2024-06-10 19:07:08 -0500

    feat(github-jobs): handle weatherapi errors in server geolocation service

--- legacy/github-jobs/src/services/geolocation/server.ts
+++ legacy/github-jobs/src/services/geolocation/server.ts
@@ -1,11 +1,20 @@
+import { z } from "zod";
 import { ServiceError, fetcher, type PromiseWithError } from "@lib/fetcher";
-import { WEATHERAPI } from "@/config";
 import {
   SearchLocationResponseSchema,
   type LocationResponse,
 } from "@/schemas/geolocation";
+import { WEATHERAPI } from "@/config";
 import type { LocationOptions } from "@/types";
 
+// based on http://www.weatherapi.com/docs/#intro-error-codes
+const ErrorSchema = z.object({
+  error: z.object({
+    code: z.number(),
+    message: z.string(),
+  }),
+});
+const ApiResponseSchema = SearchLocationResponseSchema.or(ErrorSchema);
 const GeolocationError = new ServiceError("Geolocation");
 const LOCATIONS_LIMIT = "1";
 
@@ -27,12 +36,20 @@ export const searchLocation = async (
   const [error, data] = await fetcher(
     `${WEATHERAPI.URL}/search.json?${params.toString()}`,
     {
-      schema: SearchLocationResponseSchema,
+      schema: ApiResponseSchema,
       serviceError: GeolocationError,
+      checkStatus: false, // allows to read weatherapi endpoint errors in response
     },
   );
 
   if (error) return [error];
+
+  if ("error" in data) return [new Error(data.error.message)];
+
+  // if no results, response can succeed returning empty array
+  // coordinates will always be valid because of Geolocation API
+  if (data.length === 0 && "zipCode" in options)
+    return [new Error(`No locations found for zip code: ${options.zipCode}`)];
 
   return [null, data[0]];
 };
