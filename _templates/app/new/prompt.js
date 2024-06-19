97df149ace0ddbc960ece71f745e7945c9ff40a0
hdoc <hector.ochoa.dev@gmail.com>
2024-06-18 22:40:33 -0500

    refactor(templates): add shared prompt

--- _templates/app/new/prompt.js
+++ _templates/app/new/prompt.js
@@ -1,12 +1,1 @@
-module.exports = [
-  {
-    type: "input",
-    name: "name",
-    message: "Project name:",
-  },
-  {
-    type: "input",
-    name: "challenge_url",
-    message: "Challenge URL:",
-  },
-];
+module.exports = require("../shared-prompt");
---
+++ _templates/app/shared-prompt.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    type: "input",
+    name: "name",
+    message: "Project name:",
+  },
+  {
+    type: "input",
+    name: "challenge_url",
+    message: "Challenge URL:",
+  },
+];
