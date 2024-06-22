3a9bc147b64d8f5b2df17deab17d488a7fff2105
hdoc <hector.ochoa.dev@gmail.com>
2024-06-22 12:31:52 -0500

    refactor(geolocation): use native constants of `GeolocationPositionError`

--- libs/geolocation/index.ts
+++ libs/geolocation/index.ts
@@ -3,27 +3,19 @@ export type LocationCoords = {
   longitude: number;
 };
 
+// https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPositionError
 const MESSAGES = {
-  TIMEOUT: "The request to get user location timed out, please try again.",
-};
-
-// https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPositionError/code
-const ERROR = {
-  PERMISSION_DENIED: 1,
-  POSITION_UNAVAILABLE: 2,
-  TIMEOUT: 3,
+  [GeolocationPositionError.TIMEOUT]:
+    "The request to get user location timed out, please try again by reloading the page",
+  [GeolocationPositionError.POSITION_UNAVAILABLE]:
+    "Location services are not available, please enable them in your browser or try again later",
+  [GeolocationPositionError.PERMISSION_DENIED]:
+    "Please enable location services, then reload the page",
+  UNKNOWN_ERROR: "An unknown error occurred while trying to get your location",
 };
 
 const getErrorMessage = (code: number) => {
-  if (code === ERROR.PERMISSION_DENIED)
-    return "Please enable location services, then reload the page";
-
-  if (code === ERROR.POSITION_UNAVAILABLE)
-    return "Location services are not available";
-
-  if (code === ERROR.TIMEOUT) return MESSAGES.TIMEOUT;
-
-  return "An unknown error occurred while trying to get your location";
+  return MESSAGES[code] ?? MESSAGES.UNKNOWN_ERROR;
 };
 
 export const getCurrentCoords = (
