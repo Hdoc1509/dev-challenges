1199f16aa037e9df9cd406f3e01ae1711d455104
hdoc <hector.ochoa.dev@gmail.com>
2024-07-27 12:25:44 -0500

    refactor(input-component): import only filled variant of icons font

--- legacy/input-component/src/main.tsx
+++ legacy/input-component/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@material-design-icons/font";
+import "@material-design-icons/font/filled.css";
 import '@fontsource-variable/montserrat'
 import '@fontsource/montserrat/500.css'
 import '@fontsource/montserrat/700.css'
