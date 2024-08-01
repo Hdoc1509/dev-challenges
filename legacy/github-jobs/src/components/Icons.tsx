bf531669ec63012c968331650db3d555067b1e50
hdoc <hector.ochoa.dev@gmail.com>
2024-08-01 18:41:32 -0500

    feat(github-jobs): add `<Ellipsis />`

--- legacy/github-jobs/src/components/Icons.tsx
+++ legacy/github-jobs/src/components/Icons.tsx
@@ -13,3 +13,21 @@ export const RadioChecked = () => {
     </svg>
   );
 };
+
+export const Ellipsis = () => {
+  return (
+    <svg
+      focusable="false"
+      aria-hidden="true"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+    </svg>
+  );
+};
