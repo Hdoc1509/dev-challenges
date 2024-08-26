3d90b422f502a9b2acec724ec17c2a36391227ec
hdoc <hector.ochoa.dev@gmail.com>
2024-08-26 16:52:57 -0500

    feat(github-jobs): add hook `useJobs()`

---
+++ legacy/github-jobs/src/hooks/useJobs.ts
@@ -0,0 +1,65 @@
+import { useCallback } from "react";
+import { useJobsStore } from "@/store/jobs";
+import { getJobs } from "@/services/jobs/client";
+import { getMockedJobs } from "@/services/jobs/mock";
+import { getLocationOption } from "@/utils/geolocation";
+import { isDev } from "@/config";
+import type { Job, Search } from "@/types";
+import type { PromiseWithError } from "@lib/fetcher";
+
+type SearchReturn = PromiseWithError<{
+  jobs: Job[];
+  nextPageToken?: string;
+  usedLocation: string;
+}>;
+
+export function useJobs() {
+  const jobs = useJobsStore((s) => s.jobs);
+  const error = useJobsStore((s) => s.error);
+  const status = useJobsStore((s) => s.status);
+  const cacheJobs = useJobsStore((s) => s.cacheJobs);
+  const setJobs = useJobsStore((s) => s.setJobs);
+  const setStatus = useJobsStore((s) => s.setStatus);
+  const setError = useJobsStore((s) => s.setError);
+
+  const searchJobs = useCallback(async (search: Search): SearchReturn => {
+    setStatus("loading");
+
+    const [locationError, location] = await getLocationOption(search.location);
+
+    if (locationError) {
+      setError(locationError);
+      setStatus("error");
+      return [locationError];
+    }
+
+    const jobSearch = { ...search, location };
+
+    const [jobsError, jobsSearchResult] = await (isDev
+      ? getMockedJobs(jobSearch)
+      : getJobs(jobSearch));
+
+    if (jobsError) {
+      setError(jobsError);
+      setStatus("error");
+      return [jobsError];
+    }
+
+    const { jobs: jobsResult } = jobsSearchResult;
+
+    cacheJobs(jobsResult);
+    setJobs(jobsResult);
+    setStatus("success");
+
+    return [null, { ...jobsSearchResult, usedLocation: location }];
+  }, []);
+
+  return {
+    jobs,
+    jobsError: error,
+    jobsStatus: status,
+    searchJobs,
+    setJobsError: setError,
+    setJobsStatus: setStatus,
+  };
+}
