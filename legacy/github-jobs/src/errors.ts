42e84b45aaa7ed2f02a30531f4767aaf74040fb6
hdoc <hector.ochoa.dev@gmail.com>
2024-08-27 18:23:34 -0500

    refactor(github-jobs): remove unused response errors

--- legacy/github-jobs/src/errors.ts
+++ legacy/github-jobs/src/errors.ts
@@ -4,23 +4,3 @@ export class JobsEmptyResultsError extends Error {
     this.name = "JobsEmptyResultsError";
   }
 }
-
-export class JobsResponseError extends Error {
-  response: Response;
-
-  constructor(message: string, res: Response) {
-    super(message);
-    this.name = "JobsError";
-    this.response = res;
-  }
-}
-
-export class GeolocationResponseError extends Error {
-  respone: Response;
-
-  constructor(message: string, res: Response) {
-    super(message);
-    this.name = "GeolocationError";
-    this.respone = res;
-  }
-}
