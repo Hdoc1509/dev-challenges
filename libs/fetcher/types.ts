24edfde9bec717ab3c36910ea279f253dbb4d80e
hdoc <hector.ochoa.dev@gmail.com>
2024-08-27 21:22:39 -0500

    docs(fetcher): improve jsdoc of `checkStatus` option
    - can retrieve the response form the error with `ResponseError` class
    - add code examples

--- libs/fetcher/types.ts
+++ libs/fetcher/types.ts
@@ -11,8 +11,34 @@ export type Options<S> = {
   /**
    * Whether to return an error based on `response.ok`.
    * Useful if endpoint returns the error as a JSON in the response.
+   * **
+   * If `true`, you will be able to retrieve the response from the error by
+   * using `ResponseError` class.**
+   * If `false`, be sure to use an schema that matches the expected error format.**
    *
-   * **If `false`, be sure to use an schema that matches the expected error format.**
+   * @example
+   * import { ResponseError, fetcher } from "@lib/fetcher";
+   *
+   * const [error, data] = await fetcher(...);
+   *
+   * // if `checkStatus` is `true`
+   * if (error != null) {
+   *   if (error instanceof ResponseError) {
+   *     const { res } = error;
+   *     // do something with response
+   *   }
+   *
+   *   // do something with rest of errors
+   * }
+   *
+   * // if `checkStatus` is `false`
+   * if (error != null) // do something with error
+   *
+   * // the error can be retrieved from data
+   * const errorFromData = handleErrorFromData(data);
+   *
+   * if (errorFromData != null) // do something with error
+   *
    * @default true
    * */
   checkStatus?: boolean;
