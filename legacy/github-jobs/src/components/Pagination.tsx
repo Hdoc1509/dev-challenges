7f334620371a820ba7e56642f2005f98a15c0d68
hdoc <hector.ochoa.dev@gmail.com>
2024-08-28 10:49:21 -0500

    refactor(github-jobs): implement `useJobs()` hook in `<Pagination />`

--- legacy/github-jobs/src/components/Pagination.tsx
+++ legacy/github-jobs/src/components/Pagination.tsx
@@ -1,29 +1,23 @@
 import { useCallback } from "react";
 import { useJobsStore } from "@/store/jobs";
+import { useJobs } from "@/hooks/useJobs";
 import { useSearchStore, type StoreSearch } from "@/store/search";
-import { getJobs } from "@/services/jobs/client";
-import { getMockedJobs } from "@/services/jobs/mock";
 import { JobsEmptyResultsError } from "@/errors";
-import { getLocationOption } from "@/utils/geolocation";
 import { isSameSearch } from "@/utils/search";
 import ReactPaginate from "react-paginate";
 import { Icon } from "@hrc/material-icons";
 import { Ellipsis } from "./Icons";
-import { isDev } from "@/config";
 import "./Pagination.scss";
 
 export const Pagination = () => {
-  const cachedJobs = useJobsStore((s) => s.cachedJobs);
+  const { cachedJobs, searchJobs, setJobsError } = useJobs();
   const search = useSearchStore((s) => s.search);
   const lastSearch = useSearchStore((s) => s.lastSearch);
   const userLocation = useSearchStore((s) => s.userLocation);
   const pages = useSearchStore((s) => s.pages);
   const setJobs = useJobsStore((s) => s.setJobs);
-  const cacheJobs = useJobsStore((s) => s.cacheJobs);
-  const setError = useJobsStore((s) => s.setError);
   const setSearch = useSearchStore((s) => s.setSearch);
   const setLastSearch = useSearchStore((s) => s.setLastSearch);
-  const setStatus = useJobsStore((s) => s.setStatus);
   const setPages = useSearchStore((s) => s.setPages);
 
   const handlePageChange = useCallback(
@@ -37,13 +31,7 @@ export const Pagination = () => {
         return;
       }
 
-      setStatus("loading");
-
-      const newLocation =
-        search.location === "" ? userLocation : search.location;
-      const [locationError, location] = await getLocationOption(newLocation);
-
-      if (locationError) return setError(locationError);
+      const location = search.location === "" ? userLocation : search.location;
 
       const { nextPageToken } = search;
 
@@ -55,38 +43,33 @@ export const Pagination = () => {
         nextPageToken,
       };
 
-      const [jobsError, jobsResult] = await (isDev
-        ? getMockedJobs(newSearch)
-        : getJobs(newSearch));
+      const [searchError, searchResult] = await searchJobs(newSearch);
 
-      if (jobsError) {
-        if (jobsError instanceof JobsEmptyResultsError) {
+      if (searchError) {
+        if (searchError instanceof JobsEmptyResultsError) {
           setJobs([]);
           setPages(newPage + 1);
         }
 
-        return setError(jobsError);
+        return setJobsError(searchError);
       }
 
-      const { jobs, nextPageToken: newNextPageToken } = jobsResult;
+      const { nextPageToken: newNextPageToken, usedLocation } = searchResult;
 
-      setJobs(jobs);
-      cacheJobs(jobs);
       setSearch({ pageAsIndex: newPage, nextPageToken: newNextPageToken });
-      setLastSearch({ ...search, location });
+      setLastSearch({ ...search, location: usedLocation });
       setPages(newNextPageToken == null ? newPage + 1 : 10);
     },
     [
       search,
       cachedJobs,
       userLocation,
-      setStatus,
-      setError,
+      searchJobs,
       setJobs,
-      cacheJobs,
       setSearch,
       setLastSearch,
       setPages,
+      setJobsError,
     ],
   );
 
