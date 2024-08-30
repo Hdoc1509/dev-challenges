e6a046e8472c6ff9f25d4c86b2ebfe3dcc958e4f
hdoc <hector.ochoa.dev@gmail.com>
2024-08-30 18:02:56 -0500

    feat(github-jobs): implement remaining searches service in `<RemainingSearches />`

--- legacy/github-jobs/src/components/RemainingSearches.tsx
+++ legacy/github-jobs/src/components/RemainingSearches.tsx
@@ -1,33 +1,70 @@
+import { useCallback, useEffect, useState } from "react";
+import { getRemainingSearches } from "@/services/remaining-searches/client";
 import { Icon } from "@hrc/material-icons";
+import { RingSpinner } from "@hrc/spinner";
+import type { Status } from "@lib/fetcher";
 import "./RemainingSearches.scss";
 
-const remainingSearches = "??";
-
-// TODO: implement getRemainingSearches() client service
+let didInit = false;
 
 export function RemainingSearches() {
+  const [remainingSearches, setRemainingSearches] = useState<null | number>(
+    null,
+  );
+  const [status, setStatus] = useState<Status>("idle");
+  const [error, setError] = useState<null | Error>(null);
+
+  const getInitialRemainingSearches = useCallback(async () => {
+    setStatus("loading");
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const [error, result] = await getRemainingSearches();
+
+    if (error != null) {
+      setError(error);
+      setStatus("error");
+      return;
+    }
+
+    setStatus("success");
+    setRemainingSearches(result);
+  }, []);
+
+  useEffect(() => {
+    if (!didInit) {
+      didInit = true;
+      getInitialRemainingSearches();
+    }
+  }, [getInitialRemainingSearches]);
+
   return (
     <div className="remaining-searches bold">
       Remaining searches:
-      <span className="remaining-searches__count">{remainingSearches}</span>
+      <span className="remaining-searches__count">
+        {status === "loading" && <RingSpinner />}
+        {status === "error" && "??"}
+        {status === "success" && remainingSearches}
+      </span>
       <label className="tooltip">
         <input type="checkbox" className="tooltip__checkbox" hidden />
         <span className="tooltip__icon">
           <Icon name="help_outline" />
         </span>
         <span className="tooltip__content">
-          {/* This project uses the free tier of{" "} */}
-          {/* <a */}
-          {/*   href="https://serpapi.com/pricing" */}
-          {/*   target="_blank" */}
-          {/*   rel="noreferrer" */}
-          {/* > */}
-          {/*   SerpApi */}
-          {/*   <Icon name="open_in_new" /> */}
-          {/* </a> */}
-          Not implemented yet.
+          This project uses the free tier of{" "}
+          <a
+            href="https://serpapi.com/pricing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            SerpApi
+            <Icon name="open_in_new" />
+          </a>
         </span>
       </label>
+      {status === "error" && (
+        <span className="remaining-searches__error">{error?.message}</span>
+      )}
     </div>
   );
 }
