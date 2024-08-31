1b570820b9adc2f60ddf842fe92e41af3a98d9bb
hdoc <hector.ochoa.dev@gmail.com>
2024-08-31 12:10:52 -0500

    feat(github-jobs): check remaining searches in jobs api endpoint
    skip retrieving jobs from external API when there are no free searches
    left

--- legacy/github-jobs/src/pages/api/jobs.ts
+++ legacy/github-jobs/src/pages/api/jobs.ts
@@ -1,8 +1,14 @@
 import { getJobs } from "@/services/jobs/server";
+import { getRemainingSearches } from "@/services/remaining-searches/server";
 import { createResponseError, createResponseSuccess } from "@/utils/response";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ request }) => {
+  const [searchesError, remainingSearches] = await getRemainingSearches();
+
+  if (searchesError == null && remainingSearches.plan_searches_left === 0)
+    return createResponseError(400, "No free searches left");
+
   const params = new URL(request.url).searchParams;
 
   const query = params.get("q");
