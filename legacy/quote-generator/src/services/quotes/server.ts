2ac5ba842b6aaa497198553d2a000c87c4aaf490
hdoc <hector.ochoa.dev@gmail.com>
2024-09-04 17:30:46 -0500

    feat(quote-generator): add server service to retrieve author quotes

--- legacy/quote-generator/src/services/quotes/server.ts
+++ legacy/quote-generator/src/services/quotes/server.ts
@@ -1,5 +1,9 @@
 import { ServiceError, fetcher, type PromiseWithError } from "@lib/fetcher";
-import { QuotesResponseSchema, type QuoteResponse } from "./schema";
+import {
+  QuotesResponseSchema,
+  type QuoteResponse,
+  type QuotesResponse,
+} from "./schema";
 import { FAVQS_API } from "@/config";
 
 export const getRandomQuote = async (): PromiseWithError<QuoteResponse> => {
@@ -12,4 +16,26 @@ export const getRandomQuote = async (): PromiseWithError<QuoteResponse> => {
   if (error) return [error];
 
   return [null, data.quotes[0]];
+};
+
+type AuthorParamsOptions = { filter: string; type: "author" };
+
+export const getAuthorQuotes = async (
+  author: string,
+): PromiseWithError<QuotesResponse> => {
+  const paramsOptions: AuthorParamsOptions = { type: "author", filter: author };
+  const params = new URLSearchParams(paramsOptions);
+
+  const [error, data] = await fetcher(
+    `${FAVQS_API}/quotes?${params.toString()}`,
+    {
+      schema: QuotesResponseSchema,
+      serviceError: new ServiceError("Quotes"),
+      headers: FAVQS_API.HEADERS,
+    },
+  );
+
+  if (error) return [error];
+
+  return [null, data];
 };
