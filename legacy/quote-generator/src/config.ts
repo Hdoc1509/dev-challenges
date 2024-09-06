5d333ca9e51ada56c7965511de762c4b62a4a5c2
hdoc <hector.ochoa.dev@gmail.com>
2024-09-05 22:09:00 -0500

    refactor(quote-generator): remove hashmap of favqs api error codes
    messages does not contain sensitive information

--- legacy/quote-generator/src/config.ts
+++ legacy/quote-generator/src/config.ts
@@ -7,10 +7,4 @@ export const FAVQS_API = {
   HEADERS: {
     Authorization: `Token token=${FAVQS_API_KEY}`,
   },
-  // https://favqs.com/api#ErrorCodes
-  ERROR_CODE: {
-    INVALID_REQUEST: 10,
-    PERMISSION_DENIED: 11,
-    AUTHOR_NOT_FOUND: 50,
-  },
 };
