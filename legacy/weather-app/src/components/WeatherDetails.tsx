a4183af21b23f6b70dfde1d248a247dbb7340e05
hdoc <hector.ochoa.dev@gmail.com>
2024-09-12 11:52:55 -0500

    refactor(weather-app): implement `stringifyLocation()` util in `<WeatherDetails />`

--- legacy/weather-app/src/components/WeatherDetails.tsx
+++ legacy/weather-app/src/components/WeatherDetails.tsx
@@ -1,6 +1,7 @@
 import { useTemperatureUnitStore } from "@/store/temperature-unit";
 import { useCurrentWeather } from "@/hooks/useCurrentWeather";
 import { getCurrentDate } from "@/utils/date";
+import { stringifyLocation } from "@/utils/location";
 import Skeleton from "react-loading-skeleton";
 import { Icon } from "@hrc/material-icons";
 import "./WeatherDetails.scss";
@@ -9,8 +10,6 @@ export const WeatherDetails = () => {
   const { weather } = useCurrentWeather();
   const temperatureUnit = useTemperatureUnitStore((s) => s.unit);
   const { current, location } = weather ?? {};
-  // TODO: add stringifyLocation() util
-  const locationName = `${location?.name}, ${location?.country}`;
 
   return (
     <>
@@ -28,7 +27,7 @@ export const WeatherDetails = () => {
       </p>
       <p className="weather__location">
         <Icon name="location_on" />
-        {location == null ? <Skeleton /> : locationName}
+        {location == null ? <Skeleton /> : stringifyLocation(location)}
       </p>
     </>
   );
