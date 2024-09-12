07a12b78a8de127ea791641b0bb90daf2065ba99
hdoc <hector.ochoa.dev@gmail.com>
2024-09-12 11:48:11 -0500

    refactor(weather-app): remove temperature unit from weather store

--- legacy/weather-app/src/store/weather.ts
+++ legacy/weather-app/src/store/weather.ts
@@ -1,18 +1,14 @@
 import { create } from "zustand";
 import type { Forecast, Weather } from "@/types";
 
-export type TemperatureUnit = "fahrenheit" | "celsius";
-
 type State = {
   error: Error | null;
-  temperatureUnit: TemperatureUnit;
   weather: Weather | null;
   forecast: Forecast[] | null;
 };
 
 type Action = {
   setError: (error: State["error"]) => void;
-  setTemperatureUnit: (temperatureUnit: State["temperatureUnit"]) => void;
   setWeather: (weather: Weather) => void;
   setForecast: (forecast: Forecast[]) => void;
   clearData: () => void;
@@ -20,12 +16,9 @@ type Action = {
 
 export const useWeatherStore = create<State & Action>()((set) => ({
   error: null,
-  temperatureUnit: "celsius",
   weather: null,
   forecast: null,
   setError: (error: State["error"]) => set({ error }),
-  setTemperatureUnit: (temperatureUnit: State["temperatureUnit"]) =>
-    set({ temperatureUnit }),
   setWeather: (weather: Weather) => set({ weather }),
   setForecast: (forecast: Forecast[]) => set({ forecast }),
   clearData: () => set({ weather: null, forecast: null }),
