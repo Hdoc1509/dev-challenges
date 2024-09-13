2054bbc7de7065a106be51667b63661e1c76c72a
hdoc <hector.ochoa.dev@gmail.com>
2024-09-12 21:01:50 -0500

    feat(weather-app): add `useSearchLocation()` hook

---
+++ legacy/weather-app/src/hooks/useSearchLocation.ts
@@ -0,0 +1,40 @@
+import { useCallback } from "react";
+import { useSearchStore } from "@/store/search";
+import { searchCity } from "@/services/geolocation/client";
+import { STATUS } from "@lib/fetcher";
+
+export function useSearchLocation() {
+  const status = useSearchStore((s) => s.status);
+  const error = useSearchStore((s) => s.error);
+  const results = useSearchStore((s) => s.results);
+  const setStatus = useSearchStore((s) => s.setStatus);
+  const setError = useSearchStore((s) => s.setError);
+  const setResults = useSearchStore((s) => s.setResults);
+
+  const searchLocation = useCallback(
+    async (search: string) => {
+      setStatus(STATUS.LOADING);
+
+      const [citiesError, cities] = await searchCity(search);
+
+      if (citiesError) {
+        setError(citiesError);
+        setStatus(STATUS.ERROR);
+        return;
+      }
+
+      setResults(cities);
+      setStatus(STATUS.SUCCESS);
+    },
+    [setError, setResults, setStatus],
+  );
+
+  return {
+    isLoading: status === STATUS.LOADING,
+    isError: status === STATUS.ERROR,
+    isSuccess: status === STATUS.SUCCESS,
+    error,
+    results,
+    searchLocation,
+  };
+}
