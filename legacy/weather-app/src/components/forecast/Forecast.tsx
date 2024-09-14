7e2e1a988e8598e108672960605da4ebbf4c4d95
hdoc <hector.ochoa.dev@gmail.com>
2024-09-14 18:10:09 -0500

    refactor(weather-app): move `<ForecastItem />` to its own file

--- legacy/weather-app/src/components/forecast/Forecast.tsx
+++ legacy/weather-app/src/components/forecast/Forecast.tsx
@@ -1,37 +1,6 @@
-import Skeleton from "react-loading-skeleton";
-import { useTemperatureUnitStore } from "@/store/temperature-unit";
 import { useWeather } from "@/hooks/useWeather";
-import { getWeatherIcon } from "@/utils/icons";
-import type { Forecast as ForecastType } from "@/types";
+import { ForecastItem } from "./ForecastItem";
 import "./Forecast.scss";
-
-const ForecastItem = ({ day }: { day?: ForecastType }) => {
-  const temperatureUnit = useTemperatureUnitStore((s) => s.unit);
-  const temperatureUnitLetter = temperatureUnit[0].toUpperCase();
-  const { condition, temperature } = day ?? {};
-  const maxTemp = `${temperature?.max[temperatureUnit]}°${temperatureUnitLetter}`;
-  const minTemp = `${temperature?.min[temperatureUnit]}°${temperatureUnitLetter}`;
-
-  return (
-    <article className="forecast-item">
-      <h2 className="forecast-item__day">{day?.day ?? <Skeleton />}</h2>
-      <picture className="forecast-item__icon">
-        {condition == null ? (
-          <Skeleton />
-        ) : (
-          <img
-            src={`/icons/${getWeatherIcon(condition.code)}.png`}
-            alt={condition.name}
-          />
-        )}
-      </picture>
-      <p className="forecast-item__degrees">
-        <span>{temperature == null ? <Skeleton /> : maxTemp}</span>
-        <span>{temperature == null ? <Skeleton /> : minTemp}</span>
-      </p>
-    </article>
-  );
-};
 
 export const Forecast = () => {
   const { forecast } = useWeather();
