6664e67c4593615a06fd740c6388e0f9bddc2b6b
hdoc <hector.ochoa.dev@gmail.com>
2024-09-18 11:36:00 -0500

    feat(windbnb): add `getStays()` action to stays store
    it will be a replacement to `getStays()` function returned by
    `useStays()` hook

--- legacy/windbnb/src/store/stays.ts
+++ legacy/windbnb/src/store/stays.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
+import { searchStays } from "@/services/stays";
 import { STATUS, type Status } from "@lib/fetcher";
-import type { Stay } from "@/types";
+import type { SearchOptions, Stay } from "@/types";
 
 type State = {
   stays: Stay[];
@@ -13,6 +14,7 @@ type Action = {
   setStatus: (status: Status) => void;
   setError: (error: Error) => void;
   resetStatus: () => void;
+  getStays: (options: SearchOptions) => void;
 };
 
 export const useStaysStore = create<State & Action>((set) => ({
@@ -23,4 +25,14 @@ export const useStaysStore = create<State & Action>((set) => ({
   setStatus: (status) => set({ status }),
   setError: (error) => set({ error }),
   resetStatus: () => set({ status: STATUS.IDLE }),
+
+  getStays: async (options: SearchOptions) => {
+    set({ status: STATUS.LOADING });
+
+    const [error, stays] = await searchStays(options);
+
+    if (error) return set({ error, status: STATUS.ERROR });
+
+    set({ stays, status: STATUS.SUCCESS });
+  },
 }));
