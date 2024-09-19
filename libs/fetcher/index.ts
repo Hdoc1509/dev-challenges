286f8e3e252fce5032f5eda525b79c245fcb18c7
hdoc <hector.ochoa.dev@gmail.com>
2024-09-19 11:52:09 -0500

    feat(fetcher): add `FetchingState` type util

--- libs/fetcher/index.ts
+++ libs/fetcher/index.ts
@@ -42,7 +42,7 @@ export async function fetcher<S extends z.ZodTypeAny>(
 }
 
 export * from "./error";
-export { STATUS } from "./status";
+export { STATUS, type FetchingState } from "./status";
 export { is5xxError } from "./utils";
 export type { Status, ParamOptions } from "./types";
 export type { PromiseWithError };
--- libs/fetcher/status.ts
+++ libs/fetcher/status.ts
@@ -5,3 +5,11 @@ export const STATUS = Object.freeze({
   ERROR: "error",
 });
 export type Status = (typeof STATUS)[keyof typeof STATUS];
+
+export type FetchingState<T extends Record<string, unknown>> =
+  | ({
+      status: typeof STATUS.IDLE | typeof STATUS.LOADING;
+      error?: Error;
+    } & Partial<T>)
+  | ({ status: typeof STATUS.SUCCESS; error?: Error } & T)
+  | ({ status: typeof STATUS.ERROR; error: Error } & Partial<T>);
