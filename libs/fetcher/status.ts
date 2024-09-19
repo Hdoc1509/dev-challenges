6f1f406a3fe58ed17d564d3b62d4f07fde645b90
hdoc <hector.ochoa.dev@gmail.com>
2024-09-19 12:12:21 -0500

    feat(fetcher): use strictest type in `FetchingState`
    enforce structured type by using `never`

--- libs/fetcher/status.ts
+++ libs/fetcher/status.ts
@@ -6,10 +6,12 @@ export const STATUS = Object.freeze({
 });
 export type Status = (typeof STATUS)[keyof typeof STATUS];
 
+type Never<T extends Record<string, unknown>> = { [K in keyof T]?: never };
+
 export type FetchingState<T extends Record<string, unknown>> =
   | ({
       status: typeof STATUS.IDLE | typeof STATUS.LOADING;
-      error?: Error;
-    } & Partial<T>)
-  | ({ status: typeof STATUS.SUCCESS; error?: Error } & T)
-  | ({ status: typeof STATUS.ERROR; error: Error } & Partial<T>);
+      error?: never;
+    } & Never<T>)
+  | ({ status: typeof STATUS.SUCCESS; error?: never } & T)
+  | ({ status: typeof STATUS.ERROR; error: Error } & Never<T>);
