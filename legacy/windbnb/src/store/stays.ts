65739f842617f650f11924e95ea05c6a3e6b4d4e
hdoc <hector.ochoa.dev@gmail.com>
2024-09-19 11:26:44 -0500

    feat(windbnb): improve typing of stays store
    add `FetchingState` type util that uses discriminated union type based
    on `status` property
    
    https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes-func.html#discriminated-unions

--- legacy/windbnb/src/store/stays.ts
+++ legacy/windbnb/src/store/stays.ts
@@ -1,20 +1,24 @@
 import { create } from "zustand";
 import { searchStays } from "@/services/stays";
-import { STATUS, type Status } from "@lib/fetcher";
+import { STATUS } from "@lib/fetcher";
 import type { SearchOptions, Stay } from "@/types";
 
-type State = {
-  stays: Stay[];
-  status: Status;
-  error?: Error;
-};
+type FetchingState<T extends Record<string, unknown>> =
+  | ({
+      status: typeof STATUS.IDLE | typeof STATUS.LOADING;
+      error?: Error;
+    } & Partial<T>)
+  | ({ status: typeof STATUS.SUCCESS; error?: Error } & T)
+  | ({ status: typeof STATUS.ERROR; error: Error } & Partial<T>);
+
+type StoreFetchingState = FetchingState<{ stays: Stay[] }>;
+type State = StoreFetchingState;
 
 type Action = {
   getStays: (options?: SearchOptions) => void;
 };
 
 export const useStaysStore = create<State & Action>((set) => ({
-  stays: [],
   status: STATUS.IDLE,
 
   getStays: async (options) => {
