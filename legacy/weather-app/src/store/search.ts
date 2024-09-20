6dc6dc292663eace622b5e7e8ce1e52e5bac3aae
hdoc <hector.ochoa.dev@gmail.com>
2024-09-20 10:29:45 -0500

    refactor(weather-app): remove unnecessary actions from search store

--- legacy/weather-app/src/store/search.ts
+++ legacy/weather-app/src/store/search.ts
@@ -4,34 +4,20 @@ import { STATUS, type Status } from "@lib/fetcher";
 import type { City } from "@/types";
 
 type State = {
-  search: string;
-  lastSearch: string;
   status: Status;
   error: Error | null;
   results: City[];
 };
 
 type Action = {
-  setSearch: (search: string) => void;
-  setLastSearch: (lastSearch: string) => void;
-  setStatus: (status: Status) => void;
-  setError: (error: Error) => void;
-  setResults: (results: City[]) => void;
   searchLocation: (search: string) => Promise<void>;
   removeResultById: (resultId: number) => void;
 };
 
 export const useSearchStore = create<State & Action>()((set) => ({
-  search: "",
-  lastSearch: "",
   status: STATUS.IDLE,
   error: null,
   results: [],
-  setSearch: (search) => set({ search }),
-  setLastSearch: (lastSearch) => set({ lastSearch }),
-  setStatus: (status) => set({ status }),
-  setError: (error) => set({ error }),
-  setResults: (results) => set({ results }),
 
   searchLocation: async (search) => {
     set({ status: STATUS.LOADING });
