8486bd8055662f504a3e2dafe43dc1cb129351ee
hdoc <hector.ochoa.dev@gmail.com>
2024-10-23 11:37:38 -0500

    refactor(multi-step-form): set export of form elements near its declaration and validation

--- vanilla/multi-step-form/src/form.js
+++ vanilla/multi-step-form/src/form.js
@@ -4,9 +4,10 @@ const ERROR_MESSAGE_CLASS = "error-message";
 const $unsafeForm = document.getElementById(FORM_ID);
 if (!($unsafeForm instanceof HTMLFormElement))
   throw new Error(`missing '#${FORM_ID}' element`);
+export const $registerForm = $unsafeForm;
 
-const $unsafeNameInput = $unsafeForm.querySelector("input[name='name']");
-const $unsafeNameInputError = $unsafeForm.querySelector(
+const $unsafeNameInput = $registerForm.querySelector("input[name='name']");
+const $unsafeNameInputError = $registerForm.querySelector(
   `input[name='name'] + .${ERROR_MESSAGE_CLASS}`,
 );
 if (!($unsafeNameInput instanceof HTMLInputElement))
@@ -15,9 +16,11 @@ if (!($unsafeNameInputError instanceof HTMLElement))
   throw new Error(
     `missing '.${ERROR_MESSAGE_CLASS}' element for 'name' field in '#${FORM_ID}' element`,
   );
+export const $nameInput = $unsafeNameInput;
+export const $nameInputError = $unsafeNameInputError;
 
-const $unsafeEmailInput = $unsafeForm.querySelector("input[name='email']");
-const $unsafeEmailInputError = $unsafeForm.querySelector(
+const $unsafeEmailInput = $registerForm.querySelector("input[name='email']");
+const $unsafeEmailInputError = $registerForm.querySelector(
   `input[name='email'] + .${ERROR_MESSAGE_CLASS}`,
 );
 if (!($unsafeEmailInput instanceof HTMLInputElement))
@@ -26,9 +29,5 @@ if (!($unsafeEmailInputError instanceof HTMLElement))
   throw new Error(
     `missing '.${ERROR_MESSAGE_CLASS}' element for 'email' field in '#${FORM_ID}' element`,
   );
-
-export const $registerForm = $unsafeForm;
-export const $nameInput = $unsafeNameInput;
-export const $nameInputError = $unsafeNameInputError;
 export const $emailInput = $unsafeEmailInput;
 export const $emailInputError = $unsafeEmailInputError;
