97bd41eed6f132500e70c03a1e13390838050dd0
hdoc <hector.ochoa.dev@gmail.com>
2024-10-24 20:23:26 -0500

    build(multi-step-form): use `import.meta.dirname` instead of `__dirname`

--- vanilla/multi-step-form/vite.config.js
+++ vanilla/multi-step-form/vite.config.js
@@ -1,16 +1,13 @@
-import { resolve, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/dev-challenges/multi-step-form",
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      // https://nodejs.org/api/esm.html#importmetadirname
+      "@": resolve(import.meta.dirname, "src"),
     },
   },
 });
