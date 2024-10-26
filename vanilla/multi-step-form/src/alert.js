bcd5fab22d00c91ef85342eacd540d5a81b861d8
hdoc <hector.ochoa.dev@gmail.com>
2024-10-26 11:46:30 -0500

    refactor(multi-step-form): simplify logic to show alert
    - use `animationend` and `transitionend` events
    - remove auxiliar variables for computed styles
    - remove util to parse time from computed styles

--- vanilla/multi-step-form/src/alert.js
+++ vanilla/multi-step-form/src/alert.js
@@ -10,12 +10,6 @@ const $alertTimebar = document.querySelector(".alert__time-bar");
 if (!($alertTimebar instanceof HTMLDivElement))
   throw new Error("'.alert__time-bar' element not found");
 
-/** @param {string} time */
-const parseTime = (time) =>
-  time.includes("ms")
-    ? Number(time.replace("ms", ""))
-    : Number(time.replace("s", "")) * 1000;
-
 export const resetAlertAnimation = () => {
   // https://css-tricks.com/restart-css-animation/
   $alertTimebar.classList.remove("alert__time-bar");
@@ -23,40 +17,26 @@ export const resetAlertAnimation = () => {
   $alertTimebar.classList.add("alert__time-bar");
 };
 
-/** @type {number | null} */
-let animationDuration = null;
-/** @type {number | null} */
-let movementDelay = null;
-
 /**
  * @param {Object} options
  * @param {'success' | 'error'} options.color
  * @param {string} options.text
  */
 export const showAlert = ({ color, text }) => {
-  // computed styles are intially empty on dev server
-  if (animationDuration === null || movementDelay === null) {
-    const animationDurationRaw = getComputedStyle($alert).getPropertyValue(
-      "--alert-time-bar-duration",
-    );
-    const movementDelayRaw = getComputedStyle($alert).getPropertyValue(
-      "--_movement-duration",
-    );
-
-    animationDuration = parseTime(animationDurationRaw);
-    movementDelay = parseTime(movementDelayRaw);
-  }
-
   $alert.classList.add(`alert--${color}`, "alert--open");
   $alertText.textContent = text;
+  $alertTimebar.addEventListener(
+    "animationend",
+    () => {
+      $alert.classList.remove("alert--open");
+      $alert.classList.add("alert--closing");
 
-  setTimeout(() => {
-    $alert.classList.remove("alert--open");
-    $alert.classList.add("alert--closing");
-
-    setTimeout(
-      () => $alert.classList.remove("alert--closing"),
-      /** @type {number} */ (movementDelay),
-    );
-  }, animationDuration + movementDelay);
+      $alert.addEventListener(
+        "transitionend",
+        () => $alert.classList.remove("alert--closing"),
+        { once: true },
+      );
+    },
+    { once: true },
+  );
 };
