3159cfda6ad5f03656355534aaff586453937059
hdoc <hector.ochoa.dev@gmail.com>
2024-10-26 13:15:12 -0500

    feat(multi-step-form): add util to get an element by id

---
+++ vanilla/multi-step-form/src/utils/dom.js
@@ -0,0 +1,16 @@
+/**
+ * Get an element by id and check if it's instance of the expected interface.
+ * @template {(new (...args: any) => any)} T
+ * @param {string} id
+ * @param {T} expectedInterface
+ * @param {Document|DocumentFragment} [$parent]
+ * @returns {T['prototype']}
+ */
+export function getElementById(id, expectedInterface, $parent = document) {
+  const $element = $parent.getElementById(id);
+
+  if (!($element instanceof expectedInterface))
+    throw new Error(`${expectedInterface.name} with '#${id}' id not found`);
+
+  return $element;
+}
