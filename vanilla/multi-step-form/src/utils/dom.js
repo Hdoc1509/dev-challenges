1ae7b977d90322f7f28f33d68431317cc80a75c1
hdoc <hector.ochoa.dev@gmail.com>
2024-10-27 12:36:11 -0500

    feat(multi-step-form): add `getAllElementsBySelector()` util

--- vanilla/multi-step-form/src/utils/dom.js
+++ vanilla/multi-step-form/src/utils/dom.js
@@ -37,3 +37,39 @@ export function getElementBySelector(
 
   return $element;
 }
+
+/**
+ * Get all elements by selector and check if they're instances of the expected interface.
+ * @template {(new (...args: any) => any)} T
+ * @param {string} selector
+ * @param {T} expectedInterface
+ * @param {Document|DocumentFragment|Element} [$parent]
+ * @returns {T['prototype'][]}
+ */
+export function getAllElementsBySelector(
+  selector,
+  expectedInterface,
+  $parent = document,
+) {
+  const $elements = $parent.querySelectorAll(selector);
+
+  if ($elements.length === 0)
+    throw new Error(
+      `No elements that matches "${selector}" selector were found`,
+    );
+
+  const elements = [];
+
+  for (let i = 0; i < $elements.length; i++) {
+    const $element = $elements[i];
+
+    if (!($element instanceof expectedInterface))
+      throw new Error(
+        `${expectedInterface.name} at index ${i} doesn't match "${selector}" selector`,
+      );
+
+    elements.push($element);
+  }
+
+  return elements;
+}
