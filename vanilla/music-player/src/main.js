850d15ea0d0e451f79b54ab8d33aec0b1dd4b152
hdoc <hector.ochoa.dev@gmail.com>
2024-11-13 12:04:22 -0500

    feat(music-player): update way of showing progress track

--- vanilla/music-player/src/main.js
+++ vanilla/music-player/src/main.js
@@ -1,6 +1,25 @@
-import "@fontsource-variable/inter"
-import "@fontsource/inter/400.css"
-import "@fontsource/inter/600.css"
-import './styles/main.css'
+import { getElementById } from "@lib/dom";
+import "@fontsource-variable/inter";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+import "./styles/main.css";
 
-console.log("app initialized")
+// TODO: handle play/pause/next/prev logic
+//  - https://vite.dev/guide/assets.html#new-url-url-import-meta-url
+//  - https://github.com/krau5/pomo/pull/9/files
+//  - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+
+const $playProgress = getElementById("progress-bar", HTMLInputElement);
+
+document.addEventListener("input", (e) => {
+  const $target = e.target;
+
+  if ($target instanceof HTMLInputElement) {
+    if ($target === $playProgress) {
+      const currentTime = $target.valueAsNumber;
+      const progress = (currentTime / Number($target.max)) * 100;
+
+      $target.style.setProperty("--progress-value", `${progress}%`);
+    }
+  }
+});
