2a878afdf35bca1ee6d72f4eceed8faf70e76e9d
hdoc <hector.ochoa.dev@gmail.com>
2024-11-16 12:23:11 -0500

    feat(music-player): allow to go to next song

--- vanilla/music-player/src/main.js
+++ vanilla/music-player/src/main.js
@@ -1,13 +1,10 @@
-import { getElementById } from "@lib/dom";
+import { getElementById, getElementBySelector } from "@lib/dom";
+import cover1URL from "/cover-1.png";
+import cover2URL from "/cover-2.png";
 import "@fontsource-variable/inter";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/600.css";
 import "./styles/main.css";
-
-// TODO: handle play/pause/next/prev logic
-//  - https://vite.dev/guide/assets.html#new-url-url-import-meta-url
-//  - https://github.com/krau5/pomo/pull/9/files
-//  - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
 
 const $playProgress = getElementById("progress-bar", HTMLInputElement);
 const $playPauseControl = getElementById(
@@ -17,6 +14,39 @@ const $playPauseControl = getElementById(
 const $nextControl = getElementById("control-next", HTMLButtonElement);
 const $prevControl = getElementById("control-prev", HTMLButtonElement);
 const $song = getElementById("song", HTMLAudioElement);
+const $songTitle = getElementBySelector(
+  ".player-card__title",
+  HTMLHeadingElement,
+);
+const $songAuthor = getElementBySelector(
+  ".player-card__author",
+  HTMLParagraphElement,
+);
+const $songCover = getElementBySelector(
+  ".player-card__cover > img",
+  HTMLImageElement,
+);
+const $songDuration = getElementBySelector(
+  ".player-card__duration",
+  HTMLSpanElement,
+);
+
+const songs = [
+  {
+    title: "Lost in the City Lights",
+    artist: "Cosmo Sheldrake",
+    url: new URL("/lost-in-city-lights-145038.mp3", import.meta.url).href,
+    cover: cover1URL,
+  },
+  {
+    title: "Forest Lullaby",
+    artist: "Lesfm",
+    url: new URL("/forest-lullaby-110624.mp3", import.meta.url).href,
+    cover: cover2URL,
+  },
+];
+const totalSongs = songs.length;
+let currentSongIdx = 0;
 
 $song.volume = 0.25;
 
@@ -54,4 +84,28 @@ document.addEventListener("click", (e) => {
       return;
     }
   }
+
+  if ($target === $nextControl) {
+    const newSongIdx =
+      currentSongIdx === totalSongs - 1 ? 0 : currentSongIdx + 1;
+
+    // TODO: encapsulate this logic in a function. receive the new song as
+    // argument
+    const { title, artist, url, cover } = songs[newSongIdx];
+
+    $songTitle.textContent = title;
+    $songAuthor.textContent = artist;
+    $songCover.src = cover;
+    $song.src = url; // triggers `durationchange` event
+
+    currentSongIdx = newSongIdx;
+  }
+});
+
+$song.addEventListener("durationchange", () => {
+  const duration = $song.duration;
+  const minutes = `${Math.floor(duration / 60)}`.padStart(2, "0");
+  const seconds = Math.round(duration % 60);
+
+  $songDuration.textContent = `${minutes}:${seconds}`;
 });
