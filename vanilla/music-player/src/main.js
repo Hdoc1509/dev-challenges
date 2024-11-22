876eca83081477dd90f6c09f77dd2f976d13554b
hdoc <hector.ochoa.dev@gmail.com>
2024-11-22 18:19:14 -0500

    refactor(music-player): set url of first song on `DOMContentLoaded`

--- vanilla/music-player/src/main.js
+++ vanilla/music-player/src/main.js
@@ -1,9 +1,12 @@
 import { setupEventListeners } from "./events/listeners.js";
+import { $song } from "./song.js";
+import { songs } from "./consts.js";
 import "@fontsource-variable/inter";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/600.css";
 import "./styles/main.css";
 
 document.addEventListener("DOMContentLoaded", () => {
+  $song.src = songs[0].url;
   setupEventListeners();
 });
