9a85aa077e797d065e7ae634b52a4e18ef575627
hdoc <hector.ochoa.dev@gmail.com>
2024-12-07 21:51:41 -0500

    feat(random-quote): add quote services

--- vanilla/random-quote/src/main.js
+++ vanilla/random-quote/src/main.js
@@ -1,6 +1,10 @@
+import { getMockedRandomQuote } from "./services/quotes";
 import "@fontsource-variable/outfit";
 import "@fontsource/outfit/400.css";
 import "@fontsource/outfit/600.css";
 import "./styles/main.css";
 
-console.log("app initialized");
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("app initialized");
+  getMockedRandomQuote().then(console.log);
+});
---
+++ vanilla/random-quote/src/services/quotes/index.js
@@ -0,0 +1,55 @@
+import { ServiceError, fetcher } from "@lib/fetcher";
+import { QuotesResponseSchema, QuotesErrorResponseSchema } from "./schema";
+import { parseQuote } from "./parse";
+import { randomElement } from "@/utils";
+import quotesMock from "@/mocks/quotes.json";
+
+/**
+ * @typedef {"limit"|"offset"|"lang"|"curated"} QuoteServiceParamList
+ * @typedef {import("@/types").Quote} Quote
+ * @typedef {import("@lib/fetcher").PromiseWithError<Quote>} QuoteServiceResult
+ * @typedef {import("@lib/fetcher").ParamOptions<QuoteServiceParamList>} QuoteParams
+ */
+
+const API_URL = "https://api.paperquotes.com";
+
+const ApiResponseSchema = QuotesResponseSchema.or(QuotesErrorResponseSchema);
+const QuotesError = new ServiceError("Quotes");
+
+const PARAMS = {
+  LIMIT: "1",
+  LANG: "en",
+  CURATED: "1",
+};
+
+/** @returns {QuoteServiceResult} */
+export const getRandomQuote = async () => {
+  const params = new URLSearchParams(
+    /** @satisfies {QuoteParams} */ ({
+      limit: PARAMS.LIMIT,
+      offset: Math.floor(Math.random() * 217_699).toString(),
+      lang: PARAMS.LANG,
+      curated: PARAMS.CURATED,
+    }),
+  );
+
+  const [error, data] = await fetcher(
+    `${API_URL}/quotes?${params.toString()}`,
+    {
+      schema: ApiResponseSchema,
+      serviceError: QuotesError,
+      checkStatus: false, //
+    },
+  );
+
+  if (error) return [error];
+
+  if ("errno" in data) return [QuotesError.generic(data.errmsg)];
+
+  return [null, parseQuote(data.results[0])];
+};
+
+/** @returns {QuoteServiceResult} */
+export const getMockedRandomQuote = async () => {
+  return [null, parseQuote(randomElement(quotesMock.results))];
+};
