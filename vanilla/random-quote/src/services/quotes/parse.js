ba9633470791e1e7b29c0ce47c153df7bf807294
hdoc <hector.ochoa.dev@gmail.com>
2024-12-07 22:11:13 -0500

    feat(random-quote): add `parseTags()` internal util

--- vanilla/random-quote/src/services/quotes/parse.js
+++ vanilla/random-quote/src/services/quotes/parse.js
@@ -1,11 +1,21 @@
 /** @typedef {import("@/types").Quote} Quote */
 /** @typedef {import("./schema").QuotesResponse} QuotesResponse */
 
+/** @param {Array<string>} tags */
+const parseTags = (tags) =>
+  tags.map((tag) =>
+    tag
+      .split("-")
+      .map((word) => `${word[0].toUpperCase()}${word.slice(1)}`)
+      .join(" "),
+  );
+
 /**
  * @param {QuotesResponse["results"][number]} quote
  * @returns {Quote}
  */
 export function parseQuote(quote) {
   const { author, tags, pk } = quote;
-  return { id: pk, text: quote.quote, author, tags };
+
+  return { id: pk, text: quote.quote, author, tags: parseTags(tags) };
 }
