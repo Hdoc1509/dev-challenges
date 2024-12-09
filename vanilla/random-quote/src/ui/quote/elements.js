81988ac29ac68a3a60436b98a1ef37c167be4c51
hdoc <hector.ochoa.dev@gmail.com>
2024-12-09 12:10:36 -0500

    feat(random-quote): capture more quote-related elements

--- vanilla/random-quote/src/ui/quote/elements.js
+++ vanilla/random-quote/src/ui/quote/elements.js
@@ -1,5 +1,12 @@
 import { getElementBySelector } from "@lib/dom";
 
+export const $quote = getElementBySelector(".quote", HTMLDivElement);
+
+export const $error = getElementBySelector(
+  ".quote__error",
+  HTMLParagraphElement,
+);
+
 export const $author = getElementBySelector(
   ".quote__author",
   HTMLHeadingElement,
