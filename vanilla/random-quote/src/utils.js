3a4e907af3e128d11f4607b3178a8e4eb00dd274
hdoc <hector.ochoa.dev@gmail.com>
2024-12-10 10:29:10 -0500

    feat(random-quote): add `randomInt()` util

--- vanilla/random-quote/src/utils.js
+++ vanilla/random-quote/src/utils.js
@@ -5,3 +5,11 @@
  */
 export const randomElement = (arr) =>
   arr[Math.floor(Math.random() * arr.length)];
+
+/**
+ * Returns a random integer between `min` (inclusive) and `max` (inclusive)
+ * @param {number} min
+ * @param {number} max
+ */
+export const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
