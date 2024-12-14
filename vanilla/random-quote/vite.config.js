ee464f0d6e683f7589564e420778260732968d8a
hdoc <hector.ochoa.dev@gmail.com>
2024-12-14 13:07:25 -0500

    build(random-quote): set custom chunking strategy

--- vanilla/random-quote/vite.config.js
+++ vanilla/random-quote/vite.config.js
@@ -4,6 +4,18 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/dev-challenges/random-quote",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) return "vendor";
+          if (id.includes("libs")) return "libs";
+          if (id.includes("src/events")) return "events";
+          if (id.includes("src/services")) return "services";
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       // https://nodejs.org/api/esm.html#importmetadirname
