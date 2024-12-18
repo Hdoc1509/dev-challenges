c4b077bbb19ba9ef6aeebce76581a4c5b0d38662
hdoc <hector.ochoa.dev@gmail.com>
2024-12-18 11:41:18 -0500

    feat(qr-code-generator): add styles for index page

--- vanilla/qr-code-generator/src/main.js
+++ vanilla/qr-code-generator/src/main.js
@@ -1,6 +1,7 @@
 import "@fontsource-variable/outfit";
 import "@fontsource/outfit/400.css";
 import "@fontsource/outfit/600.css";
-import './styles/main.css'
+import "./styles/global.css";
+import "./styles/page/index.css";
 
-console.log("app initialized")
+console.log("app initialized");
