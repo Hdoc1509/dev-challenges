88117476c874aa55c53e9a44d66b000d950039ea
hdoc <hector.ochoa.dev@gmail.com>
2025-01-09 10:47:51 -0500

    build(qr-code-generator): include `qrcodejs` in the bundle

---
+++ vanilla/qr-code-generator/scripts/copy-qrcodejs.sh
@@ -0,0 +1,5 @@
+project_dir="$PWD"
+
+cp \
+  "$project_dir/node_modules/qrcodejs/qrcode.min.js" \
+  "$project_dir/dist/assets/qrcode.min.js"
--- vanilla/qr-code-generator/vite.config.js
+++ vanilla/qr-code-generator/vite.config.js
@@ -2,10 +2,52 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 // import { createHtmlPlugin } from "vite-plugin-html";
 
+const BASE_URL = "/dev-challenges/qr-code-generator";
+
 // https://vite.dev/config/
 export default defineConfig({
   // plugins: [createHtmlPlugin({ minify: true })],
-  base: "/dev-challenges/qr-code-generator",
+  plugins: [
+    {
+      name: "qrcodejs-script-server",
+      apply: "serve",
+      transformIndexHtml(html) {
+        return {
+          html,
+          tags: [
+            {
+              tag: "script",
+              attrs: {
+                src: `${BASE_URL}/node_modules/qrcodejs/qrcode.min.js`,
+              },
+              injectTo: "head-prepend",
+            },
+          ],
+        };
+      },
+    },
+    {
+      name: "qrcodejs-script-build",
+      apply: "build",
+      transformIndexHtml(html) {
+        return {
+          html,
+          tags: [
+            {
+              tag: "script",
+              attrs: {
+                // NOTE: ./scripts/copy-qrcodejs.sh is the responsible for
+                // copying the file to the build folder.
+                src: `${BASE_URL}/assets/qrcode.min.js`,
+              },
+              injectTo: "head-prepend",
+            },
+          ],
+        };
+      },
+    },
+  ],
+  base: BASE_URL,
   build: {
     rollupOptions: {
       input: {
