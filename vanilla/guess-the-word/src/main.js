42c8c258590cce0e19c767d111dbaba7f5d0e90f
hdoc <hector.ochoa.dev@gmail.com>
2025-01-17 18:08:37 -0500

    feat(guess-the-word): show random scrambled word on page load

--- vanilla/guess-the-word/src/main.js
+++ vanilla/guess-the-word/src/main.js
@@ -1,6 +1,48 @@
+import { getElementById } from "@lib/dom";
 import "@fontsource-variable/outfit";
 import "@fontsource/outfit/400.css";
 import "@fontsource/outfit/600.css";
 import "./styles/main.css";
 
-console.log("app initialized");
+const $word = getElementById("word", HTMLDivElement);
+
+const words = [
+  "example",
+  "javascript",
+  "coding",
+  "challenge",
+  "flower",
+  "adventure",
+];
+let currentWord = "";
+let tries = 0;
+let mistakes = 0;
+
+/** @param {string} word */
+const scrambleWord = (word) => {
+  const letters = word.split("");
+  const scrambledLetter = letters.sort(() => Math.random() - 0.5);
+
+  return scrambledLetter.join("");
+};
+
+const generateRandomWord = () => {
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+
+  currentWord = scrambleWord(randomWord);
+  currentWord.split("").forEach(letter => {
+    const $letter = document.createElement("span");
+
+    $letter.textContent = letter;
+    $word.appendChild($letter);
+  });
+};
+
+/** @param {number} quantity */
+const createLetterFields = (quantity) => {};
+
+const handleLetterInput = () => {};
+
+const resetGame = () => {};
+
+document.addEventListener("DOMContentLoaded", () => generateRandomWord());
