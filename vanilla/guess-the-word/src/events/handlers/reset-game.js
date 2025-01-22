036f2b19a4e30a65a0dae2d8ae82b98d2fd52f78
hdoc <hector.ochoa.dev@gmail.com>
2025-01-22 18:14:58 -0500

    refactor(guess-the-word): move `resetGame()` handler to its own file

---
+++ vanilla/guess-the-word/src/events/handlers/reset-game.js
@@ -0,0 +1,29 @@
+import { resetTries } from "@/state/tries";
+import { resetMistakes } from "@/state/mistakes";
+import { $mistakenLetters, $tries, $triesIndicators } from "@/ui/info";
+import { $letterFields } from "@/ui/typing";
+import { $reset } from "@/ui/actions";
+import { CLASSES } from "@/consts";
+
+export function resetGame() {
+  const $firstField = $letterFields[0];
+  const $currentLetter = /** @type {HTMLSpanElement | null} */ (
+    document.querySelector(`.${CLASSES.TYPING_LETTER_CURRENT}`)
+  );
+
+  resetTries();
+  resetMistakes();
+
+  $tries.textContent = "0";
+  $triesIndicators.forEach(($item) => $item.removeAttribute("data-completed"));
+  $mistakenLetters.textContent = "-";
+  $currentLetter?.classList.remove(CLASSES.TYPING_LETTER_CURRENT);
+  $letterFields.forEach(($field) => {
+    $field.readOnly = false;
+    $field.disabled = false;
+    $field.value = "";
+  });
+  $firstField.parentElement?.classList.add(CLASSES.TYPING_LETTER_CURRENT);
+  $firstField.focus();
+  $reset.disabled = true;
+}
--- vanilla/guess-the-word/src/main.js
+++ vanilla/guess-the-word/src/main.js
@@ -1,20 +1,9 @@
 import { showAlert } from "@lib/alert";
 import { currentWord } from "./state/current-word";
-import {
-  hasReachedMaxTries,
-  increaseTries,
-  resetTries,
-  tries,
-} from "./state/tries";
-import {
-  hasNoMistakes,
-  resetMistakes,
-  setMistakes,
-  mistakes,
-} from "./state/mistakes";
+import { hasReachedMaxTries, increaseTries, tries } from "./state/tries";
+import { hasNoMistakes, setMistakes, mistakes } from "./state/mistakes";
 import { generateRandomWord } from "./events/handlers/random-word";
 import { $mistakenLetters, $tries, $triesIndicators } from "./ui/info";
-import { $letterFields } from "./ui/typing";
 import { $randomWord, $reset } from "./ui/actions";
 import { CLASSES } from "./consts";
 import "@lib/alert/styles.css";
@@ -22,6 +11,7 @@ import "@fontsource-variable/outfit";
 import "@fontsource/outfit/400.css";
 import "@fontsource/outfit/600.css";
 import "./styles/main.css";
+import { resetGame } from "./events/handlers/reset-game";
 
 /** @param {HTMLInputElement} $currentField */
 const handleLetterInput = ($currentField) => {
@@ -64,29 +54,6 @@ const handleLetterInput = ($currentField) => {
     showAlert({ color: "success", text: "🎉 Success!" });
     $reset.disabled = true;
   }
-};
-
-const resetGame = () => {
-  const $firstField = $letterFields[0];
-  const $currentLetter = /** @type {HTMLSpanElement | null} */ (
-    document.querySelector(`.${CLASSES.TYPING_LETTER_CURRENT}`)
-  );
-
-  resetTries();
-  resetMistakes();
-
-  $tries.textContent = "0";
-  $triesIndicators.forEach(($item) => $item.removeAttribute("data-completed"));
-  $mistakenLetters.textContent = "-";
-  $currentLetter?.classList.remove(CLASSES.TYPING_LETTER_CURRENT);
-  $letterFields.forEach(($field) => {
-    $field.readOnly = false;
-    $field.disabled = false;
-    $field.value = "";
-  });
-  $firstField.parentElement?.classList.add(CLASSES.TYPING_LETTER_CURRENT);
-  $firstField.focus();
-  $reset.disabled = true;
 };
 
 document.addEventListener("DOMContentLoaded", () => generateRandomWord());
