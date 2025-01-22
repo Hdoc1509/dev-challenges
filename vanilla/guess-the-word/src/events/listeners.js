7fa73b3c98bf681c2e6350aec146c35be96a8035
hdoc <hector.ochoa.dev@gmail.com>
2025-01-22 18:22:50 -0500

    refactor(guess-the-word): move event listeners initialization to separate file

---
+++ vanilla/guess-the-word/src/events/listeners.js
@@ -0,0 +1,26 @@
+import { generateRandomWord } from "./handlers/random-word";
+import { handleLetterInput } from "./handlers/letter-input";
+import { resetGame } from "./handlers/reset-game";
+import { $randomWord, $reset } from "@/ui/actions";
+
+export function setupEventListeners() {
+  generateRandomWord();
+
+  document.addEventListener("click", (e) => {
+    const $target = e.target;
+
+    if ($target === $randomWord) generateRandomWord();
+    if ($target === $reset) resetGame();
+  });
+
+  // NOTE: should I reset animation of letter input on blur?
+  document.addEventListener("input", (e) => {
+    const $target = e.target;
+
+    if (
+      $target instanceof HTMLInputElement &&
+      $target.matches(".typing__letter--current > input")
+    )
+      handleLetterInput($target);
+  });
+}
--- vanilla/guess-the-word/src/main.js
+++ vanilla/guess-the-word/src/main.js
@@ -1,29 +1,8 @@
-import { generateRandomWord } from "./events/handlers/random-word";
-import { handleLetterInput } from "./events/handlers/letter-input";
-import { resetGame } from "./events/handlers/reset-game";
-import { $randomWord, $reset } from "./ui/actions";
+import { setupEventListeners } from "./events/listeners";
 import "@lib/alert/styles.css";
 import "@fontsource-variable/outfit";
 import "@fontsource/outfit/400.css";
 import "@fontsource/outfit/600.css";
 import "./styles/main.css";
 
-document.addEventListener("DOMContentLoaded", () => generateRandomWord());
-
-document.addEventListener("click", (e) => {
-  const $target = e.target;
-
-  if ($target === $randomWord) generateRandomWord();
-  if ($target === $reset) resetGame();
-});
-
-document.addEventListener("input", (e) => {
-  const $target = e.target;
-
-  if (
-    $target instanceof HTMLInputElement &&
-    $target.matches(".typing__letter--current > input")
-  )
-    handleLetterInput($target);
-});
-// NOTE: should I reset animation of letter input on blur?
+document.addEventListener("DOMContentLoaded", () => setupEventListeners());
