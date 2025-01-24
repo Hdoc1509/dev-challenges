db9ac48eabb3f20e9313cad9f91900231a18bb23
hdoc <hector.ochoa.dev@gmail.com>
2025-01-24 18:36:34 -0500

    refactor(guess-the-word): rename variables for menu tabs

--- vanilla/guess-the-word/src/ui/menu.js
+++ vanilla/guess-the-word/src/ui/menu.js
@@ -1,5 +1,5 @@
-import { Tabs } from "@/tabs";
 import { getElementById } from "@lib/dom";
+import { Tabs } from "@/tabs";
 
 export const $menuOpen = getElementById("menu-open", HTMLButtonElement);
 
@@ -7,8 +7,11 @@ export const $menuClose = getElementById("menu-close", HTMLButtonElement);
 
 export const $menu = getElementById("app-menu", HTMLDialogElement);
 
-const $menuNav = getElementById("menu-nav", HTMLElement);
+const $menuTabNav = getElementById("menu-nav", HTMLElement);
 
-const $menuContent = getElementById("menu-content", HTMLElement);
+const $menuTabContent = getElementById("menu-content", HTMLElement);
 
-export const MenuTabs = new Tabs({ $nav: $menuNav, $content: $menuContent });
+export const MenuTabs = new Tabs({
+  $nav: $menuTabNav,
+  $content: $menuTabContent,
+});
