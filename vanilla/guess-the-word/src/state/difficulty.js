f07b80e488ff233a61ec297f9d009bd5fdd2a136
hdoc <hector.ochoa.dev@gmail.com>
2025-02-01 11:55:10 -0500

    feat(guess-the-word): add debug option to difficulty state setters

--- vanilla/guess-the-word/src/state/difficulty.js
+++ vanilla/guess-the-word/src/state/difficulty.js
@@ -4,11 +4,16 @@ import { DIFFICULTY } from "@/consts";
 export let difficulty = DIFFICULTY.NORMAL;
 
 /** @param {import("@/consts").Difficulty} newDifficulty */
-export const setDifficulty = (newDifficulty) => (difficulty = newDifficulty);
+export const setDifficulty = (newDifficulty, { debug = false } = {}) => {
+  difficulty = newDifficulty;
+  if (debug) console.log({ difficulty });
+};
 
 /** @type {import("@/consts").Difficulty | null} */
 export let nextDifficulty = null;
 
 /** @param {import("@/consts").Difficulty | null} newDifficulty */
-export const setNextDifficulty = (newDifficulty) =>
-  (nextDifficulty = newDifficulty);
+export const setNextDifficulty = (newDifficulty, { debug = false } = {}) => {
+  nextDifficulty = newDifficulty;
+  if (debug) console.log({ nextDifficulty });
+};
