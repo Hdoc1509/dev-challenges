052227a777b1d0e24d82ae57ceb6317c7a5e176e
hdoc <hector.ochoa.dev@gmail.com>
2025-02-01 10:47:22 -0500

    feat(guess-the-word): add `nextDifficulty` state

--- vanilla/guess-the-word/src/state/difficulty.js
+++ vanilla/guess-the-word/src/state/difficulty.js
@@ -5,3 +5,10 @@ export let difficulty = DIFFICULTY.NORMAL;
 
 /** @param {import("@/consts").Difficulty} newDifficulty */
 export const setDifficulty = (newDifficulty) => (difficulty = newDifficulty);
+
+/** @type {import("@/consts").Difficulty | null} */
+export let nextDifficulty = null;
+
+/** @param {import("@/consts").Difficulty | null} newDifficulty */
+export const setNextDifficulty = (newDifficulty) =>
+  (nextDifficulty = newDifficulty);
