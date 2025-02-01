8646e929da21b9b1b30310457d358b01d8a1e5ab
hdoc <hector.ochoa.dev@gmail.com>
2025-02-01 10:23:53 -0500

    feat(guess-the-word): capture difficulty form

---
+++ vanilla/guess-the-word/src/ui/difficulty-form.js
@@ -0,0 +1,16 @@
+import { getElementById } from "@lib/dom";
+
+export const $difficultyForm = getElementById(
+  "difficulty-form",
+  HTMLFormElement,
+);
+
+export const getSelectedDifficulty = () => {
+  const data = new FormData($difficultyForm);
+  const difficulty = data.get("difficulty");
+
+  if (typeof difficulty !== "string")
+    throw new Error("Difficulty is not a string");
+
+  return /** @type {import("@/consts").Difficulty} */ (difficulty);
+};
