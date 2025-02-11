006c411d0e48c903277081823245aef504e33d09
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-02-11 17:22:49 -0500

    refactor(guess-the-word): handle difficulty change conditions in same function
    simpler and more readable

--- vanilla/guess-the-word/src/events/handlers/difficulty-change.js
+++ vanilla/guess-the-word/src/events/handlers/difficulty-change.js
@@ -4,22 +4,20 @@ import { applyHardDifficulty } from "@/utils/difficulty/hard";
 import { DIFFICULTY, GAME_STATE } from "@/consts";
 
 /** @param {import("@/consts").Difficulty} difficulty */
-const handleReadyState = (difficulty) => {
-  setDifficulty(difficulty);
-  setNextDifficulty(null);
-
-  if (difficulty === DIFFICULTY.HARD) return applyHardDifficulty();
-};
-
-/** @param {import("@/consts").Difficulty} difficulty */
-const handlePlayingState = (difficulty) => {
-  setNextDifficulty(difficulty);
-};
-
-/** @param {import("@/consts").Difficulty} difficulty */
 export function handleDifficultyChange(difficulty) {
-  if (gameSate === GAME_STATE.READY) return handleReadyState(difficulty);
-  if (gameSate === GAME_STATE.PLAYING) return handlePlayingState(difficulty);
+  if (gameSate === GAME_STATE.READY) {
+    setDifficulty(difficulty);
+    setNextDifficulty(null);
 
-  throw new Error("Invalid game state for difficulty change");
+    // TODO: if difficulty is easy or normal
+    // - set maxTries to TRIES.MAX
+    // - update $maxTries to TRIES.MAX)
+    // - generate tries indicators
+    // - update $triesIndicators
+    if (difficulty === DIFFICULTY.HARD) return applyHardDifficulty();
+  } else if (gameSate === GAME_STATE.PLAYING) {
+    setNextDifficulty(difficulty);
+  } else {
+    throw new Error("Invalid game state for difficulty change");
+  }
 }
