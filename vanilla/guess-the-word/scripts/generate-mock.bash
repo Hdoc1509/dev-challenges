4d6d649b3f9ca45e6020b84932c03c6d40b1e901
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-02-26 18:55:58 -0500

    feat(guess-the-word): add script to handle generation of mock files

---
+++ vanilla/guess-the-word/scripts/generate-mock.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+min=4
+max=""
+file_name=""
+
+while (($# >= 1)); do
+  case $1 in
+  --min-length) min=$2 ;;
+  --max-length) max=$2 ;;
+  --name) file_name=$2 ;;
+  esac
+  shift
+done
+
+MOCKS_DIR="$PROJECT_ROOT"/src/mocks
+JQ_SCRIPT="$PROJECT_ROOT"/scripts/filter-words.jq
+
+if [[ -z $file_name ]]; then
+  echo "Missing --file argument"
+  exit 1
+fi
+
+jq --arg MIN_LENGTH "$min" --arg MAX_LENGTH "$max" --compact-output --from-file "$JQ_SCRIPT" \
+  "$MOCKS_DIR"/wordsapi_sample.json \
+  >"$MOCKS_DIR"/words/"$file_name".json
--- vanilla/guess-the-word/scripts/mocked-words.bash
+++ vanilla/guess-the-word/scripts/mocked-words.bash
@@ -2,13 +2,8 @@
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 PROJECT_ROOT="$REPO_ROOT"/vanilla/guess-the-word
-MOCKS_DIR="$PROJECT_ROOT"/src/mocks
-JQ_SCRIPT="$PROJECT_ROOT"/scripts/filter-words.jq
 
-jq --arg MIN_LENGTH "4" --arg MAX_LENGTH "9" --compact-output --from-file "$JQ_SCRIPT" \
-  "$MOCKS_DIR"/wordsapi_sample.json \
-  >"$MOCKS_DIR"/words/normal.json
+export PROJECT_ROOT
 
-jq --arg MIN_LENGTH "10" --arg MAX_LENGTH "" --compact-output --from-file "$JQ_SCRIPT" \
-  "$MOCKS_DIR"/wordsapi_sample.json \
-  >"$MOCKS_DIR"/words/extreme.json
+"$PROJECT_ROOT"/scripts/generate-mock.bash --min-length 4 --max-length 9 --name normal
+"$PROJECT_ROOT"/scripts/generate-mock.bash --min-length 10 --name extreme
