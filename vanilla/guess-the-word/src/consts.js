7949d17b802bec0f3fce96a02fcb8e7744e7303c
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-05 18:02:08 -0500

    feat(guess-the-word): add definition-related constants

--- vanilla/guess-the-word/src/consts.js
+++ vanilla/guess-the-word/src/consts.js
@@ -1,6 +1,7 @@
 import WORDS_EASY from "@/mocks/words/easy.json";
 import WORDS_NORMAL from "@/mocks/words/normal.json";
 import WORDS_EXTREME from "@/mocks/words/extreme.json";
+import DEFINITIONS_MOCK from "@/mocks/definitions.json";
 
 const CUSTOM_WORDS = [
   "example",
@@ -10,6 +11,35 @@ const CUSTOM_WORDS = [
   "flower",
   "adventure",
 ];
+const CUSTOM_DEFINITIONS = {
+  example: [
+    "A representative form or pattern",
+    "Something to be imitated",
+    "An occurrrence of something",
+  ],
+  javascript: [
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction#what_is_javascript
+    "A cross-platform, object-oriented scripting language used to make webpages interactive",
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript
+    "A lightweight interpreted (or just-in-time compiled) programming language with first-class functions",
+  ],
+  coding: ["Act of writing in code or cipher"],
+  flower: [
+    "A plant cultivated for its blooms or blossoms",
+    "The period of greatest prosperity or productivity",
+    "Reproductive organ of angiosperm plants especially one having showy or colorful parts",
+  ],
+  adventure: [
+    "Take a risk in the hope of a favorable outcome",
+    "Put at risk",
+    "A wild and exciting undertaking (not necessarily lawful",
+  ],
+};
+
+export const DEFINITIONS = Object.freeze({
+  ...DEFINITIONS_MOCK,
+  ...CUSTOM_DEFINITIONS,
+});
 
 export const WORDS = Object.freeze({
   EASY: Object.freeze(
