4350299d640fbf01490009862cf1413ba62682c6
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-06 18:03:35 -0500

    fix(guess-the-word): use correct syntax for `DefinitionWord` type

--- vanilla/guess-the-word/src/ui/definition.js
+++ vanilla/guess-the-word/src/ui/definition.js
@@ -39,7 +39,7 @@ export const renderSavedDefinitions = (words) => {
 };
 
 /**
- * @param {keyof DEFINITIONS} word
+ * @param {import("@/consts").DefinitionWord} word
  * @param {{ lastWord?: string, initialRender?: boolean }} options
  */
 export const renderDefinition = (word, { lastWord, initialRender = false }) => {
