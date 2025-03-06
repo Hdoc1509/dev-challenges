6820d4384dcc6f8857543b24cd3f6874b1e9b29c
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-06 17:59:45 -0500

    feat(guess-the-word): add constant of localStorage key for discovered words

--- vanilla/guess-the-word/src/state/discovered-words.js
+++ vanilla/guess-the-word/src/state/discovered-words.js
@@ -1,9 +1,11 @@
 /** @typedef {import("@/consts").DefinitionWord} DefinitionWord */
 
+const LOCAL_STORAGE_KEY = "discovered-words";
+
 export let discoveredWords = (() => {
   /** @type {Set<DefinitionWord>} */
   const list = new Set();
-  const savedWords = localStorage.getItem("discovered-words");
+  const savedWords = localStorage.getItem(LOCAL_STORAGE_KEY);
   const parsedWords = JSON.parse(savedWords ?? "[]");
 
   if (Array.isArray(parsedWords)) {
