6dad43507444c94db66462d41e436d032e2745c3
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-14 18:07:53 -0500

    refactor(guess-the-word): remove unused code

--- vanilla/guess-the-word/src/utils/difficulty/extreme.js
+++ vanilla/guess-the-word/src/utils/difficulty/extreme.js
@@ -1,10 +1,6 @@
 import { setWords } from "@/state/words";
 import { WORDS } from "@/consts";
 
-export function applyExtremeDifficulty() {
-  setWords(WORDS.EXTREME);
-}
-
 export const ExtremeDifficulty = Object.freeze({
   apply: () => setWords(WORDS.EXTREME),
 });
