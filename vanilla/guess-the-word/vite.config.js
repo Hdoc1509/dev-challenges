a0fbccf2f12502d359753a02b2047e09eef50ddc
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-14 19:15:33 -0500

    build(guess-the-word): apply code chunking

--- vanilla/guess-the-word/vite.config.js
+++ vanilla/guess-the-word/vite.config.js
@@ -6,6 +6,22 @@ import { createHtmlPlugin } from "vite-plugin-html";
 export default defineConfig({
   plugins: [createHtmlPlugin({ minify: true })],
   base: "/dev-challenges/guess-the-word",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("libs")) return "libs";
+          if (id.includes("src/events")) return "events";
+          if (id.includes("src/constants")) return "constants";
+          if (id.includes("mocks/words/easy")) return "words-easy";
+          if (id.includes("mocks/words/normal")) return "words-normal";
+          if (id.includes("mocks/words/extreme")) return "words-extreme";
+          if (id.includes("mocks/words/why")) return "words-why";
+          if (id.includes("mocks/definitions")) return "definitions";
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       // https://nodejs.org/api/esm.html#importmetadirname
