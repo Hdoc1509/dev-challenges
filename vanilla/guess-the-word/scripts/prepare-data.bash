cda3b50d23694d8e513bd8ba20988bdea0deca48
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-16 17:46:19 -0500

    feat(guess-the-word): add script to generate mock with all words data
    merges:
    - data from wordsapi sample file
    - data from custom words file

---
+++ vanilla/guess-the-word/scripts/prepare-data.bash
@@ -0,0 +1,17 @@
+REPO_ROOT=$(git rev-parse --show-toplevel)
+PROJECT_ROOT="$REPO_ROOT"/vanilla/guess-the-word
+MOCKS_DIR="$PROJECT_ROOT"/src/mocks
+
+NOCOLOR='\033[0m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+
+if [[ ! -f "$MOCKS_DIR"/all-words-data.json ]]; then
+  jq --slurp --compact-output 'reduce .[] as $item ({}; . * $item)' \
+    "$MOCKS_DIR"/wordsapi_sample.json "$MOCKS_DIR"/custom-words-data.json \
+    >"$MOCKS_DIR"/all-words-data.json
+  echo -e "${GREEN}[prepare-data]: All words data mock generated!${NOCOLOR}"
+else
+  echo -e "${YELLOW}[prepare-data]: All words data mock already exists"
+  echo -e "[prepare-data]: Skipping generation...${NOCOLOR}"
+fi
