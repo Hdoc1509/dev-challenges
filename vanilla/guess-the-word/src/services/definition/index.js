8a8516e5bde60b45a43c5239e6fb389eff0004f7
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-19 17:28:52 -0500

    feat(guess-the-word): add type for definition service

--- vanilla/guess-the-word/src/services/definition/index.js
+++ vanilla/guess-the-word/src/services/definition/index.js
@@ -2,12 +2,18 @@ import { z } from "zod";
 import { ServiceError, fetcher } from "@lib/fetcher";
 import { DEFINITIONS } from "@/consts/definitions";
 
-/** @param {import("@/consts/definitions").DefinitionWord} word */
+/**
+ * @callback DefinitionService
+ * @param {import("@/consts/definitions").DefinitionWord} word
+ * @returns {import("@lib/fetcher").PromiseWithError<string[]>}
+ */
+
+/** @type {DefinitionService} */
 export const getMockedDefinition = async (word) => {
   /** @type {string[]} */
   const definitions = DEFINITIONS[word];
 
-  return definitions;
+  return [null, definitions];
 };
 
 const API_URL = "https://api.npoint.io";
@@ -16,10 +22,7 @@ const BIN_ID = "da112cda6039b039864c";
 const DefinitionError = new ServiceError("Definition");
 const DefinitionResponseSchema = z.array(z.string());
 
-/**
- * @param {import("@/consts/definitions").DefinitionWord} word
- * @returns {import("@lib/fetcher").PromiseWithError<string[]>}
- */
+/** @type {DefinitionService} */
 export const getDefinition = async (word) => {
   const [error, data] = await fetcher(`${API_URL}/${BIN_ID}/${word}`, {
     schema: DefinitionResponseSchema,
