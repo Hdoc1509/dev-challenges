48f84094138e446376222541852470668d71613d
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-21 22:49:43 -0500

    feat(guess-the-word): add basic pagination for definitions

---
+++ vanilla/guess-the-word/src/ui/definition/pagination.js
@@ -0,0 +1,10 @@
+import { getElementById } from "@lib/dom";
+import { Pagination } from "@/pagination";
+
+const $pagination = getElementById("definition-pagination", HTMLMenuElement);
+
+export const DefinitionPagination = new Pagination($pagination, {
+  pages: 1,
+  onPageChange: (newPage) => console.log({ newPage }),
+  onPagesQuantity: (newPagesQuantity) => console.log({ newPagesQuantity }),
+});
