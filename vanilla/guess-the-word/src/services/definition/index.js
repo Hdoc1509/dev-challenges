cdbc9971afedd6ca582255f78dfdbc56daa44142
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-21 20:19:52 -0500

    refactor(guess-the-word): remove definition mocked service

--- vanilla/guess-the-word/src/services/definition/index.js
+++ vanilla/guess-the-word/src/services/definition/index.js
@@ -1,25 +1,11 @@
 import { z } from "zod";
 import { ServiceError, fetcher } from "@lib/fetcher";
-import { DEFINITIONS } from "@/consts/definitions";
-import { Random } from "@/utils/random";
 
 /**
  * @callback DefinitionService
  * @param {import("@/consts/definitions").DefinitionWord} word
  * @returns {import("@lib/fetcher").PromiseWithError<string[]>}
  */
-
-/** @type {DefinitionService} */
-export const getMockedDefinition = async (word) => {
-  /** @type {string[]} */
-  const definitions = DEFINITIONS[word];
-  const delay = Random.intInRange(1500, 3000);
-
-  if (delay <= 2000) return [new Error("Mocked error")];
-
-  await new Promise((resolve) => setTimeout(resolve, delay));
-  return [null, definitions];
-};
 
 const API_URL = "https://api.npoint.io";
 const BIN_ID = "da112cda6039b039864c";
