03fe0b72bc30c3d87f02e39f82728e6c58748de3
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-24 17:41:23 -0500

    feat(guess-the-word): add `DefinitionPages` instance
    it will handle the rendering of definition pages

---
+++ vanilla/guess-the-word/src/ui/definition/pages.js
@@ -0,0 +1,35 @@
+import { Pages } from "@/pages";
+import { getElementById, getElementBySelector } from "@lib/dom";
+import { discoveredWords } from "@/state/discovered-words";
+import { DEFINITIONS_PER_PAGE } from "@/consts/definitions";
+
+const $pagesContainer = getElementById(
+  "definition-pages-container",
+  HTMLDivElement,
+);
+
+export const DefinitionPages = new Pages($pagesContainer, {
+  items: Array.from(discoveredWords),
+  itemsPerPage: DEFINITIONS_PER_PAGE,
+  renderItem({ item, index, totalItems }) {
+    const $item = document.createElement("li");
+
+    $item.textContent = item;
+    if (index === totalItems - 1) $item.classList.add("last");
+
+    return $item;
+  },
+  clearEmpty: ($page) => $page.querySelector(".not-yet")?.remove(),
+  $pageTemplate: getElementById(
+    "definition-page-template",
+    HTMLTemplateElement,
+  ),
+  $emptyPageTemplate: getElementById(
+    "definition-page-empty-template",
+    HTMLTemplateElement,
+  ),
+  $total: getElementBySelector(
+    "#definition-pagination .pagination__total",
+    HTMLSpanElement,
+  ),
+});
