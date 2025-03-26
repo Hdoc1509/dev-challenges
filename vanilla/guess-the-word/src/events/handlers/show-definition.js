e03054fe9e9c80d166926621515596f0af18720e
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-25 19:48:58 -0500

    feat(guess-the-word): update page input when showing definition

--- vanilla/guess-the-word/src/events/handlers/show-definition.js
+++ vanilla/guess-the-word/src/events/handlers/show-definition.js
@@ -3,8 +3,11 @@ import { currentWord } from "@/state/current-word";
 import { $definitionsTab, $menu, MenuTabs } from "@/ui/menu";
 import { $definitionSection } from "@/ui/definition/elements";
 import { $definitionPagesContainer } from "@/ui/definition/pages";
+import { DefinitionPagination } from "@/ui/definition/pagination";
 
 export function handleShowDefinition() {
+  DefinitionPagination.updateCurrentPage(1);
+
   const $details = getElementBySelector(
     `.definitions-list.page[data-active] .definition[data-word=${currentWord}]`,
     HTMLDetailsElement,
