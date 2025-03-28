c1b81983400acbaab30c59a8a31e68f49e0e4bf1
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-28 17:46:47 -0500

    feat(guess-the-word): add script to check if filter script is up to date

--- vanilla/guess-the-word/scripts/definitions.bash
+++ vanilla/guess-the-word/scripts/definitions.bash
@@ -3,8 +3,9 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 PROJECT_ROOT="$REPO_ROOT"/vanilla/guess-the-word
 MOCKS_DIR="$PROJECT_ROOT"/src/mocks
-JQ_FILTER_SCRIPT="$PROJECT_ROOT"/scripts/filter-words.jq
-JQ_PARSE_SCRIPT="$PROJECT_ROOT"/scripts/parse-definitions.jq
+SCRIPTS_DIR="$PROJECT_ROOT"/scripts
+JQ_FILTER_SCRIPT="$SCRIPTS_DIR"/filter-words.jq
+JQ_PARSE_SCRIPT="$SCRIPTS_DIR"/parse-definitions.jq
 EASY_LENGTH="$MOCKS_DIR"/difficulties-by-length/easy.json
 MIN_LENGTH="$(jq -r '.min' "$EASY_LENGTH")"
 
@@ -12,12 +13,9 @@ NOCOLOR='\033[0m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 
-branch=$(git branch --show-current)
+export JQ_FILTER_SCRIPT
 
-filter_script_is_up_to_date() {
-  git diff --quiet origin/"$branch" "$branch" "$JQ_FILTER_SCRIPT" &&
-    git diff --quiet "$JQ_FILTER_SCRIPT"
-}
+source "$SCRIPTS_DIR"/check-filter-script.bash
 
 if filter_script_is_up_to_date &>/dev/null; then
   echo
--- vanilla/guess-the-word/scripts/words.bash
+++ vanilla/guess-the-word/scripts/words.bash
@@ -3,7 +3,8 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 PROJECT_ROOT="$REPO_ROOT"/vanilla/guess-the-word
 MOCKS_DIR="$PROJECT_ROOT"/src/mocks
-JQ_SCRIPT="$PROJECT_ROOT"/scripts/filter-words.jq
+SCRIPTS_DIR="$PROJECT_ROOT"/scripts
+JQ_FILTER_SCRIPT="$SCRIPTS_DIR"/filter-words.jq
 EASY_LENGTH="$MOCKS_DIR"/difficulties-by-length/easy.json
 NORMAL_MASTER_LENGTH="$MOCKS_DIR"/difficulties-by-length/normal-master.json
 EXTREME_INSANE_LENGTH="$MOCKS_DIR"/difficulties-by-length/extreme-insane.json
@@ -14,12 +15,9 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 
-branch=$(git branch --show-current)
+export JQ_FILTER_SCRIPT
 
-filter_script_is_up_to_date() {
-  git diff --quiet origin/"$branch" "$branch" "$JQ_SCRIPT" &&
-    git diff --quiet "$JQ_SCRIPT"
-}
+source "$SCRIPTS_DIR"/check-filter-script.bash
 
 if filter_script_is_up_to_date &>/dev/null; then
   echo
@@ -47,7 +45,7 @@ generate_mock() {
     exit 1
   fi
 
-  jq --arg MIN_LENGTH "$min" --arg MAX_LENGTH "$max" --from-file "$JQ_SCRIPT" \
+  jq --arg MIN_LENGTH "$min" --arg MAX_LENGTH "$max" --from-file "$JQ_FILTER_SCRIPT" \
     "$MOCKS_DIR"/all-words-data.json | jq --compact-output '[keys[]]' \
     >"$MOCKS_DIR"/words/"$file_name".json
 
