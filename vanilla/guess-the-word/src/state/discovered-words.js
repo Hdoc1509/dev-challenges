d2808937947c16409c61a174547576b8bcbbf601
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-29 19:05:00 -0500

    refactor(guess-the-word)!: update data structure for discovered words
    - use a Map:
      - words as key
      - difficulties Set as value
    - retro-compatibility with old data structure (Array of words) (Not full compatible)
      - word will have the first difficulty available for it

--- vanilla/guess-the-word/src/state/discovered-words.js
+++ vanilla/guess-the-word/src/state/discovered-words.js
@@ -1,22 +1,43 @@
+import { getDifficultiesOfWord } from "@/utils/difficulty/of-word";
+import { DIFFICULTIES } from "@/consts/difficulty";
+
 const LOCAL_STORAGE_KEY = "discovered-words";
 
 /** @type {string[]} */
 const wordsToSave = [];
 
 export const discoveredWords = (() => {
-  /** @type {Set<string>} */
-  const list = new Set();
+  /** @type {Map<string, Set<string>>} */
+  const wordsMap = new Map();
   const savedWords = localStorage.getItem(LOCAL_STORAGE_KEY);
   const parsedWords = JSON.parse(savedWords ?? "[]");
 
-  if (Array.isArray(parsedWords)) {
-    for (const word of parsedWords) {
-      list.add(word);
+  if (!Array.isArray(parsedWords)) return wordsMap;
+
+  for (const item of parsedWords) {
+    if (typeof item === "string") {
+      const difficulty = getDifficultiesOfWord(item)[0];
+
+      wordsMap.set(item, new Set([difficulty]));
+      wordsToSave.push(item);
+    } else if (Array.isArray(item)) {
+      if (item.length !== 2) continue;
+
+      const [word, difficulties] = item;
+
+      if (
+        typeof word !== "string" ||
+        !Array.isArray(difficulties) ||
+        !difficulties.every((difficulty) => DIFFICULTIES.has(difficulty))
+      )
+        continue;
+
+      wordsMap.set(word, new Set(difficulties));
       wordsToSave.push(word);
     }
   }
 
-  return list;
+  return wordsMap;
 })();
 
 /** @param {string} word */
