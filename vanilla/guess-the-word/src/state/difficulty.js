ddc90747d71d8262efaf5700b3a0696d78782085
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-31 18:30:13 -0500

    fix(guess-the-word): set correct path for `Difficulty` type

--- vanilla/guess-the-word/src/state/difficulty.js
+++ vanilla/guess-the-word/src/state/difficulty.js
@@ -1,10 +1,10 @@
 import { getSelectedDifficulty } from "@/ui/difficulty-form";
 import { IS_DEV } from "@/config";
 
-/** @type {import("@/consts").Difficulty} */
+/** @type {import("@/consts/difficulty").Difficulty} */
 export let difficulty = getSelectedDifficulty();
 
-/** @param {import("@/consts").Difficulty} newDifficulty */
+/** @param {import("@/consts/difficulty").Difficulty} newDifficulty */
 export const setDifficulty = (newDifficulty) => {
   difficulty = newDifficulty;
   if (IS_DEV) console.log({ difficulty });
