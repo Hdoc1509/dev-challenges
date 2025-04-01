bbcb2231285966a363ec206459c019b2cb075596
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-01 18:53:45 -0500

    feat(guess-the-word): add variables for json files in prepare-data.bash

--- vanilla/guess-the-word/scripts/prepare-data.bash
+++ vanilla/guess-the-word/scripts/prepare-data.bash
@@ -1,15 +1,18 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 PROJECT_ROOT="$REPO_ROOT"/vanilla/guess-the-word
 MOCKS_DIR="$PROJECT_ROOT"/src/mocks
+WORDSAPI_SAMPLE="$MOCKS_DIR"/wordsapi_sample.json
+CUSTOM_WORDS_DATA="$MOCKS_DIR"/custom-words-data.json
 
 NOCOLOR='\033[0m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 
-if [[ ! -f "$MOCKS_DIR"/all-words-data.json ]]; then
+TARGET_FILE="$MOCKS_DIR"/all-words-data.json
+
+if [[ ! -f "$TARGET_FILE" ]]; then
   jq --slurp --compact-output 'reduce .[] as $item ({}; . * $item)' \
-    "$MOCKS_DIR"/wordsapi_sample.json "$MOCKS_DIR"/custom-words-data.json \
-    >"$MOCKS_DIR"/all-words-data.json
+    "$WORDSAPI_SAMPLE" "$CUSTOM_WORDS_DATA" >"$TARGET_FILE"
   echo -e "${GREEN}[prepare-data]: All words data mock generated!${NOCOLOR}"
 else
   echo -e "${YELLOW}[prepare-data]: All words data mock already exists"
