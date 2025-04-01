20ddb695320d08c064730da91699f1d689a7bbb3
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-01 17:41:18 -0500

    refactor(guess-the-word): use `DIFFICULTY` constant in `getDifficultiesOfWord()` util

--- vanilla/guess-the-word/src/utils/difficulty/of-word.js
+++ vanilla/guess-the-word/src/utils/difficulty/of-word.js
@@ -1,6 +1,7 @@
 import EASY_LENGTH from "@/mocks/difficulties-by-length/easy.json";
 import NORMAL_MASTER_LENGTH from "@/mocks/difficulties-by-length/normal-master.json";
 import EXTREME_INSANE_LENGTH from "@/mocks/difficulties-by-length/extreme-insane.json";
+import { DIFFICULTY } from "@/consts/difficulty";
 
 /**
  * @param {string} word
@@ -9,8 +10,10 @@ import EXTREME_INSANE_LENGTH from "@/mocks/difficulties-by-length/extreme-insane
 export function getDifficultiesOfWord(word) {
   const size = word.length;
 
-  if (size <= EASY_LENGTH.max) return ["easy"];
-  if (size <= NORMAL_MASTER_LENGTH.max) return ["normal", "hard", "master"];
-  if (size <= EXTREME_INSANE_LENGTH.max) return ["extreme", "insane"];
-  return ["why", "void"];
+  if (size <= EASY_LENGTH.max) return [DIFFICULTY.EASY];
+  if (size <= NORMAL_MASTER_LENGTH.max)
+    return [DIFFICULTY.NORMAL, DIFFICULTY.HARD, DIFFICULTY.MASTER];
+  if (size <= EXTREME_INSANE_LENGTH.max)
+    return [DIFFICULTY.EXTREME, DIFFICULTY.INSANE];
+  return [DIFFICULTY.WHY, DIFFICULTY.VOID];
 }
