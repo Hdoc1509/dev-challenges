7922adde603694066167b6aab898222f5969612f
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-01 17:37:14 -0500

    feat(guess-the-word): add util to get random element from array

--- vanilla/guess-the-word/src/utils/random.js
+++ vanilla/guess-the-word/src/utils/random.js
@@ -6,4 +6,14 @@ export class Random {
   static intInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  /**
+   * Returns a random element from an array
+   * @template T
+   * @param {T[] | readonly T[]} array
+   * @returns {T}
+   */
+  static element(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
 }
