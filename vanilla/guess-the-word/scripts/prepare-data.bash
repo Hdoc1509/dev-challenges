60ebabfcb51759b64c9f678c391caffad2960cd9
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-01 18:54:39 -0500

    feat(guess-the-word): avoid generation of all-words-data.json if source files are up to date

--- vanilla/guess-the-word/scripts/prepare-data.bash
+++ vanilla/guess-the-word/scripts/prepare-data.bash
@@ -1,3 +1,4 @@
+branch=$(git branch --show-current)
 REPO_ROOT=$(git rev-parse --show-toplevel)
 PROJECT_ROOT="$REPO_ROOT"/vanilla/guess-the-word
 MOCKS_DIR="$PROJECT_ROOT"/src/mocks
@@ -7,6 +8,16 @@ CUSTOM_WORDS_DATA="$MOCKS_DIR"/custom-words-data.json
 NOCOLOR='\033[0m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+
+is_up_to_date() {
+  git diff --quiet origin/"$branch" "$branch" "$1" && git diff --quiet "$1" &>/dev/null
+}
+
+if is_up_to_date "$WORDSAPI_SAMPLE" && is_up_to_date "$CUSTOM_WORDS_DATA"; then
+  echo -e "${YELLOW}[prepare-data]: Source files are up to date"
+  echo -e "${YELLOW}[prepare-data]: Skipping generation...${NOCOLOR}"
+  exit 0
+fi
 
 TARGET_FILE="$MOCKS_DIR"/all-words-data.json
 
