2567215f800772320c219b7993571a5a5631bb98
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-01 19:14:41 -0500

    feat(guess-the-word): add variable for definition file in definitions.bash

--- vanilla/guess-the-word/scripts/definitions.bash
+++ vanilla/guess-the-word/scripts/definitions.bash
@@ -6,6 +6,7 @@ MOCKS_DIR="$PROJECT_ROOT"/src/mocks
 SCRIPTS_DIR="$PROJECT_ROOT"/scripts
 JQ_FILTER_SCRIPT="$SCRIPTS_DIR"/filter-words.jq
 JQ_PARSE_SCRIPT="$SCRIPTS_DIR"/parse-definitions.jq
+DEFINITIONS_FILE="$MOCKS_DIR"/definitions.json
 EASY_LENGTH="$MOCKS_DIR"/difficulties-by-length/easy.json
 MIN_LENGTH="$(jq -r '.min' "$EASY_LENGTH")"
 
@@ -15,7 +16,7 @@ YELLOW='\033[1;33m'
 
 source "$SCRIPTS_DIR"/utils.bash
 
-if is_up_to_date "$JQ_FILTER_SCRIPT" && [[ -f "$MOCKS_DIR"/definitions.json ]]; then
+if is_up_to_date "$JQ_FILTER_SCRIPT" && [[ -f "$DEFINITIONS_FILE" ]]; then
   echo
   echo -e "${YELLOW}[defs]: Filter script is up to date"
   echo -e "${YELLOW}[defs]: Skipping generation...${NOCOLOR}"
@@ -24,9 +25,9 @@ fi
 
 jq --arg MIN_LENGTH "$MIN_LENGTH" --arg MAX_LENGTH "" --from-file "$JQ_FILTER_SCRIPT" \
   "$MOCKS_DIR"/all-words-data.json |
-  jq --compact-output --from-file "$JQ_PARSE_SCRIPT" >"$MOCKS_DIR"/definitions.json
+  jq --compact-output --from-file "$JQ_PARSE_SCRIPT" >"$DEFINITIONS_FILE"
 
-jq 'length' "$MOCKS_DIR"/definitions.json >"$MOCKS_DIR"/words-total.json
+jq 'length' "$DEFINITIONS_FILE" >"$MOCKS_DIR"/words-total.json
 echo
 echo -e "${GREEN}[defs]: Generated words-total.json!${NOCOLOR}"
 echo -e "${GREEN}[defs]: Generated definitions.json!${NOCOLOR}"
