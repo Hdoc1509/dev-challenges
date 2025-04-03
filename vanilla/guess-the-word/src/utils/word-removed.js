1d9a80cb578d98be2bf2f95666cc0a3a283d9a8c
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-03 18:46:16 -0500

    feat(guess-the-word): add util to check if word has been removed from game

---
+++ vanilla/guess-the-word/src/utils/word-removed.js
@@ -0,0 +1,38 @@
+import { getDifficultiesOfWord } from "./difficulty/of-word.js";
+import { DIFFICULTY } from "@/consts/difficulty.js";
+
+/** @type {Map<import("@/consts/difficulty").Difficulty, Set<string> | null>} */
+const mockFiles = new Map([
+  [DIFFICULTY.EASY, null],
+  [DIFFICULTY.NORMAL, null],
+  [DIFFICULTY.EXTREME, null],
+  [DIFFICULTY.WHY, null],
+]);
+
+/** @param {string} word */
+export async function isWordRemovedFromGame(word) {
+  const difficulties = getDifficultiesOfWord(word);
+
+  for (const difficultyMock of mockFiles.keys()) {
+    if (difficulties.includes(difficultyMock)) {
+      const cachedModuleWords = mockFiles.get(difficultyMock);
+
+      if (cachedModuleWords == null) {
+        const wordsModule =
+          difficultyMock === DIFFICULTY.NORMAL
+            ? await import(`../consts/words/normal.js`).then(
+                (mod) => mod.WORDS_NORMAL,
+              )
+            : await import(`../consts/words/${difficultyMock}.js`).then(
+                (mod) => mod.default,
+              );
+        const moduleToCache = new Set(wordsModule);
+
+        mockFiles.set(difficultyMock, moduleToCache);
+        return !moduleToCache.has(word);
+      } else return !cachedModuleWords.has(word);
+    }
+  }
+
+  return false;
+}
