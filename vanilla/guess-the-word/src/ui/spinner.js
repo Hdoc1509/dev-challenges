702e56a0c32264de8812e3c63cc091724da7473e
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-06 18:32:11 -0500

    feat(guess-the-word): add utils to add and remove spinner from elements

--- vanilla/guess-the-word/src/ui/spinner.js
+++ vanilla/guess-the-word/src/ui/spinner.js
@@ -6,3 +6,15 @@ const $spinnerTemplate = getElementById(
 );
 
 export const createSpinner = () => $spinnerTemplate.content.cloneNode(true);
+/** @param {HTMLElement[]} $elements */
+export const addSpinner = (...$elements) => {
+  for (const $element of $elements)
+    if ($element.querySelector(".spinner") == null)
+      $element.appendChild(createSpinner());
+};
+
+/** @param {HTMLElement[]} $elements */
+export const removeSpinner = (...$elements) => {
+  for (const $element of $elements)
+    $element.querySelector(".spinner")?.remove();
+};
