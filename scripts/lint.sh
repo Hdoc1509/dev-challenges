27ab8a27822fe6cc49e3821871fcfdfff2f3a3c9
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-08 22:27:06 -0500

    chore(root): call bash script for `lint` script

---
+++ scripts/lint.sh
@@ -0,0 +1,3 @@
+pnpm run lint:vanilla vanilla &&
+  # pnpm run lint:react react && # TODO: enable once added one `react` app
+  pnpm run lint:ssr legacy # ssr # TODO: enable once added one `ssr` app
