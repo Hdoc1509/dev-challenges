6c364ff66da9da4fab7b4d66d6255bf568c8d8e0
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-12 17:58:34 -0500

    fix(guess-the-word): supress error message when checking changes of filter script
    when remote branch does not exist

--- vanilla/guess-the-word/scripts/utils.bash
+++ vanilla/guess-the-word/scripts/utils.bash
@@ -1,5 +1,6 @@
 branch=$(git branch --show-current)
 
 is_up_to_date() {
-  git diff --quiet origin/"$branch" "$branch" "$1" && git diff --quiet "$1" &>/dev/null
+  git diff --quiet origin/"$branch" "$branch" "$1" &>/dev/null &&
+    git diff --quiet "$1" &>/dev/null
 }
