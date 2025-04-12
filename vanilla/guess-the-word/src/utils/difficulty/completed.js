9a079368028df5a50f8efacea67b6a3145aaa7a0
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-12 17:40:37 -0500

    feat(guess-the-word): add util to check if word has completed current difficulty

--- vanilla/guess-the-word/src/utils/difficulty/completed.js
+++ vanilla/guess-the-word/src/utils/difficulty/completed.js
@@ -1,5 +1,10 @@
 import { discoveredWords } from "@/state/discovered-words";
+import { difficulty } from "@/state/difficulty";
 import { DIFFICULTIES_ALL } from "@/consts/difficulty";
+
+/** @param {{ word: string }} params */
+export const hasCompletedDifficulty = ({ word }) =>
+  discoveredWords.get(word)?.includes(difficulty) ?? false;
 
 /** @param {{ word: string }} params */
 export const hasCompletedAllDifficulties = ({ word }) =>
