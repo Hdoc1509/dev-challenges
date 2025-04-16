16064552d7487d81b5d35c5f1d8c534fbfa2c462
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-16 11:48:49 -0500

    refactor(guess-the-word): remove unnecessary code for applying `master` difficulty

--- vanilla/guess-the-word/src/utils/difficulty/master.js
+++ vanilla/guess-the-word/src/utils/difficulty/master.js
@@ -1,20 +1,11 @@
 import { setMaxResets } from "@/state/resets";
-import {
-  $maxResets,
-  $resetsContainer,
-  captureResetsIndicators,
-  generateResetsIndicators,
-  setResetsIndicators,
-} from "@/ui/resets";
+import { $resetsContainer } from "@/ui/resets";
 import { RESETS } from "@/consts/resets";
 
 export const MasterDifficulty = Object.freeze({
   apply() {
     setMaxResets(RESETS.MAX.MASTER);
-    generateResetsIndicators(RESETS.MAX.MASTER);
-    setResetsIndicators(captureResetsIndicators());
     $resetsContainer.setAttribute("data-active", "");
-    $maxResets.textContent = `${RESETS.MAX.MASTER}`;
   },
   unapply: () => $resetsContainer.removeAttribute("data-active"),
 });
