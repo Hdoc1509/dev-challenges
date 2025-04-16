00d8e49cfcc3a9ac83245fc991f24435b7734680
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-16 11:17:38 -0500

    refactor(guess-the-word): implement `TriesIndicator` instance for apply `hard` difficulty

--- vanilla/guess-the-word/src/utils/difficulty/hard.js
+++ vanilla/guess-the-word/src/utils/difficulty/hard.js
@@ -1,19 +1,12 @@
 import { setMaxTries } from "@/state/tries";
 import { setWords } from "@/state/words";
-import {
-  $maxTries,
-  captureTriesIndicators,
-  generateTriesIndicators,
-  setTriesIndicators,
-} from "@/ui/tries";
+import { TriesIndicator } from "@/ui/tries";
 import { TRIES } from "@/consts/tries";
 
 export const HardDifficulty = Object.freeze({
   async apply() {
     setWords(await import("@/consts/words/normal").then((mod) => mod.default));
     setMaxTries(TRIES.MAX_HARD);
-    $maxTries.textContent = `${TRIES.MAX_HARD}`;
-    generateTriesIndicators(TRIES.MAX_HARD);
-    setTriesIndicators(captureTriesIndicators());
+    TriesIndicator.generate(TRIES.MAX_HARD - 1);
   },
 });
