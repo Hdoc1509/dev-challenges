a991bb0f527007858fbe270dc56feafe163e8d80
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-17 10:09:02 -0500

    refactor(guess-the-word): remove unused constants

--- vanilla/guess-the-word/src/consts/css-classes.js
+++ vanilla/guess-the-word/src/consts/css-classes.js
@@ -5,13 +5,4 @@ export const CLASSES = Object.freeze({
     LETTER__MISTAKEN: "typing__letter--mistaken",
     LETTER__CORRECT: "typing__letter--correct",
   }),
-  TRIES: Object.freeze({
-    INDICATOR: "tries__indicator",
-  }),
-  RESETS: Object.freeze({
-    INDICATOR: "resets__indicator",
-  }),
-  STEPPER: Object.freeze({
-    STEP: "stepper__step",
-  }),
 });
