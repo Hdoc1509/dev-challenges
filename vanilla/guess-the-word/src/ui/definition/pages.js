a8d36c7541dff4f69de86f20474e52ca3d5c1074
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-18 11:32:24 -0500

    refactor(guess-the-word): remove usage of `<hr>` elements to render word definitions
    `<hr>` is not a valid child for `<ul>` elements

--- vanilla/guess-the-word/src/ui/definition/pages.js
+++ vanilla/guess-the-word/src/ui/definition/pages.js
@@ -9,36 +9,23 @@ export const $definitionPagesContainer = getElementById(
   HTMLDivElement,
 );
 
+// TODO: remove unused features from Pages class
+
 /** @type {Pages<string>} */
 export const DefinitionPages = new Pages($definitionPagesContainer, {
   itemsPerPage: DEFINITIONS_PER_PAGE,
-  renderItem({ item, index, totalItems, isNew, insertionMode }) {
-    // FIX: remove usage of `<hr>` elements as theses are not valid children of `<ul>`
-    // - simplify DOM handling in `Pages` class
-    // - use similiar approach used for `.hint-group` elements
-    const $definition = createDefinition(item, { isNew });
-    const isLast = index === totalItems - 1;
-
-    if (
-      isNew &&
-      totalItems > 1 &&
-      insertionMode === Pages.INSERTION_MODE.APPEND
-    )
-      $definition.prepend(document.createElement("hr"));
-
-    if (isLast) return $definition;
-
-    $definition.appendChild(document.createElement("hr"));
-    return $definition;
-  },
+  renderItem: ({
+    item,
+    /* index, totalItems, */ isNew /* , insertionMode */,
+  }) => createDefinition(item, { isNew }),
   clearEmpty: ($page) => $page.querySelector(".not-yet")?.remove(),
-  onItemMoved: ($page) =>
-    $page.insertBefore(document.createElement("hr"), $page.children[1]),
-  onItemRemoved: ($page) => {
-    const $lastElement = $page.lastElementChild;
-
-    if ($lastElement instanceof HTMLHRElement) $lastElement.remove();
-  },
+  // onItemMoved: ($page) =>
+  //   $page.insertBefore(document.createElement("hr"), $page.children[1]),
+  // onItemRemoved: ($page) => {
+  //   const $lastElement = $page.lastElementChild;
+  //
+  //   if ($lastElement instanceof HTMLHRElement) $lastElement.remove();
+  // },
   onPageChange: () => $menuTabContent.scrollTo(0, 0),
   $pageTemplate: getElementById(
     "definition-page-template",
