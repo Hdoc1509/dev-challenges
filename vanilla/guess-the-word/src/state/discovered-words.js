261d189520ae1b560042f71530152a783ad5866b
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-19 10:56:01 -0500

    feat(guess-the-word): add state to track discovered words by difficulty

--- vanilla/guess-the-word/src/state/discovered-words.js
+++ vanilla/guess-the-word/src/state/discovered-words.js
@@ -1,5 +1,20 @@
+import { DIFFICULTY } from "@/consts/difficulty";
 /** @typedef {import("@/consts/difficulty").Difficulty} Difficulty */
 /** @typedef {import("@/consts/difficulty").DifficultiesAll} DifficultiesAll */
 
 /** @type {Map<string, Difficulty[] | DifficultiesAll>} */
 export const discoveredWords = new Map();
+
+// NOTE: add new Statistics tab to Menu
+
+/** @type {Map<Difficulty, Set<string>>} */
+export const DiscoveredWordsByDifficulty = new Map([
+  [DIFFICULTY.EASY, new Set()],
+  [DIFFICULTY.NORMAL, new Set()],
+  [DIFFICULTY.HARD, new Set()],
+  [DIFFICULTY.MASTER, new Set()],
+  [DIFFICULTY.EXTREME, new Set()],
+  [DIFFICULTY.INSANE, new Set()],
+  [DIFFICULTY.WHY, new Set()],
+  [DIFFICULTY.VOID, new Set()],
+]);
