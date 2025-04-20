0eb7d92d464cde04c03b6038989d4f4980c5fdb8
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-20 11:32:40 -0500

    fix(guess-the-word): avoid load old format of saved words if new format is available

--- vanilla/guess-the-word/src/services/saved-words/load.js
+++ vanilla/guess-the-word/src/services/saved-words/load.js
@@ -8,12 +8,12 @@ import { DIFFICULTIES_ALL } from "@/consts/difficulty";
 import { DISCOVERED_WORDS } from "@/consts/discovered-words";
 
 export const loadSavedWords = async () => {
-  // FIX: onlye load item with old format if current format is not found
   const oldSavedItem = localStorage.getItem(
     DISCOVERED_WORDS.LOCAL_STORAGE_OLD_KEY,
   );
+  const savedItem = localStorage.getItem(DISCOVERED_WORDS.LOCAL_STORAGE_KEY);
 
-  if (oldSavedItem != null) {
+  if (oldSavedItem != null && savedItem == null) {
     const parsedItem = JSON.parse(oldSavedItem);
     await parseOldFormat(parsedItem, ({ word, difficulties, completed }) => {
       difficulties.forEach((difficulty) =>
@@ -31,8 +31,6 @@ export const loadSavedWords = async () => {
     );
     return;
   }
-
-  const savedItem = localStorage.getItem(DISCOVERED_WORDS.LOCAL_STORAGE_KEY);
 
   if (savedItem === null) return;
 
