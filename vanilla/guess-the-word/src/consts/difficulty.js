7cea3e12804bf002fdc8b85656654c4a5a49eb53
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-21 18:26:25 -0500

    feat(guess-the-word): add internal state to handle available words

--- vanilla/guess-the-word/src/consts/difficulty.js
+++ vanilla/guess-the-word/src/consts/difficulty.js
@@ -6,7 +6,7 @@ export const DIFFICULTY = Object.freeze({
   EXTREME: "extreme",
   INSANE: "insane",
   WHY: "why",
-  VOID: "void"
+  VOID: "void",
 });
 /** @typedef {DIFFICULTY[keyof DIFFICULTY]} Difficulty */
 
@@ -14,3 +14,11 @@ export const DIFFICULTIES = new Set(Object.values(DIFFICULTY));
 
 export const DIFFICULTIES_ALL = "all";
 /** @typedef {typeof DIFFICULTIES_ALL} DifficultiesAll */
+
+export const DIFFICULTY_GROUP = Object.freeze({
+  EASY: DIFFICULTY.EASY,
+  NORMAL: DIFFICULTY.NORMAL,
+  EXTREME: DIFFICULTY.EXTREME,
+  WHY: DIFFICULTY.WHY,
+});
+/** @typedef {DIFFICULTY_GROUP[keyof DIFFICULTY_GROUP]} DifficultyGroup */
--- vanilla/guess-the-word/src/state/words.js
+++ vanilla/guess-the-word/src/state/words.js
@@ -1,3 +1,15 @@
+import { DIFFICULTY_GROUP } from "@/consts/difficulty";
+/** @typedef {import("@/consts/difficulty").DifficultyGroup} DifficultyGroup */
+
+/** @type {Map<DifficultyGroup, Set<string> | null>} */
+// NOTE: delete from this map once a word has completed all of its difficulties
+const AvailableWords = new Map([
+  [DIFFICULTY_GROUP.EASY, null],
+  [DIFFICULTY_GROUP.NORMAL, null],
+  [DIFFICULTY_GROUP.EXTREME, null],
+  [DIFFICULTY_GROUP.WHY, null],
+]);
+
 /** @type {readonly string[]} */
 export let words = [];
 
