8f09fa408d6d6e4a088c497b85efa3716d497cfc
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-21 19:03:03 -0500

    feat(guess-the-word): update available words once loaded saved discovered words

--- vanilla/guess-the-word/src/main.js
+++ vanilla/guess-the-word/src/main.js
@@ -1,14 +1,17 @@
 import { loadSavedWords } from "./services/saved-words/load";
 import { difficulty } from "./state/difficulty";
 import { discoveredWords } from "./state/discovered-words";
+import { removeWord } from "./state/words";
 import { setupEventListeners } from "./events/listeners/setup";
 import { generateRandomWord } from "./events/handlers/random-word";
 import { applyDifficulty } from "./utils/difficulty/apply";
+import { getDifficultyGroupOfWord } from "./utils/difficulty/of-word";
 import { renderDefinitionsCount } from "./ui/definition/count";
 import { DefinitionPages } from "./ui/definition/pages";
 import { $word } from "./ui/word";
 import { $typing } from "./ui/typing";
 import { addSpinner, removeSpinner } from "./ui/spinner";
+import { DIFFICULTIES_ALL } from "./consts/difficulty";
 import "@lib/alert/styles.css";
 import "@fontsource-variable/outfit";
 import "@fontsource/outfit/400.css";
@@ -18,10 +21,16 @@ import "./styles/main.css";
 (async () => {
   // data initialization
   addSpinner($word, $typing);
-  await applyDifficulty(difficulty);
   await loadSavedWords();
+  for (const [word, difficulties] of discoveredWords) {
+    if (difficulties === DIFFICULTIES_ALL)
+      await removeWord(word, {
+        difficultyGroup: getDifficultyGroupOfWord(word),
+      });
+  }
+  await applyDifficulty(difficulty);
   removeSpinner($word, $typing);
-  // TODO: disable $randomButton when all words of current difficulty are completed
+  // TODO: disable $randomButton when there are no available words
   // - use an early return in generateRandomWord()
   // - also do this after handleGameSuccess() if needed
   generateRandomWord();
