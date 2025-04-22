7913f229ff8e01619b904d64d6f6ae2da99294bc
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-22 11:03:33 -0500

    feat(guess-the-word): update return value of `addDiscoveredWord()` service
    return boolean indicating whether the word has all of its difficulties completed

--- vanilla/guess-the-word/src/services/saved-words/add.js
+++ vanilla/guess-the-word/src/services/saved-words/add.js
@@ -4,32 +4,30 @@ import { DIFFICULTIES_ALL } from "@/consts/difficulty";
 import { DISCOVERED_WORDS } from "@/consts/discovered-words";
 /** @typedef {import("@/consts/difficulty").Difficulty} Difficulty */
 
-// TODO: implement DiscoveredWordsByDifficulty
-
-/**
- * @param {string} word
+/** @param {string} word
  * @param {{ difficulty: Difficulty }} extraParams
- */
+ * @returns {{ completed: boolean }} */
 export const addDiscoveredWord = (word, { difficulty }) => {
   const availableDifficulties = getDifficultiesOfWord(word);
 
-  if (!availableDifficulties.includes(difficulty)) return;
+  if (!availableDifficulties.includes(difficulty)) return { completed: false };
 
   const savedDifficulties = discoveredWords.get(word);
 
-  if (savedDifficulties === DIFFICULTIES_ALL) return;
+  if (savedDifficulties === DIFFICULTIES_ALL) return { completed: true };
 
   const difficulties = new Set(savedDifficulties ?? [difficulty]).add(
     difficulty,
   );
 
-  if (difficulties.size === savedDifficulties?.length) return;
+  if (difficulties.size === savedDifficulties?.length)
+    return { completed: false };
+
+  const isCompleted = difficulties.size === availableDifficulties.length;
 
   discoveredWords.set(
     word,
-    difficulties.size === availableDifficulties.length
-      ? DIFFICULTIES_ALL
-      : Array.from(difficulties),
+    isCompleted ? DIFFICULTIES_ALL : Array.from(difficulties),
   );
 
   const data = Array.from(discoveredWords);
@@ -38,4 +36,5 @@ export const addDiscoveredWord = (word, { difficulty }) => {
     DISCOVERED_WORDS.LOCAL_STORAGE_KEY,
     JSON.stringify(data),
   );
+  return { completed: isCompleted };
 };
