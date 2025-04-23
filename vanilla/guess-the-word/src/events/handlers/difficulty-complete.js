46333113d1e5bb8abbc9c120f04f1a3cc3334d1a
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-23 17:33:26 -0500

    feat(guess-the-word): implement more features for dificculty-complete handler

--- vanilla/guess-the-word/src/events/handlers/difficulty-complete.js
+++ vanilla/guess-the-word/src/events/handlers/difficulty-complete.js
@@ -1,12 +1,26 @@
+import { createWordLetters } from "@/ui/word";
+import { $triesContainer } from "@/ui/tries";
+import { $mistakesContainer } from "@/ui/mistakes";
+import { $resetsContainer } from "@/ui/resets";
+import { hideTimer } from "@/ui/timer";
+import {
+  $difficultyCompleted,
+  showCompletedDifficultyMessage,
+} from "@/ui/completed";
 import { $randomWord } from "@/ui/actions";
+import { difficulty } from "@/state/difficulty";
 
 export function handleDifficultyComplete() {
   $randomWord.disabled = true;
+  $triesContainer.removeAttribute("data-active");
+  $mistakesContainer.removeAttribute("data-active");
+  $resetsContainer.removeAttribute("data-active");
+  hideTimer();
+  $difficultyCompleted.setAttribute("data-active", "");
+  showCompletedDifficultyMessage(difficulty);
+
+  createWordLetters("congrats");
   // TODO:
-  // - create word letters with `congrats` as selected word
-  // - hide `info` and `typing` section
-  // - show `difficulty-completed` section
   // - update completed words of all difficulties in Stats tab
   // - disable difficulty option from Difficulty tab
-  console.log("TODO: showCompletedDifficultyMessage");
 }
