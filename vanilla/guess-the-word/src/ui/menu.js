212fc07e9c0279936357b306c10a98160ee36e99
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-28 17:44:33 -0500

    feat(guess-the-word): implement `TabNavItem` component for menu

--- vanilla/guess-the-word/src/ui/menu.js
+++ vanilla/guess-the-word/src/ui/menu.js
@@ -9,12 +9,12 @@ export const $menu = getElementById("app-menu", HTMLDialogElement);
 
 const $menuTabNav = getElementById("menu-tab-nav", HTMLElement);
 export const $definitionsTab = getElementById(
-  "definitions-tab",
+  "definitions-menu-tab",
   HTMLButtonElement,
 );
-export const $statsTab = getElementById("stats-tab", HTMLButtonElement);
+export const $statsTab = getElementById("stats-menu-tab", HTMLButtonElement);
 export const $statsTabContent = getElementById(
-  "stats-tab-content",
+  "stats-menu-tab-content",
   HTMLElement,
 );
 
