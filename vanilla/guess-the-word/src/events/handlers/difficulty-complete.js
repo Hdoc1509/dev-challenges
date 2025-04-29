f61cf01b4c3e8fa02547a952320dad88cf56d656
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-29 18:15:03 -0500

    feat(guess-the-word): handle message of all completed difficulties

--- vanilla/guess-the-word/src/events/handlers/difficulty-complete.js
+++ vanilla/guess-the-word/src/events/handlers/difficulty-complete.js
@@ -1,3 +1,6 @@
+import { gameCompleted, setGameCompleted } from "@/state/game-complete";
+import { difficulty } from "@/state/difficulty";
+import { AvailableWords } from "@/state/words";
 import { createWordLetters } from "@/ui/word";
 import { $triesContainer } from "@/ui/tries";
 import { $mistakesContainer } from "@/ui/mistakes";
@@ -6,15 +9,22 @@ import { hideTimer } from "@/ui/timer";
 import { showCompletedDifficultyMessage } from "@/ui/completed";
 import { $randomWord } from "@/ui/actions";
 import { disableDifficultyOption } from "@/ui/difficulty-form";
-import { difficulty } from "@/state/difficulty";
 
 export function handleDifficultyComplete() {
+  if (!gameCompleted) {
+    const isCompleted = Array.from(AvailableWords).every(
+      ([, words]) => words?.size === 0,
+    );
+
+    setGameCompleted(isCompleted);
+  }
+
   $randomWord.disabled = true;
   $triesContainer.removeAttribute("data-active");
   $mistakesContainer.removeAttribute("data-active");
   $resetsContainer.removeAttribute("data-active");
   hideTimer();
-  showCompletedDifficultyMessage(difficulty);
+  showCompletedDifficultyMessage(difficulty, { allCompleted: gameCompleted });
   createWordLetters("congrats");
   disableDifficultyOption(difficulty);
 }
