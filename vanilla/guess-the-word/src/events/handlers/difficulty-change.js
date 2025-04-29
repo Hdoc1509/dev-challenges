7a06a44c4e7604ce4d542df335c563dc6a183070
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-04-29 17:41:26 -0500

    feat(guess-the-word): handle completed difficulty when changing selecting a new one

--- vanilla/guess-the-word/src/events/handlers/difficulty-change.js
+++ vanilla/guess-the-word/src/events/handlers/difficulty-change.js
@@ -1,10 +1,17 @@
 import { setDifficulty } from "@/state/difficulty";
+import { words } from "@/state/words";
 import { generateRandomWord } from "./random-word";
+import { handleDifficultyComplete } from "./difficulty-complete";
 import { applyDifficulty } from "@/utils/difficulty/apply";
+import { hideCompletedDifficultyMessage } from "@/ui/completed";
 
 /** @param {import("@/consts/difficulty").Difficulty} difficulty */
 export async function handleDifficultyChange(difficulty) {
   setDifficulty(difficulty);
   await applyDifficulty(difficulty);
-  generateRandomWord();
+  if (words.length === 0) handleDifficultyComplete();
+  else {
+    hideCompletedDifficultyMessage();
+    generateRandomWord();
+  }
 }
