8e626eb935579fe37bada4eb60502194959b2d27
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-05-03 12:18:28 -0500

    docs(guess-the-word): simplfy typing of definition service

--- vanilla/guess-the-word/src/services/definition/index.js
+++ vanilla/guess-the-word/src/services/definition/index.js
@@ -1,19 +1,14 @@
 import { z } from "zod";
 import { ServiceError, fetcher } from "@lib/fetcher";
 
-/**
- * @callback DefinitionService
- * @param {string} word
- * @returns {import("@lib/fetcher").PromiseWithError<string[]>}
- */
-
 const API_URL = "https://api.npoint.io";
 const BIN_ID = "da112cda6039b039864c";
 
 const DefinitionError = new ServiceError("Definition");
 const DefinitionResponseSchema = z.array(z.string());
 
-/** @type {DefinitionService} */
+/** @param {string} word
+ * @returns {import("@lib/fetcher").PromiseWithError<string[]>} */
 export const getDefinition = async (word) => {
   const [error, data] = await fetcher(`${API_URL}/${BIN_ID}/${word}`, {
     schema: DefinitionResponseSchema,
