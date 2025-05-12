5b0498587e18e200549f75ca6d66c15feef234e3
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-05-12 18:29:37 -0500

    refactor(guess-the-word): move handling of new discovered difficulty to its own file

--- vanilla/guess-the-word/src/events/handlers/game-success.js
+++ vanilla/guess-the-word/src/events/handlers/game-success.js
@@ -1,16 +1,12 @@
 import { showAlert } from "@lib/alert";
-import { addDiscoveredWord } from "@/services/saved-words/add";
 import { discoveredWords } from "@/state/discovered-words";
-import { removeAvailableWord, words } from "@/state/words";
 import { currentWord } from "@/state/current-word";
 import { difficulty } from "@/state/difficulty";
-import { hasInitialStatsInitialized } from "@/state/stats";
 import { handleNewWord } from "./new-word";
+import { handleNewDifficulty } from "./new-difficulty";
 import { hasCompletedAllDifficulties } from "@/utils/difficulty/completed";
 import { InsaneDifficulty } from "@/utils/difficulty/insane";
 import { showCorrectWord } from "@/ui/word";
-import { renderCompletedDifficulty } from "@/ui/definition/difficulty";
-import { $definitionSection } from "@/ui/definition/elements";
 import { $hints, $hintsContent } from "@/ui/hints";
 import { $reset } from "@/ui/actions";
 import { TOTAL_WORDS } from "@/consts/words/total";
@@ -29,29 +25,9 @@ export async function handleGameSuccess() {
 
   const totalWords = TOTAL_WORDS[DIFFICULTY_GROUP[difficulty]];
 
-  if (!discoveredWords.has(currentWord)) {
+  if (!discoveredWords.has(currentWord))
+    // TODO: access to `currentWord` internally
     handleNewWord(currentWord, { totalWords });
-  } else if (!hasCompletedAllDifficulties({ word: currentWord })) {
-    // TODO: move to handleCompletedDifficulty()
-    const { completed } = addDiscoveredWord(currentWord, { difficulty });
-
-    await removeAvailableWord(currentWord, { difficulty });
-
-    if (hasInitialStatsInitialized)
-      await import("@/ui/stats/current").then(({ renderCurrentStats }) =>
-        renderCurrentStats({
-          category: difficulty,
-          count: totalWords - words.length,
-          total: totalWords,
-        }),
-      );
-
-    if (completed && words.length === 0)
-      import("./difficulty-complete").then(({ handleDifficultyComplete }) =>
-        handleDifficultyComplete(),
-      );
-
-    if (renderCompletedDifficulty({ word: currentWord, difficulty }))
-      $definitionSection.setAttribute("data-active", "");
-  }
+  else if (!hasCompletedAllDifficulties({ word: currentWord }))
+    handleNewDifficulty();
 }
---
+++ vanilla/guess-the-word/src/events/handlers/new-difficulty.js
@@ -0,0 +1,33 @@
+import { addDiscoveredWord } from "@/services/saved-words/add";
+import { removeAvailableWord, words } from "@/state/words";
+import { currentWord } from "@/state/current-word";
+import { difficulty } from "@/state/difficulty";
+import { hasInitialStatsInitialized } from "@/state/stats";
+import { renderCompletedDifficulty } from "@/ui/definition/difficulty";
+import { $definitionSection } from "@/ui/definition/elements";
+import { TOTAL_WORDS } from "@/consts/words/total";
+import { DIFFICULTY_GROUP } from "@/consts/difficulty";
+
+export async function handleNewDifficulty() {
+  const { completed } = addDiscoveredWord(currentWord, { difficulty });
+  const totalWords = TOTAL_WORDS[DIFFICULTY_GROUP[difficulty]];
+
+  await removeAvailableWord(currentWord, { difficulty });
+
+  if (hasInitialStatsInitialized)
+    await import("@/ui/stats/current").then(({ renderCurrentStats }) =>
+      renderCurrentStats({
+        category: difficulty,
+        count: totalWords - words.length,
+        total: totalWords,
+      }),
+    );
+
+  if (completed && words.length === 0)
+    import("./difficulty-complete").then(({ handleDifficultyComplete }) =>
+      handleDifficultyComplete(),
+    );
+
+  if (renderCompletedDifficulty({ word: currentWord, difficulty }))
+    $definitionSection.setAttribute("data-active", "");
+}
