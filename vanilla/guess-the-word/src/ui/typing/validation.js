b1b6ccce0e2edfe00edb1e5df7d047e5d60c8adb
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-05-13 16:36:11 -0500

    feat(guess-the-word): use strictest validations for elements in `typing` section

--- vanilla/guess-the-word/src/ui/typing/validation.js
+++ vanilla/guess-the-word/src/ui/typing/validation.js
@@ -1,13 +1,14 @@
+import { $typing } from "./elements";
 import { CLASSES } from "@/consts/css-classes";
-
-// TODO: check if typing section contains element to check
 
 /** @type {($target: EventTarget | null) => $target is HTMLInputElement} */
 export const isValidLetterField = ($target) =>
   $target instanceof HTMLInputElement &&
+  $typing.contains($target) &&
   $target.matches(`.${CLASSES.TYPING.LETTER} > input`);
 
 /** @type {($target: EventTarget | null) => $target is HTMLSpanElement} */
 export const isValidTypingLetter = ($target) =>
   $target instanceof HTMLSpanElement &&
+  $typing.contains($target) &&
   $target.matches(`.${CLASSES.TYPING.LETTER}`);
