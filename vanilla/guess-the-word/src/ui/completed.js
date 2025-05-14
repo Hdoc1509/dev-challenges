025282b8cdacefffaa11e5cfe4917c2a3626526b
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-05-14 18:44:39 -0500

    refactor(guess-the-word): remove usage of `.menu-trigger` class from button to show difficulties

--- vanilla/guess-the-word/src/ui/completed.js
+++ vanilla/guess-the-word/src/ui/completed.js
@@ -25,7 +25,7 @@ const $completedName = getElementBySelector(
 );
 
 export const $showDifficulties = getElementBySelector(
-  ":scope > .menu-trigger",
+  ":scope > .button",
   HTMLButtonElement,
   $difficultyCompleted,
 );
