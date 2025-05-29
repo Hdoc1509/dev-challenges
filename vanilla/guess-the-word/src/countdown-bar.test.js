8646f31c7ffd51cc58eba0643010e71e640989f0
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-05-29 18:12:54 -0500

    test(guess-the-word): add initial test for `CountdownBar`

---
+++ vanilla/guess-the-word/src/countdown-bar.test.js
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "vitest";
+import { CountdownBar } from "./countdown-bar";
+
+const createCountdownBar = ({ labeled = false } = {}) => {
+  const $countdownBar = document.createElement("div");
+  const $track = document.createElement("div");
+  let $label;
+
+  $countdownBar.classList.add("countdown-bar");
+  $track.classList.add("countdown-bar__track");
+
+  $countdownBar.append($track);
+
+  if (labeled) {
+    $label = document.createElement("span");
+
+    $label.classList.add("countdown-bar__label", "visually-hidden");
+    $label.setAttribute("role", "alert");
+    $countdownBar.append($label);
+  }
+
+  return { $countdownBar, $track, $label };
+};
+
+describe("CountdownBar", () => {
+  it("should be enabled with .start()  method", () => {
+    // reference: https://stackoverflow.com/a/53390149
+    const { $countdownBar, $track } = createCountdownBar();
+    const TestCountdownBar = new CountdownBar($countdownBar);
+
+    TestCountdownBar.start({ duration: 0, onEnd: () => {} });
+
+    expect($track.hasAttribute("data-active")).toBe(true);
+  });
+});
