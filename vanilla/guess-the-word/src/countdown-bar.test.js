1b27e2dd9a0cc67f2a5761ca41c23f81bdb7f4bf
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-05-29 18:16:02 -0500

    test(guess-the-word): add test for `onEnd` handler of `CountdownBar.start()` method

--- vanilla/guess-the-word/src/countdown-bar.test.js
+++ vanilla/guess-the-word/src/countdown-bar.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { CountdownBar } from "./countdown-bar";
 
 const createCountdownBar = ({ labeled = false } = {}) => {
@@ -31,5 +31,16 @@ describe("CountdownBar", () => {
     TestCountdownBar.start({ duration: 0, onEnd: () => {} });
 
     expect($track.hasAttribute("data-active")).toBe(true);
+  });
+
+  it("should call `onEnd` handler once countdown finishes", async () => {
+    const { $countdownBar, $track } = createCountdownBar();
+    const TestCountdownBar = new CountdownBar($countdownBar);
+    const onEnd = vi.fn();
+
+    TestCountdownBar.start({ duration: 0, onEnd });
+    $track.dispatchEvent(new Event("animationend"));
+
+    expect(onEnd).toHaveBeenCalledOnce();
   });
 });
