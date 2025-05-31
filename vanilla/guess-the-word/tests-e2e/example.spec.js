984d40ce84fda50d9dc8439fdaa6cf88287b5935
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-05-31 16:41:05 -0500

    test(guess-the-word): check correct page title

--- vanilla/guess-the-word/tests-e2e/example.spec.js
+++ vanilla/guess-the-word/tests-e2e/example.spec.js
@@ -4,8 +4,7 @@ import { test, expect } from "@playwright/test";
 test("has title", async ({ page }) => {
   await page.goto("/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Guess the word Game - DevChallenges/);
+  await expect(page).toHaveTitle(/Guess The Word Game - DevChallenges/);
 });
 
 /* test("get started link", async ({ page }) => {
