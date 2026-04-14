c1bd21b1128b81eb7519c237320c63e5745a5b16
Hdoc1509 <hector.ochoa.dev@gmail.com>
2026-04-14 12:06:52 -0500

    fix(music-player): update `max` value of progress bar when changing song. fixes #12

--- vanilla/music-player/TODO.md
+++ vanilla/music-player/TODO.md
@@ -4,12 +4,6 @@
 
 - Improve visibility of hovered/focused buttons
 
-- Update play time progress bar when song changes, in `handleSongChange()`:
-
-  ```js
-  $playProgress.max = `${duration}`;
-  ```
-
 ## Vite
 
 - Update to Vite 7 to use initial `src` attribute in `<audio>` tag.
--- vanilla/music-player/src/events/handlers/song-change.js
+++ vanilla/music-player/src/events/handlers/song-change.js
@@ -1,8 +1,10 @@
 import { formatTime } from "@/utils";
 import { $song, $songDuration } from "@/song.js";
+import { $playProgress } from "@/progress";
 
 export function handleSongChange() {
   const duration = $song.duration;
 
   $songDuration.textContent = formatTime(duration);
+  $playProgress.max = `${Math.round(duration)}`;
 }
