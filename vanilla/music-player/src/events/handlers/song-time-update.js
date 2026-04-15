cd234ee9c90db9e956c5d185d009bf7d9e5fa684
Hdoc1509 <hector.ochoa.dev@gmail.com>
2026-04-14 20:34:30 -0500

    perf(music-player): only update progress bar when seconds change

--- vanilla/music-player/src/events/handlers/song-time-update.js
+++ vanilla/music-player/src/events/handlers/song-time-update.js
@@ -2,11 +2,18 @@ import { formatTime } from "@/utils";
 import { $playProgress } from "@/progress.js";
 import { $song, $songCurrentTime } from "@/song.js";
 
+let previousSecond = 0;
+
 export function handleSongTimeUpdate() {
   const currentTime = $song.currentTime;
+  const currentSecond = parseInt(`${currentTime}`);
   const progress = (currentTime / Number($playProgress.max)) * 100;
 
+  $playProgress.style.setProperty("--progress-value", `${progress}%`);
+
+  if (currentSecond === previousSecond) return;
+
+  previousSecond = currentSecond;
   $songCurrentTime.textContent = formatTime(currentTime);
   $playProgress.value = currentTime.toString();
-  $playProgress.style.setProperty("--progress-value", `${progress}%`);
 }
