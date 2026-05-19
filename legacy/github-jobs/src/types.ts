9b9754d9e393afbe44e882151028d58d06389734
Hdoc1509 <hector.ochoa.dev@gmail.com>
2026-05-19 10:54:08 -0500

    refactor(github-jobs): declare local `LocationCoords` type

--- legacy/github-jobs/src/types.ts
+++ legacy/github-jobs/src/types.ts
@@ -1,5 +1,4 @@
 import type { Simplify } from "@hrc/type-utils";
-import type { LocationCoords } from "@lib/geolocation";
 
 export type ApplyOption = {
   title: string;
@@ -26,6 +25,7 @@ export type Search = {
   nextPageToken?: string;
 };
 
+export type LocationCoords = { latitude: number; longitude: number };
 export type LocationOptions = { zipCode: number } | { coords: LocationCoords };
 
 export type SetOptional<BaseType, Keys extends keyof BaseType> = Simplify<
